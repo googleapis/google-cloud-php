135d5d1ea70983e1df218cfcef7efeaeead7f468
jdpedrie <john@pedrie.com>
2016-12-08 18:26:18 -0500

    Prepare v0.20.0
    :tada: First beta release!!

--- src/ServiceBuilder.php
+++ src/ServiceBuilder.php
@@ -48,7 +48,7 @@ use Psr\Cache\CacheItemPoolInterface;
  */
 class ServiceBuilder
 {
-    const VERSION = '0.13.2';
+    const VERSION = '0.20.0';
 
     /**
      * @var array Configuration options to be used between clients.
