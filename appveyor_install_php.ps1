4dafb2a03bb7e2d252f671c52b69bea126fbb0bc
Cedric Ziel <cedric@cedric-ziel.com>
2017-01-04 12:29:32 +0100

    Download php zip file from releases folder

--- appveyor_install_php.ps1
+++ appveyor_install_php.ps1
@@ -1,2 +1,3 @@
-appveyor DownloadFile http://windows.php.net/downloads/releases/archives/php-$env:PHP_VERSION.zip
+appveyor DownloadFile http://windows.php.net/downloads/releases/php-$env:PHP_VERSION.zip
+
 7z x php-$env:PHP_VERSION.zip -oc:\tools\php
