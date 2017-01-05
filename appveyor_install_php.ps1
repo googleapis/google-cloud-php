35ccd9b8775b7774a0ea34a6a84b8e47421f457d
Cedric Ziel <cedric@cedric-ziel.com>
2017-01-05 19:42:30 +0100

    Test PHP 7.1 compatibility and fix regressions (#303)
    * Test PHP 7.1 compatibility with travis
    
    * Update Appveyor PHP version matrix to use latest versions
    
    Adds PHP 7.1
    
    * Fix EntityMapperTest for PHP 7.1
    
    This fixes an issue where to different date objects (naturally) don't have
    the same microseconds when formatted.
    
    The solution is to use the same object which yields the same
    microseconds.
    
    * Download php zip file from releases folder
    
    * fallback to archived releases

--- appveyor_install_php.ps1
+++ appveyor_install_php.ps1
@@ -1,2 +1,13 @@
-appveyor DownloadFile http://windows.php.net/downloads/releases/archives/php-$env:PHP_VERSION.zip
-7z x php-$env:PHP_VERSION.zip -oc:\tools\php
+$file = "php-$env:PHP_VERSION.zip"
+$archiveUrl = "http://windows.php.net/downloads/releases/archives/$file"
+$url = "http://windows.php.net/downloads/releases/$file"
+$projectPath = "C:\projects\google-cloud"
+$client = New-Object NET.WebClient
+
+try {
+    $client.DownloadFile($url, "$projectPath\$file")
+} catch {
+    $client.DownloadFile($archiveUrl, "$projectPath\$file")
+}
+
+7z x $file -oc:\tools\php
