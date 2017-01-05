08848ce8c67ed7c08e18b89d5a9c67d9aea62407
Dave Supplee <dwsupplee@gmail.com>
2017-01-05 11:44:35 -0500

    fallback to archived releases

--- appveyor_install_php.ps1
+++ appveyor_install_php.ps1
@@ -1,3 +1,13 @@
-appveyor DownloadFile http://windows.php.net/downloads/releases/php-$env:PHP_VERSION.zip
+$file = "php-$env:PHP_VERSION.zip"
+$archiveUrl = "http://windows.php.net/downloads/releases/archives/$file"
+$url = "http://windows.php.net/downloads/releases/$file"
+$projectPath = "C:\projects\google-cloud"
+$client = New-Object NET.WebClient
 
-7z x php-$env:PHP_VERSION.zip -oc:\tools\php
+try {
+    $client.DownloadFile($url, "$projectPath\$file")
+} catch {
+    $client.DownloadFile($archiveUrl, "$projectPath\$file")
+}
+
+7z x $file -oc:\tools\php
