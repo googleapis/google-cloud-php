bb5887767bdb69fafe00efff38138ddfb128e33b
Jeff Ching <chingor@google.com>
2017-03-22 15:01:25 -0700

    Some backtrace entries do not have line numbers

--- src/Trace/TraceSpan.php
+++ src/Trace/TraceSpan.php
@@ -199,10 +199,11 @@ class TraceSpan
     {
         // Try to find the first stacktrace class entry that doesn't start with Google\Cloud\Trace
         foreach (debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS) as $bt) {
+            $bt += ['line' => null];
             if (!array_key_exists('class', $bt)) {
-                return implode('/', array_filter(['app', basename($bt['file']), $b['function'], $bt['line']]));
+                return implode('/', array_filter(['app', basename($bt['file']), $bt['function'], $bt['line']]));
             } elseif (substr($bt['class'], 0, 18) != 'Google\Cloud\Trace') {
-                return implode('/', array_filter(['app', $bt['class'], $b['function'], $bt['line']]));
+                return implode('/', array_filter(['app', $bt['class'], $bt['function'], $bt['line']]));
             }
         }
 
