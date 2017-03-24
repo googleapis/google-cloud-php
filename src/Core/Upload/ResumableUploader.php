5d714998b892803eb9d8b579d611f3b172725916
Petar Milenov <petar.milenov@gmail.com>
2017-03-24 11:08:40 +0200

    Include forgotten namespace.

--- src/Core/Upload/ResumableUploader.php
+++ src/Core/Upload/ResumableUploader.php
@@ -23,6 +23,7 @@ use GuzzleHttp\Psr7;
 use GuzzleHttp\Psr7\LimitStream;
 use GuzzleHttp\Psr7\Request;
 use Psr\Http\Message\ResponseInterface;
+use Google\Cloud\Core\RequestWrapper;
 
 /**
  * Resumable upload implementation.
