7daad390c39e2db3bc71ddc1600b9f0032049a93
Takashi Matsuo <tmatsuo@google.com>
2017-04-13 23:24:29 -0700

    Use exec to make sure the child processes are direct children

--- src/Core/Batch/BatchDaemon.php
+++ src/Core/Batch/BatchDaemon.php
@@ -67,7 +67,7 @@ class BatchDaemon
         pcntl_signal(SIGTERM, [$this, "sigHandler"]);
         pcntl_signal(SIGINT, [$this, "sigHandler"]);
         pcntl_signal(SIGHUP, [$this, "sigHandler"]);
-        $this->command = sprintf('php -d auto_prepend_file="" %s', $entrypoint);
+        $this->command = sprintf('exec php -d auto_prepend_file="" %s', $entrypoint);
         $this->initFailureFile();
     }
 
