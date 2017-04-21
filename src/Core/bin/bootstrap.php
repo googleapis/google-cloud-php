e759c24638850a5db6ee95cc6e685dd88ecc6e2b
Takashi Matsuo <tmatsuo@google.com>
2017-04-20 20:42:27 -0700

    Needed to traverse one more level.

--- src/Core/bin/bootstrap.php
+++ src/Core/bin/bootstrap.php
@@ -25,9 +25,9 @@ if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
 } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
     // Called from google/cloud-core installation.
     require_once __DIR__ . '/../../../autoload.php';
-} elseif (file_exists(__DIR__ . '/../../../../autoload.php')) {
+} elseif (file_exists(__DIR__ . '/../../../../../autoload.php')) {
     // Called from google/cloud installation.
-    require_once __DIR__ . '/../../../../autoload.php';
+    require_once __DIR__ . '/../../../../../autoload.php';
 } else {
     die('No autoloader found');
 }
