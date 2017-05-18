6fa82bd64a3d1d62d8c649c7ce5d0562f93c5611
Dave Supplee <dwsupplee@gmail.com>
2017-05-18 17:27:08 -0400

    Prepare v0.30.1

--- src/ServiceBuilder.php
+++ src/ServiceBuilder.php
@@ -50,7 +50,7 @@ use Psr\Cache\CacheItemPoolInterface;
  */
 class ServiceBuilder
 {
-    const VERSION = '0.30.0';
+    const VERSION = '0.30.1';
 
     /**
      * @var array Configuration options to be used between clients.
