49a598e2d518f0244d007cf68f812d4951126a38
David Supplee <dwsupplee@gmail.com>
2017-05-18 11:22:19 -0400

    Prepare v0.29.0 (#493)

--- src/PubSub/PubSubClient.php
+++ src/PubSub/PubSubClient.php
@@ -85,7 +85,7 @@ class PubSubClient
     use IncomingMessageTrait;
     use ResourceNameTrait;
 
-    const VERSION = '0.4.0';
+    const VERSION = '0.5.0';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/pubsub';
 
