c22d363f88d1e74bccd882b8ac3ed1a09732bbcf
Jeff Ching <chingor@google.com>
2017-05-23 13:59:57 -0700

    Add ramsey/uuid dependency for generating trace ids (uuids)

--- src/Trace/IdGeneratorTrait.php
+++ src/Trace/IdGeneratorTrait.php
@@ -17,6 +17,8 @@
 
 namespace Google\Cloud\Trace;
 
+use Ramsey\Uuid\Uuid;
+
 /**
  * Trait which provides helper methods for generating trace identifiers.
  */
@@ -29,24 +31,6 @@ trait IdGeneratorTrait
      */
     private function generateTraceId()
     {
-        return sprintf(
-            '%04x%04x%04x%04x%04x%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-            // 48 bits for "node"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
+        return str_replace('-', '', Uuid::uuid4());
     }
 }
