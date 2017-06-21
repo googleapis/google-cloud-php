159a15dc95956fe1ac5af43828e3781097a18dad
Giorgio Balduzzi <gio.balduz@gmail.com>
2017-06-21 22:57:21 +0200

    Add the proper Exception message for all JSON error types by PHP (firebase/php-jwt#110)

--- src/JWT.php
+++ src/JWT.php
@@ -352,8 +352,10 @@ class JWT
     {
         $messages = array(
             JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
             JSON_ERROR_CTRL_CHAR => 'Unexpected control character found',
-            JSON_ERROR_SYNTAX => 'Syntax error, malformed JSON'
+            JSON_ERROR_SYNTAX => 'Syntax error, malformed JSON',
+            JSON_ERROR_UTF8 => 'Malformed UTF-8 characters' //PHP >= 5.3.3
         );
         throw new DomainException(
             isset($messages[$errno])
