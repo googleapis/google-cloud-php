dc2ed29e585fecb2ab118e098c8ba40b9bd01e57
Jeff Ching <chingor@google.com>
2017-07-13 10:22:49 -0700

    Fix use for doctrine

--- src/Trace/Integrations/Doctrine.php
+++ src/Trace/Integrations/Doctrine.php
@@ -19,6 +19,8 @@ namespace Google\Cloud\Trace\Integrations;
 
 use Doctrine\ORM\Version;
 use Doctrine\ORM\AbstractQuery;
+use Doctrine\DBAL\Driver\PDOConnection;
+use Doctrine\DBAL\Driver\PDOStatement;
 
 /**
  * This class handles instrumenting the Doctrine ORM queries using the stackdriver_trace extension.
