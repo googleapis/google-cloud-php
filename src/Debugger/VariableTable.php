bdaf0f5ae3cf2626bcada341771563683e2694b8
Jeff Ching <chingor@google.com>
2017-07-19 16:16:38 -0700

    Fix member depth and only write to the variable table if it's new

--- src/Debugger/VariableTable.php
+++ src/Debugger/VariableTable.php
@@ -30,23 +30,31 @@ class VariableTable implements \JsonSerializable
                 $valueString = (string)$value;
         }
 
-        $variable = new Variable([
-            'name' => $name,
-            'type' => $type,
-            'value' => $valueString
-        ]);
-        if ($depth < self::MAX_MEMBER_DEPTH) {
-            foreach ($members as $index => $member) {
-                array_push($variable->members, $this->register($index, $member));
+        if (array_key_exists($index, $this->variables)) {
+            return new Variable([
+                'name' => $name,
+                'type' => $type,
+                'varTableIndex' => $this->variables[$index]->varTableIndex
+            ]);
+        } else {
+            $variable = new Variable([
+                'name' => $name,
+                'type' => $type,
+                'value' => $valueString,
+                'varTableIndex' => $this->nextIndex
+            ]);
+            if ($depth < self::MAX_MEMBER_DEPTH) {
+                foreach ($members as $index => $member) {
+                    array_push($variable->members, $this->register($index, $member, $depth + 1));
+                }
             }
+            $this->variables[$index] = $variable;
+            return new Variable([
+                'name' => $name,
+                'type' => $type,
+                'varTableIndex' => $this->nextIndex++
+            ]);
         }
-        $this->variables[$index] = $variable;
-
-        return new Variable([
-            'name' => $name,
-            'type' => $type,
-            'varTableIndex' => $this->nextIndex++
-        ]);
     }
 
     public function jsonSerialize()
