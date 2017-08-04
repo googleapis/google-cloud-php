0539615c4f864ad331e3832b02b18f2be00e7980
jdpedrie <john@pedrie.com>
2017-08-04 12:45:05 -0400

    Add Firestore public API classes

---
+++ src/Firestore/FirestoreClient.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Firestore;
+
+use Google\Cloud\Firestore\Connection\Rest;
+
+class FirestoreClient
+{
+    const VERSION = 'master';
+
+    const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/datastore';
+
+    private $rest;
+
+    private $database = 'default';
+
+    /**
+     * Create a Firestore client.
+     *
+     * @param array $config [optional] {
+     *     Configuration Options.
+     *
+     *     @type string $projectId The project ID from the Google Developer's
+     *           Console.
+     *     @type CacheItemPoolInterface $authCache A cache for storing access
+     *           tokens. **Defaults to** a simple in memory implementation.
+     *     @type array $authCacheOptions Cache configuration options.
+     *     @type callable $authHttpHandler A handler used to deliver Psr7
+     *           requests specifically for authentication.
+     *     @type callable $httpHandler A handler used to deliver Psr7 requests.
+     *           Only valid for requests sent over REST.
+     *     @type array $keyFile The contents of the service account credentials
+     *           .json file retrieved from the Google Developer's Console.
+     *           Ex: `json_decode(file_get_contents($path), true)`.
+     *     @type string $keyFilePath The full path to your service account
+     *           credentials .json file retrieved from the Google Developers
+     *           Console.
+     *     @type int $retries Number of retries for a failed request. **Defaults
+     *           to** `3`.
+     *     @type array $scopes Scopes to be used for the request.
+     *     @type bool $returnInt64AsObject If true, 64 bit integers will be
+     *           returned as a {@see Google\Cloud\Core\Int64} object for 32 bit
+     *           platform compatibility. **Defaults to** false.
+     * }
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(array $config = [])
+    {
+        $config += [
+            'returnInt64AsObject' => false,
+            'scopes' => [self::FULL_CONTROL_SCOPE]
+        ];
+
+        $this->connection = new Rest($this->configureAuthentication($config));
+    }
+
+    public function collection($collectionPath, array $options = [])
+    {}
+
+    public function collections(array $options = [])
+    {}
+
+    public function document($documentPath, array $options = [])
+    {}
+
+    public function documents(array $references, array $options = [])
+    {}
+
+    public function runTransaction(callable $transaction, array $options = [])
+    {}
+}
