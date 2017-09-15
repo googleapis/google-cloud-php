70f132f8a7a591d6ce5ca3efed1fd0f074490c39
Jeff Ching <chingor@google.com>
2017-09-15 12:04:02 -0700

    capture array members also

--- src/Debugger/VariableTable.php
+++ src/Debugger/VariableTable.php
@@ -13,12 +13,14 @@ class VariableTable implements \JsonSerializable
     public function register($name, $value, $depth = 0)
     {
         $name = (string)$name;
-        $type = gettype($value);
-        $index = $this->nextIndex;
         $members = [];
         $shared = false;
+        $variable = new Variable([
+            'name' => $name,
+            'type' => gettype($value)
+        ]);
 
-        switch ($type) {
+        switch ($variable->type) {
             case 'object':
                 $type = get_class($value);
                 $hash = spl_object_hash($value);
@@ -27,17 +29,13 @@ class VariableTable implements \JsonSerializable
                     $index = $this->sharedVariableIndex[$hash];
                     $shared = true;
                 } else {
+                    $index = $this->nextIndex;
                     $this->sharedVariableIndex[$hash] = $index;
-                    $valueString = "$type ($hash)";
-                    $members = get_object_vars($value);
+                    $variable->type = $type;
+                    $variable->value = "$type ($hash)";
 
-                    $variable = new Variable([
-                        'name' => $name,
-                        'type' => $type,
-                        'value' => $valueString
-                    ]);
                     if ($depth < self::MAX_MEMBER_DEPTH) {
-                        foreach ($members as $key => $member) {
+                        foreach (get_object_vars($value) as $key => $member) {
                             array_push($variable->members, $this->register($key, $member, $depth + 1));
                         }
                     }
@@ -52,20 +50,20 @@ class VariableTable implements \JsonSerializable
                 break;
             case 'array':
                 $arraySize = count($value);
-                $valueString = "array ($arraySize)";
-                $members = $value;
+                $variable->value = "array ($arraySize)";
+                if ($depth < self::MAX_MEMBER_DEPTH) {
+                    foreach ($value as $key => $member) {
+                        array_push($variable->members, $this->register($key, $member, $depth + 1));
+                    }
+                }
                 break;
             case 'NULL':
-                $valueString = 'NULL';
+                $variable->value = 'NULL';
             default:
-                $valueString = (string)$value;
+                $variable->value = (string)$value;
         }
 
-        return new Variable([
-            'name' => $name,
-            'type' => $type,
-            'value' => $valueString
-        ]);
+        return $variable;
     }
 
     public function jsonSerialize()
