571c6438a67769af2a0d81966bfc77f4fb8ddcc2
jdpedrie <john@pedrie.com>
2017-10-04 13:39:19 -0400

    Fix regression in Cloud Spanner

--- src/Spanner/Timestamp.php
+++ src/Spanner/Timestamp.php
@@ -17,6 +17,8 @@
 
 namespace Google\Cloud\Spanner;
 
+use Google\Cloud\Core\Timestamp as CoreTimestamp;
+
 /**
  * Represents a value with a data type of
  * [Timestamp](https://cloud.google.com/spanner/docs/reference/rpc/google.spanner.v1#google.spanner.v1.TypeCode).
@@ -40,48 +42,8 @@ namespace Google\Cloud\Spanner;
  * echo (string) $timestamp;
  * ```
  */
-class Timestamp implements ValueInterface
+class Timestamp extends CoreTimestamp implements ValueInterface
 {
-    const FORMAT = 'Y-m-d\TH:i:s.u\Z';
-    const FORMAT_INTERPOLATE = 'Y-m-d\TH:i:s.%\s\Z';
-
-    /**
-     * @var \DateTimeInterface
-     */
-    private $value;
-
-    /**
-     * @var int
-     */
-    private $nanoSeconds;
-
-    /**
-     * @param \DateTimeInterface $value The timestamp value.
-     * @param int $nanoSeconds [optional] The number of nanoseconds in the timestamp.
-     */
-    public function __construct(\DateTimeInterface $value, $nanoSeconds = null)
-    {
-        $this->value = $value;
-        $this->nanoSeconds = $nanoSeconds ?: (int) $this->value->format('u');
-    }
-
-    /**
-     * Get the underlying `\DateTimeInterface` implementation.
-     *
-     * Please note that nanosecond precision is not present in this method.
-     *
-     * Example:
-     * ```
-     * $dateTime = $timestamp->get();
-     * ```
-     *
-     * @return \DateTimeInterface
-     */
-    public function get()
-    {
-        return $this->value;
-    }
-
     /**
      * Get the type.
      *
@@ -95,35 +57,5 @@ class Timestamp implements ValueInterface
     public function type()
     {
         return Database::TYPE_TIMESTAMP;
-    }
-
-    /**
-     * Format the value as a string.
-     *
-     * This method retains nanosecond precision, if available.
-     *
-     * Example:
-     * ```
-     * $value = $timestamp->formatAsString();
-     * ```
-     *
-     * @return string
-     */
-    public function formatAsString()
-    {
-        $this->value->setTimezone(new \DateTimeZone('UTC'));
-        $ns = str_pad((string) $this->nanoSeconds, 6, '0', STR_PAD_LEFT);
-        return sprintf($this->value->format(self::FORMAT_INTERPOLATE), $ns);
-    }
-
-    /**
-     * Format the value as a string.
-     *
-     * @return string
-     * @access private
-     */
-    public function __toString()
-    {
-        return $this->formatAsString();
     }
 }
