e775c312865f4ae1e1c6d64dd8e2cd7336aaa60c
Dave Supplee <dwsupplee@gmail.com>
2017-11-10 20:33:36 -0500

    Prepare v0.43.0

--- src/Logging/LoggingClient.php
+++ src/Logging/LoggingClient.php
@@ -59,7 +59,7 @@ class LoggingClient
     use ArrayTrait;
     use ClientTrait;
 
-    const VERSION = '1.6.0';
+    const VERSION = '1.6.1';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/logging.admin';
     const READ_ONLY_SCOPE = 'https://www.googleapis.com/auth/logging.read';
--- src/Translate/TranslateClient.php
+++ src/Translate/TranslateClient.php
@@ -51,7 +51,7 @@ class TranslateClient
 {
     use ClientTrait;
 
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2';
 
     const ENGLISH_LANGUAGE_CODE = 'en';
 
