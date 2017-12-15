e3f52a201d41df6ecec2d5adb31c07a15a1242d5
John Pedrie <john@pedrie.com>
2017-12-15 11:19:09 -0500

    Prepare v0.47.0

--- src/Datastore/DatastoreClient.php
+++ src/Datastore/DatastoreClient.php
@@ -76,7 +76,7 @@ class DatastoreClient
     use ClientTrait;
     use DatastoreTrait;
 
-    const VERSION = '1.1.0';
+    const VERSION = '1.2.0';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/datastore';
 
