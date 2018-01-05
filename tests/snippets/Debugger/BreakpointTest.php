d929d42bca1cbcf02c7bc7090095bfd748c497f4
Michael Bausor <michaelbausor@google.com>
2018-01-05 13:04:59 -0800

    Fix snippet tests

--- tests/snippets/Debugger/BreakpointTest.php
+++ tests/snippets/Debugger/BreakpointTest.php
@@ -17,7 +17,7 @@
 
 namespace Google\Cloud\Tests\Snippets\Debugger;
 
-use Google\Cloud\Dev\Snippet\SnippetTestCase;
+use Google\Cloud\Core\Testing\Snippet\SnippetTestCase;
 use Google\Cloud\Debugger\Breakpoint;
 use Google\Cloud\Debugger\SourceLocation;
 use Google\Cloud\Debugger\StackFrame;
