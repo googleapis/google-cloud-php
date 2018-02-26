7136c7e12596a3461840cac916937e24462b2ad1
David Supplee <dwsupplee@gmail.com>
2018-02-26 15:57:20 -0500

    Prepare v0.56.0 (#918)

--- src/Language/LanguageClient.php
+++ src/Language/LanguageClient.php
@@ -43,7 +43,7 @@ class LanguageClient
     use ClientTrait;
     use RetryDeciderTrait;
 
-    const VERSION = '0.12.1';
+    const VERSION = '0.12.2';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/cloud-platform';
 
