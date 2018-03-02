376d9db6721402fbbff69902dbfb9f5815dbfe17
Michael Bausor <michaelbausor@google.com>
2018-03-02 09:36:03 -0800

    Update bootstrap files

--- Dataproc/tests/Snippet/bootstrap.php
+++ Dataproc/tests/Snippet/bootstrap.php
@@ -2,7 +2,7 @@
 
 // Provide a project ID. If you're mocking your service calls (and if you aren't
 // start now) you don't need anything else.
-putenv('GOOGLE_APPLICATION_CREDENTIALS='. __DIR__ . '/../../vendor/google/cloud-core/src/Testing/Snippet/keyfile-stub.json');
+putenv('GOOGLE_APPLICATION_CREDENTIALS='. \Google\Cloud\Core\Testing\Snippet\Fixtures::KEYFILE_STUB_FIXTURE());
 
 use Google\Cloud\Core\Testing\Snippet\Container;
 use Google\Cloud\Core\Testing\Snippet\Coverage\Coverage;
