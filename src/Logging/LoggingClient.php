27d3a0bcfbecef2df5c506cc7c2de63f041831a1
Dave Supplee <dwsupplee@gmail.com>
2018-03-16 13:45:47 -0400

    Prepare v0.57.0

--- src/Logging/LoggingClient.php
+++ src/Logging/LoggingClient.php
@@ -68,7 +68,7 @@ class LoggingClient
     use ArrayTrait;
     use ClientTrait;
 
-    const VERSION = '1.9.3';
+    const VERSION = '1.9.4';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/logging.admin';
     const READ_ONLY_SCOPE = 'https://www.googleapis.com/auth/logging.read';
