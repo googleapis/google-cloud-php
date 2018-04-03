5e9ecb6a1df8a4f966f0f9238317a72df6020a7b
michaelbausor <michaelbausor@google.com>
2018-04-03 11:07:00 -0700

    Re-add Iot generated client (#983)
    * Add iot generated client
    
    * Bump gax version to 0.31.1

---
+++ Iot/src/V1/resources/device_manager_descriptor_config.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.iot.v1.DeviceManager' => [
+            'ListDeviceRegistries' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getDeviceRegistries',
+                ],
+            ],
+            'ListDevices' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getDevices',
+                ],
+            ],
+        ],
+    ],
+];
