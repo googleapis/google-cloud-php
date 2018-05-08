a5b5886246b5b3d72e1c08b4a6e48d085045b0b2
michaelbausor <michaelbausor@google.com>
2018-05-08 07:46:32 -0700

    Add Cloud Redis generated client (#1046)
    * Add Redis
    
    * Bump proto-client to 0.38.0
    
    * Add README entry, fix composer.json
    
    * Add comment to README
    
    * Trigger CI
    
    * Update Redis to require gRPC
    
    * Add Redis system test

---
+++ Redis/src/V1beta1/resources/cloud_redis_descriptor_config.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.redis.v1beta1.CloudRedis' => [
+            'CreateInstance' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Cloud\Redis\V1beta1\Instance',
+                    'initialPollDelayMillis' => '60000',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '360000',
+                    'totalPollTimeoutMillis' => '1200000',
+                ],
+            ],
+            'UpdateInstance' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Cloud\Redis\V1beta1\Instance',
+                    'initialPollDelayMillis' => '60000',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '360000',
+                    'totalPollTimeoutMillis' => '1200000',
+                ],
+            ],
+            'DeleteInstance' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Protobuf\GPBEmpty',
+                    'initialPollDelayMillis' => '60000',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '360000',
+                    'totalPollTimeoutMillis' => '1200000',
+                ],
+            ],
+            'ListInstances' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getInstances',
+                ],
+            ],
+        ],
+    ],
+];
