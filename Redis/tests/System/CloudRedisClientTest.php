91960c1371b94db3630549dc008f5e5e92a60abf
Michael Bausor <michaelbausor@google.com>
2018-05-16 14:21:43 -0700

    Update redis and composer

--- Redis/tests/System/CloudRedisClientTest.php
+++ Redis/tests/System/CloudRedisClientTest.php
@@ -53,13 +53,8 @@ class CloudRedisClientTest extends TestCase
         $keyFilePath = getenv('GOOGLE_CLOUD_PHP_TESTS_KEY_PATH');
         $keyFileData = json_decode(file_get_contents($keyFilePath), true);
 
-        $credentialsLoader = CredentialsLoader::makeCredentials(
-            ['https://www.googleapis.com/auth/cloud-platform'],
-            $keyFileData
-        );
-
         self::$grpcClient = new CloudRedisClient([
-            'credentialsLoader' => $credentialsLoader,
+            'credentials' => $keyFilePath,
             'transport' => 'grpc'
         ]);
 
