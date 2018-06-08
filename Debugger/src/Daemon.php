b262e56d58efefdbd10b3b5dff842689faf441ce
John Pedrie <john@pedrie.com>
2018-06-08 14:58:58 -0400

    Use `php_uname`

--- Debugger/src/Daemon.php
+++ Debugger/src/Daemon.php
@@ -256,7 +256,7 @@ class Daemon
         if (isset($_SERVER['GAE_SERVICE'])) {
             return $_SERVER['GAE_SERVICE'] . ' - ' . $_SERVER['GAE_VERSION'];
         }
-        return gethostname() . ' - ' . getcwd();
+        return php_uname('n') . ' - ' . getcwd();
     }
 
     private function defaultSourceContext()
