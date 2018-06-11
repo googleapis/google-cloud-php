1f08492d81569f553ab3ab7fc449a04cae15f9bb
Michael Bausor <michaelbausor@google.com>
2018-06-11 15:58:14 -0700

    Update bootstrap files

--- Debugger/tests/System/bootstrap.php
+++ Debugger/tests/System/bootstrap.php
@@ -3,3 +3,4 @@
 use Google\Cloud\Core\Testing\TestHelpers;
 
 TestHelpers::requireKeyfile('GOOGLE_CLOUD_PHP_TESTS_KEY_PATH');
+TestHelpers::generatedSystemTestBootstrap();
--- Firestore/tests/System/bootstrap.php
+++ Firestore/tests/System/bootstrap.php
@@ -2,4 +2,5 @@
 
 use Google\Cloud\Core\Testing\TestHelpers;
 
-TestHelpers::requireKeyfile('GOOGLE_CLOUD_PHP_FIRESTORE_TESTS_KEY_PATH');
+TestHelpers::requireKeyfile('GOOGLE_CLOUD_PHP_TESTS_KEY_PATH');
+TestHelpers::generatedSystemTestBootstrap();
