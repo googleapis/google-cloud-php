f2660524970aaf07f04428dcbdbe723db1ae0198
Michael Bausor <michaelbausor@google.com>
2018-06-12 08:44:14 -0700

    Remove script to bump proto-client

--- dev/sh/bump-proto.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find . -maxdepth 2 -name composer.json -not -path "./vendor/*" -exec sed -i "s/google\/proto-client[\"']: [\"']\^0\.[0-9][0-9]\(\.[0-9]\)\?[\"']/google\/proto-client\": \"\^$1\"/" "{}" \;
