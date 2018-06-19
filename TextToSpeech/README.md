00b5b3e102b4326bb3f616f223b98d74d2322e72
Takashi Matsuo <tmatsuo@google.com>
2018-06-19 13:28:47 -0700

    Adding tts client lib

---
+++ TextToSpeech/README.md
@@ -0,0 +1,66 @@
+# Cloud Text-to-Speech for PHP
+
+> Idiomatic PHP client for [Cloud Text-to-Speech](https://cloud.google.com/text-to-speech).
+
+[![Latest Stable Version](https://poser.pugx.org/google/cloud-text-to-speech/v/stable)](https://packagist.org/packages/google/cloud-text-to-speech) [![Packagist](https://img.shields.io/packagist/dm/google/cloud-text-to-speech.svg)](https://packagist.org/packages/google/cloud-text-to-speech)
+
+* [API documentation](http://googlecloudplatform.github.io/google-cloud-php/#/docs/cloud-text-to-speech/latest/texttospeech/readme)
+
+**NOTE:** This repository is part of [Google Cloud PHP](https://github.com/googlecloudplatform/google-cloud-php). Any
+support requests, bug reports, or development contributions should be directed to
+that project.
+
+### Installation
+
+To begin, install the preferred dependency manager for PHP, [Composer](https://getcomposer.org/).
+
+Now to install just this component:
+
+```sh
+$ composer require google/cloud-text-to-speech
+```
+
+Or to install the entire suite of components at once:
+
+```sh
+$ composer require google/cloud
+```
+
+
+### Authentication
+
+Please see our [Authentication guide](https://github.com/GoogleCloudPlatform/google-cloud-php/blob/master/AUTHENTICATION.md) for more information
+on authenticating your client. Once authenticated, you'll be ready to start making requests.
+
+### Sample
+
+```php
+require __DIR__ . '/vendor/autoload.php';
+
+use Google\Cloud\TextToSpeech\V1\AudioConfig;
+use Google\Cloud\TextToSpeech\V1\AudioEncoding;
+use Google\Cloud\TextToSpeech\V1\SynthesisInput;
+use Google\Cloud\TextToSpeech\V1\TextToSpeechClient;
+use Google\Cloud\TextToSpeech\V1\VoiceSelectionParams;
+
+
+$textToSpeechClient = new TextToSpeechClient();
+
+$input = new SynthesisInput();
+$input->setText('Japan national team won against Colombia!');
+$voice = new VoiceSelectionParams();
+$voice->setLanguageCode('en-US');
+$audioConfig = new AudioConfig();
+$audioConfig->setAudioEncoding(AudioEncoding::MP3);
+
+$resp = $textToSpeechClient->synthesizeSpeech($input, $voice, $audioConfig);
+TODO: complete this sample
+```
+
+### Version
+
+This component is considered alpha. As such, it is still a work-in-progress and is more likely to get backwards-incompatible updates.
+
+### Next Steps
+
+1. Understand the [official documentation](https://cloud.google.com/text-to-speech/docs).
---
+++ TextToSpeech/src/V1/TextToSpeechGrpcClient.php
@@ -0,0 +1,65 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+// Original file comments:
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+namespace Google\Cloud\TextToSpeech\V1;
+
+/**
+ * Service that implements Google Cloud Text-to-Speech API.
+ */
+class TextToSpeechGrpcClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * Returns a list of [Voice][google.cloud.texttospeech.v1.Voice]
+     * supported for synthesis.
+     * @param \Google\Cloud\TextToSpeech\V1\ListVoicesRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function ListVoices(\Google\Cloud\TextToSpeech\V1\ListVoicesRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/google.cloud.texttospeech.v1.TextToSpeech/ListVoices',
+        $argument,
+        ['\Google\Cloud\TextToSpeech\V1\ListVoicesResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * Synthesizes speech synchronously: receive results after all text input
+     * has been processed.
+     * @param \Google\Cloud\TextToSpeech\V1\SynthesizeSpeechRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function SynthesizeSpeech(\Google\Cloud\TextToSpeech\V1\SynthesizeSpeechRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/google.cloud.texttospeech.v1.TextToSpeech/SynthesizeSpeech',
+        $argument,
+        ['\Google\Cloud\TextToSpeech\V1\SynthesizeSpeechResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
