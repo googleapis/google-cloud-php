5e4e08f59a2015778c9bc998e2053ca134ab1ddf
Takashi Matsuo <tmatsuo@google.com>
2018-06-21 13:54:27 -0700

    Added system test for tts

---
+++ TextToSpeech/tests/System/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Google\Cloud\Core\Testing\TestHelpers;
+
+TestHelpers::requireKeyfile('GOOGLE_CLOUD_PHP_TESTS_KEY_PATH');
+TestHelpers::generatedSystemTestBootstrap();
+
