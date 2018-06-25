16cd52790d159f15a756a349328625de48f36ec1
Jeff Ching <chingor@google.com>
2018-06-25 13:51:11 -0700

    Add Iot synth script

--- Iot/src/V1/Gapic/DeviceManagerGapicClient.php
+++ Iot/src/V1/Gapic/DeviceManagerGapicClient.php
@@ -21,10 +21,6 @@
  * https://github.com/google/googleapis/blob/master/google/cloud/iot/v1/device_manager.proto
  * and updates to that file get reflected here through a refresh process.
  *
- * EXPERIMENTAL: This client library class has not yet been declared GA (1.0). This means that
- * even though we intend the surface to be stable, we may make backwards incompatible changes
- * if necessary.
- *
  * @experimental
  */
 
@@ -70,10 +66,6 @@ use Google\Protobuf\GPBEmpty;
 /**
  * Service Description: Internet of things (IoT) service. Allows to manipulate device registry
  * instances and the registration of devices (Things) to the cloud.
- *
- * EXPERIMENTAL: This client library class has not yet been declared GA (1.0). This means that
- * even though we intend the surface to be stable, we may make backwards incompatible changes
- * if necessary.
  *
  * This class provides the ability to make remote calls to the backing service through method
  * calls that map to API methods. Sample code to get started:
