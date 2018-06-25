c4e1b56924ce9d5a2c9e3009e81ab84c2a7dbb25
Jeff Ching <chingor@google.com>
2018-06-25 15:00:40 -0700

    Add VideoIntelligence synth script

--- VideoIntelligence/src/V1/Gapic/VideoIntelligenceServiceGapicClient.php
+++ VideoIntelligence/src/V1/Gapic/VideoIntelligenceServiceGapicClient.php
@@ -20,6 +20,8 @@
  * This file was generated from the file
  * https://github.com/google/googleapis/blob/master/google/cloud/videointelligence/v1/video_intelligence.proto
  * and updates to that file get reflected here through a refresh process.
+ *
+ * @experimental
  */
 
 namespace Google\Cloud\VideoIntelligence\V1\Gapic;
@@ -82,6 +84,8 @@ use Google\LongRunning\Operation;
  *     $videoIntelligenceServiceClient->close();
  * }
  * ```
+ *
+ * @experimental
  */
 class VideoIntelligenceServiceGapicClient
 {
@@ -138,6 +142,7 @@ class VideoIntelligenceServiceGapicClient
      * Return an OperationsClient object with the same endpoint as $this.
      *
      * @return OperationsClient
+     * @experimental
      */
     public function getOperationsClient()
     {
@@ -155,6 +160,7 @@ class VideoIntelligenceServiceGapicClient
      * @param string $methodName    The name of the method used to start the operation
      *
      * @return OperationResponse
+     * @experimental
      */
     public function resumeOperation($operationName, $methodName = null)
     {
@@ -217,6 +223,7 @@ class VideoIntelligenceServiceGapicClient
      * }
      *
      * @throws ValidationException
+     * @experimental
      */
     public function __construct(array $options = [])
     {
@@ -313,6 +320,7 @@ class VideoIntelligenceServiceGapicClient
      * @return \Google\ApiCore\OperationResponse
      *
      * @throws ApiException if the remote call fails
+     * @experimental
      */
     public function annotateVideo(array $optionalArgs = [])
     {
---
+++ VideoIntelligence/synth.py
@@ -0,0 +1,53 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICGenerator()
+common = gcp.CommonTemplates()
+
+for version in ['V1', 'V1beta2']:
+    lower_version = version.lower()
+
+    library = gapic.php_library(
+        service='videointelligence',
+        version=lower_version,
+        artman_output_name=f'google-cloud-video-intelligence-{lower_version}')
+
+    # copy all src except partial veneer classes
+    s.move(library / f'src/{version}/Gapic')
+    s.move(library / f'src/{version}/resources')
+
+    # copy proto files to src also
+    s.move(library / f'proto/src/Google/Cloud/VideoIntelligence', f'src/')
+    s.move(library / f'tests/')
+
+    # copy GPBMetadata file to metadata
+    s.move(library / f'proto/src/GPBMetadata/Google/Cloud/Videointelligence', f'metadata/')
+
+# fix year
+s.replace(
+    '**/Gapic/*GapicClient.php',
+    r'Copyright \d{4}',
+    r'Copyright 2017')
+s.replace(
+    'tests/**/V1*/*Test.php',
+    r'Copyright \d{4}',
+    r'Copyright 2018')
