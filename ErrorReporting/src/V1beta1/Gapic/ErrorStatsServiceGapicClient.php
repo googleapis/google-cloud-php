1dcf405639e7d96e8f2963b34a70df64975e60a2
Jeff Ching <chingor@google.com>
2018-06-25 13:44:26 -0700

    Add ErrorReporting synth script

--- ErrorReporting/src/V1beta1/Gapic/ErrorStatsServiceGapicClient.php
+++ ErrorReporting/src/V1beta1/Gapic/ErrorStatsServiceGapicClient.php
@@ -21,10 +21,6 @@
  * https://github.com/google/googleapis/blob/master/google/devtools/clouderrorreporting/v1beta1/error_stats_service.proto
  * and updates to that file get reflected here through a refresh process.
  *
- * EXPERIMENTAL: This client library class has not yet been declared GA (1.0). This means that
- * even though we intend the surface to be stable, we may make backwards incompatible changes
- * if necessary.
- *
  * @experimental
  */
 
@@ -52,10 +48,6 @@ use Google\Protobuf\Timestamp;
 /**
  * Service Description: An API for retrieving and managing error statistics as well as data for
  * individual events.
- *
- * EXPERIMENTAL: This client library class has not yet been declared GA (1.0). This means that
- * even though we intend the surface to be stable, we may make backwards incompatible changes
- * if necessary.
  *
  * This class provides the ability to make remote calls to the backing service through method
  * calls that map to API methods. Sample code to get started:
