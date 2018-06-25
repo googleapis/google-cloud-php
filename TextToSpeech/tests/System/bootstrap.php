f4200b36fdb3496d74297992d738f5c9012368a2
Takashi Matsuo <tmatsuo@google.com>
2018-06-25 14:28:00 -0700

    Only require the autoload if it exists

--- TextToSpeech/tests/System/bootstrap.php
+++ TextToSpeech/tests/System/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../vendor/autoload.php';
+}
 
 use Google\Cloud\Core\Testing\TestHelpers;
 
