c278330133eabd499973d7b0e0ad62965c095945
Takashi Matsuo <tmatsuo@google.com>
2018-06-26 10:32:44 -0700

    Add system test for V1

--- OsLogin/tests/System/V1beta/OsLoginServiceClientTest.php
+++ OsLogin/tests/System/V1beta/OsLoginServiceClientTest.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-namespace Google\Cloud\Container\Tests\System;
+namespace Google\Cloud\OsLogin\V1beta\Tests\System;
 
 use Google\Auth\CredentialsLoader;
 use Google\Cloud\OsLogin\V1beta\OsLoginServiceClient;
