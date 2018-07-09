13d14103acfa8ada0c2704e46f1bbf0232588530
Rob Loach <robloach@gmail.com>
2018-07-09 18:27:41 -0400

    Add isRacy() to SafeSearch
    The SafeSearch class included `is` functions for each SafeSearch feature (adult, spoof, medical, violence), but was missing Racy. This Pull Request adds `isRacy()` to the SafeSearch class.

--- Vision/src/Annotation/SafeSearch.php
+++ Vision/src/Annotation/SafeSearch.php
@@ -76,6 +76,16 @@ use Google\Cloud\Core\CallTrait;
  *
  *     @return string
  * }
+ * @method racy() {
+ *     Racy likelihood.
+ *
+ *     Example:
+ *     ```
+ *     echo $safeSearch->racy();
+ *     ```
+ *
+ *     @return string
+ * }
  * @method info() {
  *     Get the raw annotation result
  *
@@ -190,5 +200,26 @@ class SafeSearch extends AbstractFeature
     public function isViolent($strength = self::STRENGTH_LOW)
     {
         return $this->likelihood($this->info['violence'], $strength);
+    }
+
+    /**
+     * Check whether the image contains racy content
+     *
+     * Example:
+     * ```
+     * if ($safeSearch->isRacy()) {
+     *     echo "Image contains racy content.";
+     * }
+     * ```
+     *
+     * @param  string $strength [optional] Value should be one of "low",
+     *         "medium" or "high". Recommended usage is via `Face::STRENGTH_*`
+     *         constants. Higher strength will result in fewer `true` results,
+     *         but fewer false positives. **Defaults to** `"low"`.
+     * @return bool
+     */
+    public function isRacy($strength = self::STRENGTH_LOW)
+    {
+        return $this->likelihood($this->info['racy'], $strength);
     }
 }
