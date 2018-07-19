42e0b9f6a7d8da2e2a044bf776d0dd33cd7d73c1
Takashi Matsuo <tmatsuo@google.com>
2018-07-19 10:51:07 -0700

    Prepare v0.72.0 (#1181)

--- PubSub/src/PubSubClient.php
+++ PubSub/src/PubSubClient.php
@@ -85,7 +85,7 @@ class PubSubClient
     use IncomingMessageTrait;
     use ResourceNameTrait;
 
-    const VERSION = '1.4.0';
+    const VERSION = '1.4.1';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/pubsub';
 
