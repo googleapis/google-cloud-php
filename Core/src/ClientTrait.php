e57419c07cb7f46953e95094a41ec63226ac6483
John Pedrie <jdpedrie@users.noreply.github.com>
2018-08-14 10:28:20 -0400

    Trigger notice if keyfile is missing a project ID (#803)
    Closes #171.

--- Core/src/ClientTrait.php
+++ Core/src/ClientTrait.php
@@ -184,8 +184,27 @@ trait ClientTrait
             return $config['projectId'];
         }
 
-        if (isset($config['keyFile']['project_id'])) {
-            return $config['keyFile']['project_id'];
+        if (isset($config['keyFile'])) {
+            if (isset($config['keyFile']['project_id'])) {
+                return $config['keyFile']['project_id'];
+            }
+
+            if (!isset($config['suppressKeyFileNotice']) && $config['suppressKeyFileNotice'] !== true) {
+                $serviceAccountUri = 'https://cloud.google.com/iam/docs/' .
+                    'creating-managing-service-account-keys#creating_service_account_keys';
+
+                trigger_error(
+                    sprintf(
+                        'A keyfile was given, but it does not contain a project ' .
+                        'ID. This can indicate an old and obsolete keyfile, ' .
+                        'in which case you should create a new one. To suppress ' .
+                        'this message, set `suppressKeyFileNotice` to `true` in your client configuration. ' .
+                        'To learn more about generating new keys, see this URL: %s',
+                        $serviceAccountUri
+                    ),
+                    E_USER_NOTICE
+                );
+            }
         }
 
         if (getenv('GOOGLE_CLOUD_PROJECT')) {
