c65c79d3870518e81f07805bd05c4b0316a9af68
ajaaym <34161822+ajaaym@users.noreply.github.com>
2018-08-17 11:04:34 -0400

    Bigtable data operation mutate rows (#1230)
    * mutate rows
    
    * Fixed type in comment

---
+++ Bigtable/src/DataClient.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * Copyright 2018, Google LLC All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Bigtable;
+
+use Google\ApiCore\ApiException;
+use Google\Cloud\Bigtable\Exception\BigtableDataOperationException;
+use Google\Cloud\Bigtable\V2\BigtableClient as TableClient;
+use Google\Cloud\Core\ClientTrait;
+use Google\Rpc\Code;
+
+/**
+ * Represents a DataOperation Client to perform data operation on Bigtable table.
+ * This is used to perform insert,update, delete operation on table in Bigtable.
+ *
+ * Example:
+ * ```
+ * use Google\Cloud\Bigtable\DataClient;
+ *
+ * $dataClient = new DataClient('my-instance', 'my-table');
+ * ```
+ *
+ */
+class DataClient
+{
+    use ClientTrait;
+
+    /**
+     * @var string
+     */
+    private $instanceId;
+
+    /**
+     * @var string
+     */
+    private $tableId;
+
+    /**
+     * @var TableClient
+     */
+    private $bigtableClient;
+
+    /**
+     * @var array
+     */
+    private $options;
+
+    /**
+     * @var string
+     */
+    private $tableName;
+
+    /**
+     * Create a Bigtable data client.
+     *
+     * @param string $instanceId The instance id.
+     * @param string $tableId The table id on which operation to be performed.
+     * @param array $config [optional] {
+     *     Configuration Options.
+     *
+     *      @type string $appProfileId The appProfileId to be used.
+     *      @type array $headers The headers to be passed to request.
+     *      @type TableClient $bigtableClient The GAPIC Bigtable client to use.
+     *            If not provided it will create one.
+     *     @type array $keyFile The contents of the service account credentials
+     *           .json file retrieved from the Google Developer's Console.
+     *           Ex: `json_decode(file_get_contents($path), true)`.
+     *     @type string $keyFilePath The full path to your service account
+     *           credentials .json file retrieved from the Google Developers
+     *           Console.
+     * }
+     */
+    public function __construct($instanceId, $tableId, array $config = [])
+    {
+        $this->instanceId = $instanceId;
+        $this->tableId = $tableId;
+        $this->options = [];
+        if (isset($config['appProfileId'])) {
+            $this->options['appProfileId'] = $config['appProfileId'];
+        }
+        if (isset($config['headers'])) {
+            $this->options['headers'] = $config['headers'];
+        }
+        $config = $this->configureAuthentication($config);
+        if (isset($config['bigtableClient'])) {
+            $this->bigtableClient = $config['bigtableClient'];
+        } else {
+            $this->bigtableClient = new TableClient($config);
+        }
+        $this->tableName = TableClient::tableName($this->projectId, $instanceId, $tableId);
+    }
+
+    /**
+     * Mutates rows in a table.
+     *
+     * Example:
+     * ```
+     * use Google\Cloud\Bigtable\DataClient;
+     * use Google\Cloud\Bigtable\RowMutation;
+     *
+     * $rowMutation = new RowMutation('r1');
+     * $rowMutation->upsert('cf1','cq1','value1',1534183334215000);
+     *
+     * $dataClient->mutateRows([$rowMutation]);
+     * ```
+     * @param array $rowMutations array of RowMutation object.
+     * @return void
+     * @throws ApiException|BigtableDataOperationException if the remote call fails or operation fails
+     */
+    public function mutateRows(array $rowMutations)
+    {
+        $entries = [];
+        foreach ($rowMutations as $rowMutation) {
+            $entries[] = $rowMutation->getEntry();
+        }
+        $responseStream = $this->bigtableClient->mutateRows($this->tableName, $entries, $this->options);
+        $rowMutationsFailedResponse = [];
+        $failureCode = Code::OK;
+        $message = 'partial failure';
+        foreach ($responseStream->readAll() as $mutateRowsResponse) {
+            $mutateRowsResponseEntries = $mutateRowsResponse->getEntries();
+            foreach ($mutateRowsResponseEntries as $mutateRowsResponseEntry) {
+                if ($mutateRowsResponseEntry->getStatus()->getCode() !== Code::OK) {
+                    $failureCode = $mutateRowsResponseEntry->getStatus()->getCode();
+                    $message = $mutateRowsResponseEntry->getStatus()->getMessage();
+                    $rowMutationsFailedResponse[] = [
+                        'rowKey' => $rowMutations[$mutateRowsResponseEntry->getIndex()]->getRowKey(),
+                        'rowMutationIndex' => $mutateRowsResponseEntry->getIndex(),
+                        'statusCode' => $failureCode,
+                        'message' => $message
+                    ];
+                }
+            }
+        }
+        if (!empty($rowMutationsFailedResponse)) {
+            throw new BigtableDataOperationException($message, $failureCode, $rowMutationsFailedResponse);
+        }
+    }
+
+    /**
+     * Insert/update rows in a table.
+     *
+     * Example:
+     * ```
+     * use Google\Cloud\Bigtable\DataClient;
+     *
+     * $dataClient->upsert(['r1' => ['cf1' => ['cq1' => ['value'=>'value1', 'timeStamp' => 1534183334215000]]]]);
+     * ```
+     * @param array[] $rows array of row.
+     * @return void
+     * @throws ApiException|BigtableDataOperationException if the remote call fails or operation fails
+     */
+    public function upsert(array $rows)
+    {
+        $rowMutations = [];
+        foreach ($rows as $rowKey => $families) {
+            $rowMutation = new RowMutation($rowKey);
+            foreach ($families as $family => $qualifiers) {
+                foreach ($qualifiers as $qualifier => $value) {
+                    if (isset($value['timeStamp'])) {
+                        $rowMutation->upsert(
+                            $family,
+                            $qualifier,
+                            $value['value'],
+                            $value['timeStamp']
+                        );
+                    } else {
+                        $rowMutation->upsert($family, $qualifier, $value['value']);
+                    }
+                }
+            }
+            $rowMutations[] = $rowMutation;
+        }
+        $this->mutateRows($rowMutations);
+    }
+}
---
+++ Bigtable/src/RowMutation.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Copyright 2018, Google LLC All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Bigtable;
+
+use Google\Cloud\Bigtable\V2\MutateRowsRequest\Entry;
+use Google\Cloud\Bigtable\V2\Mutation;
+use Google\Cloud\Bigtable\V2\Mutation\DeleteFromColumn;
+use Google\Cloud\Bigtable\V2\Mutation\DeleteFromFamily;
+use Google\Cloud\Bigtable\V2\Mutation\DeleteFromRow;
+use Google\Cloud\Bigtable\V2\Mutation\SetCell;
+use Google\Cloud\Bigtable\V2\TimestampRange;
+
+/**
+ * Represents a RowMutation to perform data operation on Bigtable table.
+ * This is used to insert,update, delete operation on row in Bigtable table.
+ */
+class RowMutation
+{
+
+    /**
+     * @var string
+     */
+    private $rowKey;
+
+    /**
+     * @var array Mutation
+     */
+    private $mutations = [];
+
+    public function __construct($rowKey, array $options = [])
+    {
+        $this->rowKey = $rowKey;
+    }
+
+    /**
+     * Returns the row key.
+     *
+     * @return string RowKey of the Row.
+     */
+    public function getRowKey()
+    {
+        return $this->rowKey;
+    }
+
+    /**
+     * Creates Insert/Update mutation for a row.
+     *
+     * @param string $family Family name of the row.
+     * @param string $qualifier Column qualifier of the row.
+     * @param string $value Value of the Column qualifier.
+     * @param int $timeStamp optional timestamp value.
+     *
+     * @return RowMutation returns current RowMutation object.
+     */
+    public function upsert($family, $qualifier, $value, $timeStamp = null)
+    {
+        $mutation = new Mutation;
+        $mutationSetCell = new SetCell;
+        $mutationSetCell->setFamilyName($family)
+            ->setColumnQualifier($qualifier)
+            ->setValue($value);
+        if ($timeStamp === null) {
+            $mutationSetCell->setTimestampMicros(
+                // gives milli second
+                round(microtime(true) * 1000)
+                // multiply by 1000 to get micro
+                * 1000
+            );
+        } else {
+            $mutationSetCell->setTimestampMicros($timeStamp);
+        }
+        $mutation->setSetCell($mutationSetCell);
+        $this->mutations[] = $mutation;
+        return $this;
+    }
+
+    /**
+     * Creates delete from family mutation for a row.
+     *
+     * @param string $family Family name of the row.
+     *
+     * @return RowMutation returns current RowMutation object.
+     */
+    public function deleteFromFamily($family)
+    {
+        $mutation = new Mutation;
+        $deleteFromFamily = new DeleteFromFamily;
+        $deleteFromFamily->setFamilyName($family);
+        $mutation->setDeleteFromFamily($deleteFromFamily);
+        $this->mutations[] = $mutation;
+        return $this;
+    }
+
+    /**
+     * Creates delete from column mutation for a row.
+     *
+     * @param string $family Family name of the row.
+     * @param string $qualifier Column qualifier of the row.
+     * @param array $timeRange optional array of time range to delete from column,
+     *        keyed by `start` and `end` representing time range window.
+     * @return RowMutation returns current RowMutation object.
+     */
+    public function deleteFromColumn($family, $qualifier, array $timeRange = [])
+    {
+        $mutation = new Mutation;
+        $deleteFromColumn = new DeleteFromColumn;
+        $deleteFromColumn->setFamilyName($family)->setColumnQualifier($qualifier);
+        if (!empty($timeRange)) {
+            $timestampRange = new TimestampRange;
+            $timestampRange->setStartTimestampMicros($timeRange['start']);
+            $timestampRange->setEndTimestampMicros($timeRange['end']);
+            $deleteFromColumn->setTimeRange($timestampRange);
+        }
+        $mutation->setDeleteFromColumn($deleteFromColumn);
+        $this->mutations[] = $mutation;
+        return $this;
+    }
+
+    /**
+     * Creates delete row mutation for a row.
+     *
+     * @return RowMutation returns current RowMutation object.
+     */
+    public function deleteRow()
+    {
+        $mutation = new Mutation;
+        $mutation->setDeleteFromRow(new DeleteFromRow);
+        $this->mutations[] = $mutation;
+        return $this;
+    }
+
+    /**
+     * Returns proto representation of RowMutation.
+     *
+     * @return Entry Entry for Row.
+     */
+    public function getEntry()
+    {
+        $mutateRowsRequestEntry = new Entry;
+        $mutateRowsRequestEntry->setRowKey($this->rowKey);
+        $mutateRowsRequestEntry->setMutations($this->mutations);
+        return $mutateRowsRequestEntry;
+    }
+}
