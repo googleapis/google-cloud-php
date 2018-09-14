cf9b78480aeb9359cbbc625087ee68ebb749dc76
Takashi Matsuo <tmatsuo@google.com>
2018-09-13 17:38:18 -0700

    Add --log-junit option and the artifact definition for the junit logs

--- .kokoro/presubmit/run-tests.sh
+++ .kokoro/presubmit/run-tests.sh
@@ -4,5 +4,23 @@ set -ex
 
 pushd github/google-cloud-php
 composer --no-interaction --no-ansi --no-progress update
-dev/sh/tests
+
+SHORT_JOB_NAME=${KOKORO_JOB_NAME##*/}
+UNIT_LOG_FILENAME=${SHORT_JOB_NAME}_unit_sponge_log.xml
+SNIPPETS_LOG_FILENAME=${SHORT_JOB_NAME}_snippets_sponge_log.xml
+
+echo "Running PHPCS Code Style Checker"
+dev/sh/style
+
+echo "Running Unit Test Suite"
+vendor/bin/phpunit --log-junit ${UNIT_LOG_FILENAME}
+
+echo "Running Snippet Test Suite"
+vendor/bin/phpunit -c phpunit-snippets.xml.dist --verbose --log-junit \
+                   ${SNIPPETS_LOG_FILENAME}
+
+# Move the file to the source top directory for better look
+mv ${UNIT_LOG_FILENAME} ../..
+mv ${SNIPPETS_LOG_FILENAME} ../..
+
 popd
