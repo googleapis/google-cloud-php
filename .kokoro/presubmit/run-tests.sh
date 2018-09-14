b4f630d658c8e4c09d1fe508ac8c3686197db801
Takashi Matsuo <tmatsuo@google.com>
2018-09-14 10:52:45 -0700

    Use directories for indicating php version and the test type

--- .kokoro/presubmit/run-tests.sh
+++ .kokoro/presubmit/run-tests.sh
@@ -6,8 +6,12 @@ pushd github/google-cloud-php
 composer --no-interaction --no-ansi --no-progress update
 
 SHORT_JOB_NAME=${KOKORO_JOB_NAME##*/}
-UNIT_LOG_FILENAME=${SHORT_JOB_NAME}_unit_sponge_log.xml
-SNIPPETS_LOG_FILENAME=${SHORT_JOB_NAME}_snippets_sponge_log.xml
+
+mkdir -p ${SHORT_JOB_NAME}/unit
+mkdir -p ${SHORT_JOB_NAME}/snippets
+
+UNIT_LOG_FILENAME=${SHORT_JOB_NAME}/unit/sponge_log.xml
+SNIPPETS_LOG_FILENAME=${SHORT_JOB_NAME}/snippets/sponge_log.xml
 
 echo "Running PHPCS Code Style Checker"
 dev/sh/style
