495f8ad62e2ef5765e71450124c0161454a64409
Takashi Matsuo <tmatsuo@google.com>
2018-10-10 18:22:24 -0700

    Stop using HOME because it's different when running the test

--- .kokoro/presubmit/windows/test.bat
+++ .kokoro/presubmit/windows/test.bat
@@ -1,9 +1,9 @@
 SET SHORT_JOB_NAME=%KOKORO_JOB_NAME:~-5%
 
 MKDIR %SHORT_JOB_NAME%\unit
-RENAME %HOME%\software\%SHORT_JOB_NAME% php
+RENAME C:\Users\kbuilder\software\%SHORT_JOB_NAME% php
 
-CALL php %HOME%\bin\composer update
+CALL php C:\Users\kbuilder\bin\composer update
 CALL vendor/bin/phpunit --log-junit %SHORT_JOB_NAME%\unit\sponge_log.xml
 
-RENAME %HOME%\software\php %SHORT_JOB_NAME%
+RENAME C:\Users\kbuilder\software\php %SHORT_JOB_NAME%
