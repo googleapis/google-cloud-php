d74d15ae172ff3eab5fcbfe99c892c00cb02d969
Takashi Matsuo <tmatsuo@google.com>
2018-10-10 18:27:49 -0700

    Change directory before running the test

--- .kokoro/presubmit/windows/test.bat
+++ .kokoro/presubmit/windows/test.bat
@@ -1,8 +1,9 @@
 SET SHORT_JOB_NAME=%KOKORO_JOB_NAME:~-5%
 
-MKDIR %SHORT_JOB_NAME%\unit
 RENAME C:\Users\kbuilder\software\%SHORT_JOB_NAME% php
 
+CD github/google-cloud-php
+MKDIR %SHORT_JOB_NAME%\unit
 CALL php C:\Users\kbuilder\bin\composer update
 CALL vendor/bin/phpunit --log-junit %SHORT_JOB_NAME%\unit\sponge_log.xml
 
