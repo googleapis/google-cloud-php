7f9d7705ed41c72a887fc43190b3b3a10ac82e1c
Takashi Matsuo <tmatsuo@google.com>
2018-10-12 16:28:15 -0700

    Adding kokoro config for windows (#1339)
    * Adding kokoro config for php72 test on windows
    
    * Get the right job name
    
    * Stop using HOME because it's different when running the test
    
    * Change directory before running the test
    
    * Add windows configs for php versions
    
    * Run self-update on composer

---
+++ .kokoro/presubmit/windows/test.bat
@@ -0,0 +1,11 @@
+SET SHORT_JOB_NAME=%KOKORO_JOB_NAME:~-5%
+
+RENAME C:\Users\kbuilder\software\%SHORT_JOB_NAME% php
+
+CD github/google-cloud-php
+MKDIR %SHORT_JOB_NAME%\unit
+CALL php C:\Users\kbuilder\bin\composer self-update
+CALL php C:\Users\kbuilder\bin\composer update
+CALL vendor/bin/phpunit --log-junit %SHORT_JOB_NAME%\unit\sponge_log.xml
+
+RENAME C:\Users\kbuilder\software\php %SHORT_JOB_NAME%
