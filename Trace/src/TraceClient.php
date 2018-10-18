35758c63503e01f2c3975ff9c1cd8534f512fb17
Takashi Matsuo <tmatsuo@google.com>
2018-10-18 11:46:37 -0700

    Prepare v0.83.0 (#1355)

--- Trace/src/TraceClient.php
+++ Trace/src/TraceClient.php
@@ -44,7 +44,7 @@ class TraceClient
 {
     use ClientTrait;
 
-    const VERSION = '0.10.6';
+    const VERSION = '0.11.0';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/cloud-platform';
 
