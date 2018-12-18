702be4a66d047ff46003c9a88d375052383f1398
yoshi-automation <yoshi-automation@google.com>
2018-12-18 03:04:43 -0800

    [CHANGE ME] Re-generated Trace to pick up changes in the API or client library generator.

--- Trace/src/V2/Gapic/TraceServiceGapicClient.php
+++ Trace/src/V2/Gapic/TraceServiceGapicClient.php
@@ -28,12 +28,12 @@ namespace Google\Cloud\Trace\V2\Gapic;
 
 use Google\ApiCore\ApiException;
 use Google\ApiCore\CredentialsWrapper;
-use Google\ApiCore\FetchAuthTokenInterface;
 use Google\ApiCore\GapicClientTrait;
 use Google\ApiCore\PathTemplate;
 use Google\ApiCore\RetrySettings;
 use Google\ApiCore\Transport\TransportInterface;
 use Google\ApiCore\ValidationException;
+use Google\Auth\FetchAuthTokenInterface;
 use Google\Cloud\Trace\V2\BatchWriteSpansRequest;
 use Google\Cloud\Trace\V2\Span;
 use Google\Cloud\Trace\V2\Span\Attributes;
