8308c1d39ef3f5091d054635c217831f9ef2a80d
Ajay <ajay@qlogic.io>
2018-12-21 17:58:06 -0500

    fix snippet test

--- Bigtable/tests/Snippet/ChunkFormatterTest.php
+++ Bigtable/tests/Snippet/ChunkFormatterTest.php
@@ -59,7 +59,7 @@ class ChunkFormatterTest extends SnippetTestCase
         $snippet->replace('$table = $bigtable->table(\'my-instance\', \'my-table\');', '');
         $this->serverStream->readAll()
             ->shouldBeCalled()
-            ->willReturn((yield));
+            ->willReturn([]);
         $this->bigtableClient->readRows(self::TABLE_NAME, [])
             ->shouldBeCalled()
             ->willReturn(
@@ -77,9 +77,14 @@ class ChunkFormatterTest extends SnippetTestCase
         $formatter = $this->prophesize(ChunkFormatter::class);
         $formatter->readAll()
             ->shouldBeCalled()
-            ->willReturn((yield));
+            ->willReturn($this->resultGenerator());
         $snippet->addLocal('formatter', $formatter->reveal());
         $res = $snippet->invoke('rows');
         $this->assertInstanceOf(\Generator::class, $res->returnVal());
+    }
+
+    private function resultGenerator()
+    {
+        yield [];
     }
 }
