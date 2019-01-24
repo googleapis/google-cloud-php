33b80b47dd4dd22d93ec20ff23d546d05634fe45
Daniel Sims <danielgarthsims@gmail.com>
2019-01-23 23:31:04 -0500

    Removing erroneous deprecated tag from speech helpers trait

--- Speech/src/SpeechHelpersTrait.php
+++ Speech/src/SpeechHelpersTrait.php
@@ -21,9 +21,6 @@ use InvalidArgumentException;
 
 /**
  * Provides helper functions for generated Speech clients.
- * 
- * @deprecated This class is no longer supported and will be removed in a future
- * release.
  */
 trait SpeechHelpersTrait
 {
