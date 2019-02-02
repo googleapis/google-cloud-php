2dafcabe821fa912db783c1f0d585d2ab6f0d453
yoshi-automation <yoshi-automation@google.com>
2019-02-02 03:03:11 -0800

    [CHANGE ME] Re-generated Redis to pick up changes in the API or client library generator.

--- Redis/src/V1/Gapic/CloudRedisGapicClient.php
+++ Redis/src/V1/Gapic/CloudRedisGapicClient.php
@@ -155,7 +155,7 @@ class CloudRedisGapicClient
 
     private static function getLocationNameTemplate()
     {
-        if (self::$locationNameTemplate == null) {
+        if (null == self::$locationNameTemplate) {
             self::$locationNameTemplate = new PathTemplate('projects/{project}/locations/{location}');
         }
 
@@ -164,7 +164,7 @@ class CloudRedisGapicClient
 
     private static function getInstanceNameTemplate()
     {
-        if (self::$instanceNameTemplate == null) {
+        if (null == self::$instanceNameTemplate) {
             self::$instanceNameTemplate = new PathTemplate('projects/{project}/locations/{location}/instances/{instance}');
         }
 
@@ -173,7 +173,7 @@ class CloudRedisGapicClient
 
     private static function getPathTemplateMap()
     {
-        if (self::$pathTemplateMap == null) {
+        if (null == self::$pathTemplateMap) {
             self::$pathTemplateMap = [
                 'location' => self::getLocationNameTemplate(),
                 'instance' => self::getInstanceNameTemplate(),
