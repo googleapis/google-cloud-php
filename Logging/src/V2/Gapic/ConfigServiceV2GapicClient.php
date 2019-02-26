1edc9a9232abff1b6ccbcf5172cd00dca33b71f8
yoshi-automation <yoshi-automation@google.com>
2019-02-26 03:01:13 -0800

    [CHANGE ME] Re-generated Logging to pick up changes in the API or client library generator.

--- Logging/src/V2/Gapic/ConfigServiceV2GapicClient.php
+++ Logging/src/V2/Gapic/ConfigServiceV2GapicClient.php
@@ -511,7 +511,8 @@ class ConfigServiceV2GapicClient
      *          If this field is set to true, or if the sink is owned by a non-project
      *          resource such as an organization, then the value of `writer_identity` will
      *          be a unique service account used only for exports from the new sink.  For
-     *          more information, see `writer_identity` in [LogSink][google.logging.v2.LogSink].
+     *          more information, see `writer_identity` in
+     *          [LogSink][google.logging.v2.LogSink].
      *     @type RetrySettings|array $retrySettings
      *          Retry settings to use for this call. Can be a
      *          {@see Google\ApiCore\RetrySettings} object, or an associative array
@@ -895,8 +896,9 @@ class ConfigServiceV2GapicClient
      *                                 in `update_mask` are relevant.
      * @param FieldMask    $updateMask Required. A nonempty list of fields to change in the existing exclusion.
      *                                 New values for the fields are taken from the corresponding fields in the
-     *                                 [LogExclusion][google.logging.v2.LogExclusion] included in this request. Fields not mentioned in
-     *                                 `update_mask` are not changed and are ignored in the request.
+     *                                 [LogExclusion][google.logging.v2.LogExclusion] included in this request.
+     *                                 Fields not mentioned in `update_mask` are not changed and are ignored in
+     *                                 the request.
      *
      * For example, to change the filter and description of an exclusion,
      * specify an `update_mask` of `"filter,description"`.
