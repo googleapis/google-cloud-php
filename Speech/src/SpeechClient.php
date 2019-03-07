21454e7d897ef28afa6993b6850fdd77a3bc9636
John Pedrie <john@pedrie.com>
2019-03-07 15:14:09 -0500

    Prepare v0.96.0

--- Speech/src/SpeechClient.php
+++ Speech/src/SpeechClient.php
@@ -50,7 +50,7 @@ class SpeechClient
 {
     use ClientTrait;
 
-    const VERSION = '0.21.0';
+    const VERSION = '0.21.1';
 
     const SCOPE = 'https://www.googleapis.com/auth/cloud-platform';
 
--- src/Version.php
+++ src/Version.php
@@ -22,5 +22,5 @@ namespace Google\Cloud;
  */
 class Version
 {
-    const VERSION = '0.95.0';
+    const VERSION = '0.96.0';
 }
