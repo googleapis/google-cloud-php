0c0b81adc1ac3de8a3b25e4ac33e64592afc545e
yoshi-automation <yoshi-automation@google.com>
2019-03-29 02:59:52 -0700

    [CHANGE ME] Re-generated Debugger to pick up changes in the API or client library generator.

--- Debugger/src/V2/StatusMessage/Reference.php
+++ Debugger/src/V2/StatusMessage/Reference.php
@@ -4,6 +4,8 @@
 
 namespace Google\Cloud\Debugger\V2\StatusMessage;
 
+use UnexpectedValueException;
+
 /**
  * Enumerates references to which the message applies.
  *
@@ -53,6 +55,36 @@ class Reference
      * Generated from protobuf enum <code>VARIABLE_VALUE = 6;</code>
      */
     const VARIABLE_VALUE = 6;
+
+    private static $valueToName = [
+        self::UNSPECIFIED => 'UNSPECIFIED',
+        self::BREAKPOINT_SOURCE_LOCATION => 'BREAKPOINT_SOURCE_LOCATION',
+        self::BREAKPOINT_CONDITION => 'BREAKPOINT_CONDITION',
+        self::BREAKPOINT_EXPRESSION => 'BREAKPOINT_EXPRESSION',
+        self::BREAKPOINT_AGE => 'BREAKPOINT_AGE',
+        self::VARIABLE_NAME => 'VARIABLE_NAME',
+        self::VARIABLE_VALUE => 'VARIABLE_VALUE',
+    ];
+
+    public static function name($value)
+    {
+        if (!isset(self::$valueToName[$value])) {
+            throw new UnexpectedValueException(sprintf(
+                    'Enum %s has no name defined for value %s', __CLASS__, $value));
+        }
+        return self::$valueToName[$value];
+    }
+
+
+    public static function value($name)
+    {
+        $const = __CLASS__ . '::' . strtoupper($name);
+        if (!defined($const)) {
+            throw new UnexpectedValueException(sprintf(
+                    'Enum %s has no value defined for name %s', __CLASS__, $name));
+        }
+        return constant($const);
+    }
 }
 
 // Adding a class alias for backwards compatibility with the previous class name.
