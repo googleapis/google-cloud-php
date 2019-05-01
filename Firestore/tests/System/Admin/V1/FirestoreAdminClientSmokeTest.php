6f84b1d3ed5e1555baffaa94ba515ab3e681c56d
David Supplee <dwsupplee@gmail.com>
2019-05-01 11:29:31 -0700

    use the correct option for providing credentials

--- Firestore/tests/System/Admin/V1/FirestoreAdminClientSmokeTest.php
+++ Firestore/tests/System/Admin/V1/FirestoreAdminClientSmokeTest.php
@@ -41,7 +41,7 @@ class FirestoreAdminClientSmokeTest extends SystemTestCase
         }
         $keyFilePath = getenv('GOOGLE_CLOUD_PHP_FIRESTORE_TESTS_KEY_PATH');
         self::$adminClient = new FirestoreAdminClient([
-            'keyFilePath' => $keyFilePath
+            'credentials' => $keyFilePath
         ]);
     }
 
