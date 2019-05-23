67ae638bf00f947a5512e6d600a8ea5ada4e2fee
David Supplee <dwsupplee@gmail.com>
2019-05-23 11:12:19 -0700

    add synth script

---
+++ WebSecurityScanner/synth.py
@@ -0,0 +1,56 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICGenerator()
+common = gcp.CommonTemplates()
+
+library = gapic.php_library(
+    service='websecurityscanner',
+    version='v1beta',
+    config_path=f'/google/cloud/websecurityscanner/artman_websecurityscanner_v1beta.yaml',
+    artman_output_name=f'google-cloud-websecurityscanner-v1beta')
+
+# copy all src
+s.move(library / f'src/V1beta')
+
+# copy proto files to src also
+s.move(library / f'proto/src/Google/Cloud/WebSecurityScanner', f'src/')
+s.move(library / f'tests/')
+
+# copy GPBMetadata file to metadata
+s.move(library / f'proto/src/GPBMetadata/Google/Cloud/Websecurityscanner', f'metadata/')
+
+# fix year
+s.replace(
+    'src/**/**/*.php',
+    r'Copyright \d{4}',
+    r'Copyright 2019')
+s.replace(
+    'tests/**/**/*Test.php',
+    r'Copyright \d{4}',
+    r'Copyright 2019')
+
+# Change the wording for the deprecation warning.
+s.replace(
+    'src/*/*_*.php',
+    r'will be removed in the next major release',
+    'will be removed in a future release')
