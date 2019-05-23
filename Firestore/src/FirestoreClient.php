da356f004ff891e11a369fd634f55d6abade708f
John Pedrie <jdpedrie@users.noreply.github.com>
2019-05-23 17:03:53 -0400

    Prepare v0.102.0 (#1965)

--- Firestore/src/FirestoreClient.php
+++ Firestore/src/FirestoreClient.php
@@ -75,7 +75,7 @@ class FirestoreClient
     use SnapshotTrait;
     use ValidateTrait;
 
-    const VERSION = '1.5.2';
+    const VERSION = '1.6.0';
 
     const DEFAULT_DATABASE = '(default)';
 
--- src/Version.php
+++ src/Version.php
@@ -22,5 +22,5 @@ namespace Google\Cloud;
  */
 class Version
 {
-    const VERSION = '0.101.1';
+    const VERSION = '0.102.0';
 }
