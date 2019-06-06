2e89cce1f62607cec115f604b7f18a860a141910
Brent Shaffer <betterbrent@google.com>
2019-06-06 09:02:01 -0700

    Addresses invalid serialization of timestamps (#2021)
    * Addresses invalid serialization of timestamps
    
    Fixes #2020
    
    * Update Grpc.php

--- Datastore/src/Connection/Grpc.php
+++ Datastore/src/Connection/Grpc.php
@@ -72,6 +72,14 @@ class Grpc implements ConnectionInterface
             'google.protobuf.Timestamp' => function ($v) {
                 return $this->formatTimestampFromApi($v);
             }
+        ], [], [
+            'google.protobuf.Timestamp' => function ($v) {
+                if (is_string($v)) {
+                    $dt = new \DateTime($v);
+                    return ['seconds' => $dt->format('U')];
+                }
+                return $v;
+            }
         ]);
         //@codeCoverageIgnoreEnd
 
