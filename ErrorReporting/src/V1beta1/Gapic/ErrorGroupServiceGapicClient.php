c7f7592ada108ae9777d3a8869e176183993a744
yoshi-automation <yoshi-automation@google.com>
2019-08-07 03:01:27 -0700

    [CHANGE ME] Re-generated ErrorReporting to pick up changes in the API or client library generator.

--- ErrorReporting/src/V1beta1/Gapic/ErrorGroupServiceGapicClient.php
+++ ErrorReporting/src/V1beta1/Gapic/ErrorGroupServiceGapicClient.php
@@ -99,7 +99,7 @@ class ErrorGroupServiceGapicClient
     {
         return [
             'serviceName' => self::SERVICE_NAME,
-            'serviceAddress' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
+            'apiEndpoint' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
             'clientConfig' => __DIR__.'/../resources/error_group_service_client_config.json',
             'descriptorsConfigPath' => __DIR__.'/../resources/error_group_service_descriptor_config.php',
             'gcpApiConfigPath' => __DIR__.'/../resources/error_group_service_grpc_config.json',
@@ -200,6 +200,9 @@ class ErrorGroupServiceGapicClient
      *                       Optional. Options for configuring the service API wrapper.
      *
      *     @type string $serviceAddress
+     *           **Deprecated**. This option will be removed in a future major release. Please
+     *           utilize the `$apiEndpoint` option instead.
+     *     @type string $apiEndpoint
      *           The address of the API remote host. May optionally include the port, formatted
      *           as "<uri>:<port>". Default 'clouderrorreporting.googleapis.com:443'.
      *     @type string|array|FetchAuthTokenInterface|CredentialsWrapper $credentials
@@ -227,7 +230,7 @@ class ErrorGroupServiceGapicClient
      *           or `grpc`. Defaults to `grpc` if gRPC support is detected on the system.
      *           *Advanced usage*: Additionally, it is possible to pass in an already instantiated
      *           {@see \Google\ApiCore\Transport\TransportInterface} object. Note that when this
-     *           object is provided, any settings in $transportConfig, and any $serviceAddress
+     *           object is provided, any settings in $transportConfig, and any `$apiEndpoint`
      *           setting, will be ignored.
      *     @type array $transportConfig
      *           Configuration options that will be used to construct the transport. Options for
