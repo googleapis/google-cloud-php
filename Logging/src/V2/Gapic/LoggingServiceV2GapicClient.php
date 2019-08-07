223a9397b32feafa4561645bd61ceada52cb27f9
yoshi-automation <yoshi-automation@google.com>
2019-08-07 11:02:19 -0700

    [CHANGE ME] Re-generated Logging to pick up changes in the API or client library generator.

--- Logging/src/V2/Gapic/LoggingServiceV2GapicClient.php
+++ Logging/src/V2/Gapic/LoggingServiceV2GapicClient.php
@@ -113,7 +113,7 @@ class LoggingServiceV2GapicClient
     {
         return [
             'serviceName' => self::SERVICE_NAME,
-            'serviceAddress' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
+            'apiEndpoint' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
             'clientConfig' => __DIR__.'/../resources/logging_service_v2_client_config.json',
             'descriptorsConfigPath' => __DIR__.'/../resources/logging_service_v2_descriptor_config.php',
             'gcpApiConfigPath' => __DIR__.'/../resources/logging_service_v2_grpc_config.json',
@@ -241,6 +241,9 @@ class LoggingServiceV2GapicClient
      *                       Optional. Options for configuring the service API wrapper.
      *
      *     @type string $serviceAddress
+     *           **Deprecated**. This option will be removed in a future major release. Please
+     *           utilize the `$apiEndpoint` option instead.
+     *     @type string $apiEndpoint
      *           The address of the API remote host. May optionally include the port, formatted
      *           as "<uri>:<port>". Default 'logging.googleapis.com:443'.
      *     @type string|array|FetchAuthTokenInterface|CredentialsWrapper $credentials
@@ -268,7 +271,7 @@ class LoggingServiceV2GapicClient
      *           or `grpc`. Defaults to `grpc` if gRPC support is detected on the system.
      *           *Advanced usage*: Additionally, it is possible to pass in an already instantiated
      *           {@see \Google\ApiCore\Transport\TransportInterface} object. Note that when this
-     *           object is provided, any settings in $transportConfig, and any $serviceAddress
+     *           object is provided, any settings in $transportConfig, and any `$apiEndpoint`
      *           setting, will be ignored.
      *     @type array $transportConfig
      *           Configuration options that will be used to construct the transport. Options for
