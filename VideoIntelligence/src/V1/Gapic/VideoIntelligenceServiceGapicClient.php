9f0ce2a0916e6c698cf219f0b0f9cd25b89785c0
yoshi-automation <yoshi-automation@google.com>
2019-08-07 11:06:33 -0700

    [CHANGE ME] Re-generated VideoIntelligence to pick up changes in the API or client library generator.

--- VideoIntelligence/src/V1/Gapic/VideoIntelligenceServiceGapicClient.php
+++ VideoIntelligence/src/V1/Gapic/VideoIntelligenceServiceGapicClient.php
@@ -123,7 +123,7 @@ class VideoIntelligenceServiceGapicClient
     {
         return [
             'serviceName' => self::SERVICE_NAME,
-            'serviceAddress' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
+            'apiEndpoint' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
             'clientConfig' => __DIR__.'/../resources/video_intelligence_service_client_config.json',
             'descriptorsConfigPath' => __DIR__.'/../resources/video_intelligence_service_descriptor_config.php',
             'gcpApiConfigPath' => __DIR__.'/../resources/video_intelligence_service_grpc_config.json',
@@ -178,6 +178,9 @@ class VideoIntelligenceServiceGapicClient
      *                       Optional. Options for configuring the service API wrapper.
      *
      *     @type string $serviceAddress
+     *           **Deprecated**. This option will be removed in a future major release. Please
+     *           utilize the `$apiEndpoint` option instead.
+     *     @type string $apiEndpoint
      *           The address of the API remote host. May optionally include the port, formatted
      *           as "<uri>:<port>". Default 'videointelligence.googleapis.com:443'.
      *     @type string|array|FetchAuthTokenInterface|CredentialsWrapper $credentials
@@ -205,7 +208,7 @@ class VideoIntelligenceServiceGapicClient
      *           or `grpc`. Defaults to `grpc` if gRPC support is detected on the system.
      *           *Advanced usage*: Additionally, it is possible to pass in an already instantiated
      *           {@see \Google\ApiCore\Transport\TransportInterface} object. Note that when this
-     *           object is provided, any settings in $transportConfig, and any $serviceAddress
+     *           object is provided, any settings in $transportConfig, and any `$apiEndpoint`
      *           setting, will be ignored.
      *     @type array $transportConfig
      *           Configuration options that will be used to construct the transport. Options for
