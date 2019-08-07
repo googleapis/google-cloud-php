201cb1d17f761a33e8c590ac43e8dab44b8f7130
yoshi-automation <yoshi-automation@google.com>
2019-08-07 03:04:04 -0700

    [CHANGE ME] Re-generated PubSub to pick up changes in the API or client library generator.

--- PubSub/src/V1/Gapic/PublisherGapicClient.php
+++ PubSub/src/V1/Gapic/PublisherGapicClient.php
@@ -120,7 +120,7 @@ class PublisherGapicClient
     {
         return [
             'serviceName' => self::SERVICE_NAME,
-            'serviceAddress' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
+            'apiEndpoint' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
             'clientConfig' => __DIR__.'/../resources/publisher_client_config.json',
             'descriptorsConfigPath' => __DIR__.'/../resources/publisher_descriptor_config.php',
             'gcpApiConfigPath' => __DIR__.'/../resources/publisher_grpc_config.json',
@@ -248,6 +248,9 @@ class PublisherGapicClient
      *                       Optional. Options for configuring the service API wrapper.
      *
      *     @type string $serviceAddress
+     *           **Deprecated**. This option will be removed in a future major release. Please
+     *           utilize the `$apiEndpoint` option instead.
+     *     @type string $apiEndpoint
      *           The address of the API remote host. May optionally include the port, formatted
      *           as "<uri>:<port>". Default 'pubsub.googleapis.com:443'.
      *     @type string|array|FetchAuthTokenInterface|CredentialsWrapper $credentials
@@ -275,7 +278,7 @@ class PublisherGapicClient
      *           or `grpc`. Defaults to `grpc` if gRPC support is detected on the system.
      *           *Advanced usage*: Additionally, it is possible to pass in an already instantiated
      *           {@see \Google\ApiCore\Transport\TransportInterface} object. Note that when this
-     *           object is provided, any settings in $transportConfig, and any $serviceAddress
+     *           object is provided, any settings in $transportConfig, and any `$apiEndpoint`
      *           setting, will be ignored.
      *     @type array $transportConfig
      *           Configuration options that will be used to construct the transport. Options for
