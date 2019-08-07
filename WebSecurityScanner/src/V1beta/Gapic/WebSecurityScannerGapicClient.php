a1c66fe2855f1024dbd06bf2fd9ee7842c4cd6f5
yoshi-automation <yoshi-automation@google.com>
2019-08-07 03:08:10 -0700

    [CHANGE ME] Re-generated WebSecurityScanner to pick up changes in the API or client library generator.

--- WebSecurityScanner/src/V1beta/Gapic/WebSecurityScannerGapicClient.php
+++ WebSecurityScanner/src/V1beta/Gapic/WebSecurityScannerGapicClient.php
@@ -125,7 +125,7 @@ class WebSecurityScannerGapicClient
     {
         return [
             'serviceName' => self::SERVICE_NAME,
-            'serviceAddress' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
+            'apiEndpoint' => self::SERVICE_ADDRESS.':'.self::DEFAULT_SERVICE_PORT,
             'clientConfig' => __DIR__.'/../resources/web_security_scanner_client_config.json',
             'descriptorsConfigPath' => __DIR__.'/../resources/web_security_scanner_descriptor_config.php',
             'gcpApiConfigPath' => __DIR__.'/../resources/web_security_scanner_grpc_config.json',
@@ -317,6 +317,9 @@ class WebSecurityScannerGapicClient
      *                       Optional. Options for configuring the service API wrapper.
      *
      *     @type string $serviceAddress
+     *           **Deprecated**. This option will be removed in a future major release. Please
+     *           utilize the `$apiEndpoint` option instead.
+     *     @type string $apiEndpoint
      *           The address of the API remote host. May optionally include the port, formatted
      *           as "<uri>:<port>". Default 'websecurityscanner.googleapis.com:443'.
      *     @type string|array|FetchAuthTokenInterface|CredentialsWrapper $credentials
@@ -344,7 +347,7 @@ class WebSecurityScannerGapicClient
      *           or `grpc`. Defaults to `grpc` if gRPC support is detected on the system.
      *           *Advanced usage*: Additionally, it is possible to pass in an already instantiated
      *           {@see \Google\ApiCore\Transport\TransportInterface} object. Note that when this
-     *           object is provided, any settings in $transportConfig, and any $serviceAddress
+     *           object is provided, any settings in $transportConfig, and any `$apiEndpoint`
      *           setting, will be ignored.
      *     @type array $transportConfig
      *           Configuration options that will be used to construct the transport. Options for
