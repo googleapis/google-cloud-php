c71b1dde495af13a3a981dd55e12f8b410739e44
John Pedrie <john@pedrie.com>
2019-08-09 14:52:27 -0400

    feat: Add apiEndpoint support to BigQuery

--- BigQuery/src/BigQueryClient.php
+++ BigQuery/src/BigQueryClient.php
@@ -79,6 +79,9 @@ class BigQueryClient
      * @param array $config [optional] {
      *     Configuration options.
      *
+     *     @type string $apiEndpoint The hostname with optional port to use in
+     *           place of the default service endpoint. Example:
+     *           `foobar.com` or `foobar.com:1234`.
      *     @type string $projectId The project ID from the Google Developer's
      *           Console.
      *     @type CacheItemPoolInterface $authCache A cache for storing access
