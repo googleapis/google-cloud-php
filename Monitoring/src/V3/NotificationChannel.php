14152f663665d3e39fec661c35e1d6693ec0f67a
yoshi-automation <yoshi-automation@google.com>
2019-08-29 03:02:15 -0700

    [CHANGE ME] Re-generated Monitoring to pick up changes in the API or client library generator.

--- Monitoring/src/V3/NotificationChannel.php
+++ Monitoring/src/V3/NotificationChannel.php
@@ -458,7 +458,7 @@ class NotificationChannel extends \Google\Protobuf\Internal\Message
      * Generated from protobuf field <code>.google.protobuf.BoolValue enabled = 11;</code>
      * @return bool|null
      */
-    public function getEnabledValue()
+    public function getEnabledUnwrapped()
     {
         $wrapper = $this->getEnabled();
         return is_null($wrapper) ? null : $wrapper->getValue();
@@ -498,7 +498,7 @@ class NotificationChannel extends \Google\Protobuf\Internal\Message
      * @param bool|null $var
      * @return $this
      */
-    public function setEnabledValue($var)
+    public function setEnabledUnwrapped($var)
     {
         $wrappedVar = is_null($var) ? null : new \Google\Protobuf\BoolValue(['value' => $var]);
         return $this->setEnabled($wrappedVar);
