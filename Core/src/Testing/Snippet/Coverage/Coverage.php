6572eb8c68a447b865cc6f2bf40bcbc9ade7c6d5
Jérôme Gamez <jerome@gamez.name>
2019-09-06 17:16:32 +0200

    Exclude Google\Cloud\Core\Logger from snippet coverage

--- Core/src/Testing/Snippet/Coverage/Coverage.php
+++ Core/src/Testing/Snippet/Coverage/Coverage.php
@@ -28,8 +28,9 @@ use Google\Cloud\Core\Testing\Snippet\Parser\Snippet;
 class Coverage
 {
     private static $snippetExcludeList = [
+        '/\\\GcTestListener/',
+        '/\\\Google\\\Cloud\\\Core\\\Logger/',
         '/\\\Google\\\Cloud\\\Core\\\PhpArray/',
-        '/\\\GcTestListener/'
     ];
 
     /**
