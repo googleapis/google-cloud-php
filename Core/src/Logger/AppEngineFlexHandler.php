37de613d39150e102fad0bc26c3d60eeea42d747
Jérôme Gamez <jerome@gamez.name>
2019-09-06 19:56:36 +0200

    Replace posix_getpid() with getmypid()
    The former is not available on Windows.

--- Core/src/Logger/AppEngineFlexHandler.php
+++ Core/src/Logger/AppEngineFlexHandler.php
@@ -46,7 +46,7 @@ class AppEngineFlexHandler extends StreamHandler
         $stream = null
     ) {
         if ($stream === null) {
-            $pid = posix_getpid();
+            $pid = getmypid();
             $stream = "file:///var/log/app_engine/app.$pid.json";
         }
         parent::__construct(
