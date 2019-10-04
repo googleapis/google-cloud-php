e6a4f1733d95b6a26f63bb65082045138c5f4140
David Supplee <dwsupplee@gmail.com>
2019-10-04 10:53:12 -0400

    feat: Introduce Cloud Translation V3 client (#2366)
    * add synth script
    
    * generate translate v3 client
    
    * update README
    
    * update composer files
    
    * update docs site
    
    * update test group name
    
    * add to the README

---
+++ Translate/src/V3/resources/translation_service_rest_client_config.php
@@ -0,0 +1,120 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.translation.v3.TranslationService' => [
+            'TranslateText' => [
+                'method' => 'post',
+                'uriTemplate' => '/v3/{parent=projects/*/locations/*}:translateText',
+                'body' => '*',
+                'additionalBindings' => [
+                    [
+                        'method' => 'post',
+                        'uriTemplate' => '/v3/{parent=projects/*}:translateText',
+                        'body' => '*',
+                    ],
+                ],
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'DetectLanguage' => [
+                'method' => 'post',
+                'uriTemplate' => '/v3/{parent=projects/*/locations/*}:detectLanguage',
+                'body' => '*',
+                'additionalBindings' => [
+                    [
+                        'method' => 'post',
+                        'uriTemplate' => '/v3/{parent=projects/*}:detectLanguage',
+                        'body' => '*',
+                    ],
+                ],
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'GetSupportedLanguages' => [
+                'method' => 'get',
+                'uriTemplate' => '/v3/{parent=projects/*/locations/*}/supportedLanguages',
+                'additionalBindings' => [
+                    [
+                        'method' => 'get',
+                        'uriTemplate' => '/v3/{parent=projects/*}/supportedLanguages',
+                    ],
+                ],
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'BatchTranslateText' => [
+                'method' => 'post',
+                'uriTemplate' => '/v3/{parent=projects/*/locations/*}:batchTranslateText',
+                'body' => '*',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'CreateGlossary' => [
+                'method' => 'post',
+                'uriTemplate' => '/v3/{parent=projects/*/locations/*}/glossaries',
+                'body' => 'glossary',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'ListGlossaries' => [
+                'method' => 'get',
+                'uriTemplate' => '/v3/{parent=projects/*/locations/*}/glossaries',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'GetGlossary' => [
+                'method' => 'get',
+                'uriTemplate' => '/v3/{name=projects/*/locations/*/glossaries/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'DeleteGlossary' => [
+                'method' => 'delete',
+                'uriTemplate' => '/v3/{name=projects/*/locations/*/glossaries/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
---
+++ Translate/synth.py
@@ -0,0 +1,86 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This script is used to synthesize generated parts of this library."""
+
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICGenerator()
+common = gcp.CommonTemplates()
+
+v1_library = gapic.php_library(
+    service='translate',
+    version='v3',
+    artman_output_name='google-cloud-translate-v3')
+
+# copy all src except partial veneer classes
+s.move(v1_library / f'src/')
+
+# copy proto files to src also
+s.move(v1_library / f'proto/src/Google/Cloud/Translate', f'src/')
+s.move(v1_library / f'tests/')
+
+# copy GPBMetadata file to metadata
+s.move(v1_library / f'proto/src/GPBMetadata/Google/Cloud/Translate', f'metadata/')
+
+# document and utilize apiEndpoint instead of serviceAddress
+s.replace(
+    "**/Gapic/*GapicClient.php",
+    r"'serviceAddress' =>",
+    r"'apiEndpoint' =>")
+s.replace(
+    "**/Gapic/*GapicClient.php",
+    r"@type string \$serviceAddress\n\s+\*\s+The address",
+    r"""@type string $serviceAddress
+     *           **Deprecated**. This option will be removed in a future major release. Please
+     *           utilize the `$apiEndpoint` option instead.
+     *     @type string $apiEndpoint
+     *           The address""")
+s.replace(
+    "**/Gapic/*GapicClient.php",
+    r"\$transportConfig, and any \$serviceAddress",
+    r"$transportConfig, and any `$apiEndpoint`")
+
+# prevent proto messages from being marked final
+s.replace(
+    "src/V*/**/*.php",
+    r"final class",
+    r"class")
+
+# Replace "Unwrapped" with "Value" for method names.
+s.replace(
+    "src/V*/**/*.php",
+    r"public function ([s|g]\w{3,})Unwrapped",
+    r"public function \1Value"
+)
+
+# fix year
+s.replace(
+    'src/V3/**/*.php',
+    r'Copyright \d{4}',
+    r'Copyright 2019')
+s.replace(
+    'tests/**/V3/*Test.php',
+    r'Copyright \d{4}',
+    r'Copyright 2019')
+
+# Change the wording for the deprecation warning.
+s.replace(
+    'src/*/*_*.php',
+    r'will be removed in the next major release',
+    'will be removed in a future release')
