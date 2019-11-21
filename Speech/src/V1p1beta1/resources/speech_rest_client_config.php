279e39e0e818fdecce3652b72370d78667ff3cf4
David Supplee <dwsupplee@gmail.com>
2019-11-20 19:03:32 -0800

    manual regen

--- Speech/src/V1p1beta1/resources/speech_rest_client_config.php
+++ Speech/src/V1p1beta1/resources/speech_rest_client_config.php
@@ -14,5 +14,41 @@ return [
                 'body' => '*',
             ],
         ],
+        'google.longrunning.Operations' => [
+            'GetOperation' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1p1beta1/operations/{name=**}',
+                'additionalBindings' => [
+                    [
+                        'method' => 'get',
+                        'uriTemplate' => '/v1p1beta1/{name=projects/*/locations/*/operations/*}',
+                    ],
+                ],
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'ListOperations' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1p1beta1/operations',
+                'additionalBindings' => [
+                    [
+                        'method' => 'get',
+                        'uriTemplate' => '/v1p1beta1/{name=projects/*/locations/*}/operations',
+                    ],
+                ],
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
