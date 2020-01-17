e5259297b9a034df88c0f5fa5ec4d2aa6c885b48
John Pedrie <john@pedrie.com>
2020-01-17 17:25:30 -0500

    chore: fix proto messages (#2612)

--- SecretManager/synth.py
+++ SecretManager/synth.py
@@ -58,19 +58,6 @@ s.replace(
     r"\$transportConfig, and any \$serviceAddress",
     r"$transportConfig, and any `$apiEndpoint`")
 
-# prevent proto messages from being marked final
-s.replace(
-    "src/V*/**/*.php",
-    r"final class",
-    r"class")
-
-# Replace "Unwrapped" with "Value" for method names.
-s.replace(
-    "src/V*/**/*.php",
-    r"public function ([s|g]\w{3,})Unwrapped",
-    r"public function \1Value"
-)
-
 # fix year
 s.replace(
     'src/**/**/*.php',
@@ -92,3 +79,28 @@ s.replace(
     'src/*/*_*.php',
     r'will be removed in the next major release',
     'will be removed in a future release')
+
+### [START] protoc backwards compatibility fixes
+
+# roll back to private properties.
+s.replace(
+    "src/**/V*/**/*.php",
+    r"Generated from protobuf field ([^\n]{0,})\n\s{5}\*/\n\s{4}protected \$",
+    r"""Generated from protobuf field \1
+     */
+    private $""")
+
+# prevent proto messages from being marked final
+s.replace(
+    "src/**/V*/**/*.php",
+    r"final class",
+    r"class")
+
+# Replace "Unwrapped" with "Value" for method names.
+s.replace(
+    "src/**/V*/**/*.php",
+    r"public function ([s|g]\w{3,})Unwrapped",
+    r"public function \1Value"
+)
+
+### [END] protoc backwards compatibility fixes
--- Trace/synth.py
+++ Trace/synth.py
@@ -58,19 +58,6 @@ s.replace(
     r"\$transportConfig, and any \$serviceAddress",
     r"$transportConfig, and any `$apiEndpoint`")
 
-# prevent proto messages from being marked final
-s.replace(
-    "src/V*/**/*.php",
-    r"final class",
-    r"class")
-
-# Replace "Unwrapped" with "Value" for method names.
-s.replace(
-    "src/V*/**/*.php",
-    r"public function ([s|g]\w{3,})Unwrapped",
-    r"public function \1Value"
-)
-
 # fix year
 s.replace(
     '**/Gapic/*GapicClient.php',
@@ -102,3 +89,28 @@ s.replace(
     'src/V2/Gapic/TraceServiceGapicClient.php',
     r'Span_',
     r'Span\\')
+
+### [START] protoc backwards compatibility fixes
+
+# roll back to private properties.
+s.replace(
+    "src/**/V*/**/*.php",
+    r"Generated from protobuf field ([^\n]{0,})\n\s{5}\*/\n\s{4}protected \$",
+    r"""Generated from protobuf field \1
+     */
+    private $""")
+
+# prevent proto messages from being marked final
+s.replace(
+    "src/**/V*/**/*.php",
+    r"final class",
+    r"class")
+
+# Replace "Unwrapped" with "Value" for method names.
+s.replace(
+    "src/**/V*/**/*.php",
+    r"public function ([s|g]\w{3,})Unwrapped",
+    r"public function \1Value"
+)
+
+### [END] protoc backwards compatibility fixes
