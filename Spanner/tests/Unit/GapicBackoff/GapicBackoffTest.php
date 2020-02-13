39a463a6db426a3ad38ef05282bcb241b27a18c0
Alexey Andreev <alexey.andreev@qlogic.io>
2020-02-13 17:12:28 +0300

    chore: GAPIC backoffs
    refactor: get rid of Reflection
    test: new flag does not interfere with custom gRPC options

--- Spanner/tests/Unit/GapicBackoff/GapicBackoffTest.php
+++ Spanner/tests/Unit/GapicBackoff/GapicBackoffTest.php
@@ -23,10 +23,7 @@ class GapicBackoffTest extends TestCase {
         ];
 
         $spanner = new SpannerClient($config);
-        $reflection = new \ReflectionObject($spanner);
-        $connection = $reflection->getProperty('connection');
-        $connection->setAccessible(true);
-        $connection = $connection->getValue($spanner);
+        $connection = $spanner->instance('nonexistent')->database('nonexistent')->connection();
         return $connection->requestWrapper();
     }
 
@@ -49,12 +46,11 @@ class GapicBackoffTest extends TestCase {
             $args = func_get_args();
             return new MockOperationResponse('test', null, array_pop($args));
         };
-        $response = $wrapper->send($handler, [[]])->options;
-        $this->assertTrue(
-            array_key_exists('retrySettings', $response) and
-            array_key_exists('retriesEnabled', $response['retrySettings']) and
-            $response['retrySettings']['retriesEnabled'] === false
-        );
+        $response = $wrapper->send($handler, [[]]);
+        $expected = [
+            'retriesEnabled' => false,
+        ];
+        $this->assertEquals($expected, $response->options['retrySettings']);
     }
 
     public function testBackoffEnabledManually()
@@ -67,11 +63,29 @@ class GapicBackoffTest extends TestCase {
             $args = func_get_args();
             return new MockOperationResponse('test', null, array_pop($args));
         };
-        $response = $wrapper->send($handler, [[]])->options;
-        $this->assertTrue(
-            array_key_exists('retrySettings', $response) and
-            is_array($response['retrySettings']) and
-            empty($response['retrySettings'])
-        );
+        $response = $wrapper->send($handler, [[]]);
+        $expected = [];
+        $this->assertEquals($expected, $response->options['retrySettings']);
+    }
+
+    public function testUserConfigsAreNotRuined()
+    {
+        $retrySettings = [
+            'retriesEnabled' => false,
+            'noRetriesRpcTimeoutMillis' => 1234,
+        ];
+        $config = [
+            'useGapicBackoffs' => true,
+            'grpcOptions' => [
+                'retrySettings' => $retrySettings,
+            ],
+        ];
+        $wrapper = $this->getWrapper($config);
+        $handler = function () {
+            $args = func_get_args();
+            return new MockOperationResponse('test', null, array_pop($args));
+        };
+        $response = $wrapper->send($handler, [[]]);
+        $this->assertEquals($retrySettings, $response->options['retrySettings']);
     }
 }
