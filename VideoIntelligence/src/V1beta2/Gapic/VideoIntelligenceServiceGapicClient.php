3512ac814ac2480469220ee4fa45ca1de4a9cc38
yoshi-automation <yoshi-automation@google.com>
2020-03-06 03:11:01 -0800

    [CHANGE ME] Re-generated VideoIntelligence to pick up changes in the API or client library generator.

--- VideoIntelligence/src/V1beta2/Gapic/VideoIntelligenceServiceGapicClient.php
+++ VideoIntelligence/src/V1beta2/Gapic/VideoIntelligenceServiceGapicClient.php
@@ -295,7 +295,7 @@ class VideoIntelligenceServiceGapicClient
      *          supported, which must be specified in the following format:
      *          `gs://bucket-id/object-id` (other URI formats return
      *          [google.rpc.Code.INVALID_ARGUMENT][google.rpc.Code.INVALID_ARGUMENT]). For
-     *          more information, see [Request URIs](https://cloud.google.com/storage/docs/reference-uris). A video
+     *          more information, see [Request URIs](https://cloud.google.com/storage/docs/request-endpoints). A video
      *          URI may include wildcards in `object-id`, and thus identify multiple
      *          videos. Supported wildcards: '*' to match 0 or more characters;
      *          '?' to match 1 character. If unset, the input video should be embedded
@@ -315,7 +315,7 @@ class VideoIntelligenceServiceGapicClient
      *          URIs are supported, which must be specified in the following format:
      *          `gs://bucket-id/object-id` (other URI formats return
      *          [google.rpc.Code.INVALID_ARGUMENT][google.rpc.Code.INVALID_ARGUMENT]). For
-     *          more information, see [Request URIs](https://cloud.google.com/storage/docs/reference-uris).
+     *          more information, see [Request URIs](https://cloud.google.com/storage/docs/request-endpoints).
      *     @type string $locationId
      *          Optional. Cloud region where annotation should take place. Supported cloud
      *          regions: `us-east1`, `us-west1`, `europe-west1`, `asia-east1`. If no region
