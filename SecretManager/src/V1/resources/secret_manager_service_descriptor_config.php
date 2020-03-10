bdc1c7f1077b835edb272c12f2972c3bb0261814
David Supplee <dwsupplee@gmail.com>
2020-03-10 14:16:05 -0700

    feat: introduce secret manager v1 client

---
+++ SecretManager/src/V1/resources/secret_manager_service_descriptor_config.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.secretmanager.v1.SecretManagerService' => [
+            'ListSecrets' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getSecrets',
+                ],
+            ],
+            'ListSecretVersions' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getVersions',
+                ],
+            ],
+        ],
+    ],
+];
---
+++ SecretManager/src/V1/resources/secret_manager_service_rest_client_config.php
@@ -0,0 +1,182 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.secretmanager.v1.SecretManagerService' => [
+            'ListSecrets' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{parent=projects/*}/secrets',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'CreateSecret' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{parent=projects/*}/secrets',
+                'body' => 'secret',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'AddSecretVersion' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{parent=projects/*/secrets/*}:addVersion',
+                'body' => '*',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'GetSecret' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{name=projects/*/secrets/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'UpdateSecret' => [
+                'method' => 'patch',
+                'uriTemplate' => '/v1/{secret.name=projects/*/secrets/*}',
+                'body' => 'secret',
+                'placeholders' => [
+                    'secret.name' => [
+                        'getters' => [
+                            'getSecret',
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'DeleteSecret' => [
+                'method' => 'delete',
+                'uriTemplate' => '/v1/{name=projects/*/secrets/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'ListSecretVersions' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{parent=projects/*/secrets/*}/versions',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'GetSecretVersion' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{name=projects/*/secrets/*/versions/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'AccessSecretVersion' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{name=projects/*/secrets/*/versions/*}:access',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'DisableSecretVersion' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{name=projects/*/secrets/*/versions/*}:disable',
+                'body' => '*',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'EnableSecretVersion' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{name=projects/*/secrets/*/versions/*}:enable',
+                'body' => '*',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'DestroySecretVersion' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{name=projects/*/secrets/*/versions/*}:destroy',
+                'body' => '*',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'SetIamPolicy' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{resource=projects/*/secrets/*}:setIamPolicy',
+                'body' => '*',
+                'placeholders' => [
+                    'resource' => [
+                        'getters' => [
+                            'getResource',
+                        ],
+                    ],
+                ],
+            ],
+            'GetIamPolicy' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{resource=projects/*/secrets/*}:getIamPolicy',
+                'placeholders' => [
+                    'resource' => [
+                        'getters' => [
+                            'getResource',
+                        ],
+                    ],
+                ],
+            ],
+            'TestIamPermissions' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{resource=projects/*/secrets/*}:testIamPermissions',
+                'body' => '*',
+                'placeholders' => [
+                    'resource' => [
+                        'getters' => [
+                            'getResource',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
