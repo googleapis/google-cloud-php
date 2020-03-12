5a97c70290718370ef068f15206e86bc9282ba14
David Supplee <dwsupplee@gmail.com>
2020-03-12 15:57:38 -0700

    restore operation bindings

--- SecurityCenter/src/V1/resources/security_center_rest_client_config.php
+++ SecurityCenter/src/V1/resources/security_center_rest_client_config.php
@@ -284,5 +284,52 @@ return [
                 ],
             ],
         ],
+        'google.longrunning.Operations' => [
+            'ListOperations' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{name=organizations/*/operations}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'GetOperation' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{name=organizations/*/operations/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'DeleteOperation' => [
+                'method' => 'delete',
+                'uriTemplate' => '/v1/{name=organizations/*/operations/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'CancelOperation' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{name=organizations/*/operations/*}:cancel',
+                'body' => '*',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
