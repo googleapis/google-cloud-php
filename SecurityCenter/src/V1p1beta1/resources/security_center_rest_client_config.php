e25ac2761787d2be04a08dfe054c2ad4afad68ef
yoshi-automation <yoshi-automation@google.com>
2020-03-27 03:10:52 -0700

    [CHANGE ME] Re-generated SecurityCenter to pick up changes in the API or client library generator.

--- SecurityCenter/src/V1p1beta1/resources/security_center_rest_client_config.php
+++ SecurityCenter/src/V1p1beta1/resources/security_center_rest_client_config.php
@@ -284,52 +284,5 @@ return [
                 ],
             ],
         ],
-        'google.longrunning.Operations' => [
-            'ListOperations' => [
-                'method' => 'get',
-                'uriTemplate' => '/v1p1beta1/{name=organizations/*/operations}',
-                'placeholders' => [
-                    'name' => [
-                        'getters' => [
-                            'getName',
-                        ],
-                    ],
-                ],
-            ],
-            'GetOperation' => [
-                'method' => 'get',
-                'uriTemplate' => '/v1p1beta1/{name=organizations/*/operations/*}',
-                'placeholders' => [
-                    'name' => [
-                        'getters' => [
-                            'getName',
-                        ],
-                    ],
-                ],
-            ],
-            'DeleteOperation' => [
-                'method' => 'delete',
-                'uriTemplate' => '/v1p1beta1/{name=organizations/*/operations/*}',
-                'placeholders' => [
-                    'name' => [
-                        'getters' => [
-                            'getName',
-                        ],
-                    ],
-                ],
-            ],
-            'CancelOperation' => [
-                'method' => 'post',
-                'uriTemplate' => '/v1p1beta1/{name=organizations/*/operations/*}:cancel',
-                'body' => '*',
-                'placeholders' => [
-                    'name' => [
-                        'getters' => [
-                            'getName',
-                        ],
-                    ],
-                ],
-            ],
-        ],
     ],
 ];
