59ebb475dc657b04526a784f63d4dd2d54069d76
John Pedrie <john@pedrie.com>
2020-03-30 09:30:37 -0400

    fix: revert rest LRO bindings removal

--- Dialogflow/src/V2/resources/contexts_rest_client_config.php
+++ Dialogflow/src/V2/resources/contexts_rest_client_config.php
@@ -111,5 +111,18 @@ return [
                 ],
             ],
         ],
+        'google.longrunning.Operations' => [
+            'GetOperation' => [
+                'method' => 'get',
+                'uriTemplate' => '/v2/{name=projects/*/operations/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
