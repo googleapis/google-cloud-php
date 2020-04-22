adc7213bc8060e1ba5c3f9f4e0dc327e4103d4a0
David Supplee <dwsupplee@gmail.com>
2020-04-22 10:22:45 -0700

    feat: introduce memorystore for memcached client

---
+++ Memcache/src/V1beta2/resources/cloud_memcache_rest_client_config.php
@@ -0,0 +1,174 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.memcache.v1beta2.CloudMemcache' => [
+            'ListInstances' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1beta2/{parent=projects/*/locations/*}/instances',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'GetInstance' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1beta2/{name=projects/*/locations/*/instances/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'CreateInstance' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1beta2/{parent=projects/*/locations/*}/instances',
+                'body' => 'resource',
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'UpdateInstance' => [
+                'method' => 'patch',
+                'uriTemplate' => '/v1beta2/{resource.name=projects/*/locations/*/instances/*}',
+                'body' => 'resource',
+                'placeholders' => [
+                    'resource.name' => [
+                        'getters' => [
+                            'getResource',
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'UpdateParameters' => [
+                'method' => 'patch',
+                'uriTemplate' => '/v1beta2/{name=projects/*/locations/*/instances/*}:updateParameters',
+                'body' => '*',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'DeleteInstance' => [
+                'method' => 'delete',
+                'uriTemplate' => '/v1beta2/{name=projects/*/locations/*/instances/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'ApplyParameters' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1beta2/{name=projects/*/locations/*/instances/*}:applyParameters',
+                'body' => '*',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        'google.iam.v1.IAMPolicy' => [
+            'GetIamPolicy' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1beta2/{resource=projects/*/locations/*/instances/*}:getIamPolicy',
+                'placeholders' => [
+                    'resource' => [
+                        'getters' => [
+                            'getResource',
+                        ],
+                    ],
+                ],
+            ],
+            'SetIamPolicy' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1beta2/{resource=projects/*/locations/*/instances/*}:setIamPolicy',
+                'body' => '*',
+                'placeholders' => [
+                    'resource' => [
+                        'getters' => [
+                            'getResource',
+                        ],
+                    ],
+                ],
+            ],
+            'TestIamPermissions' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1beta2/{resource=projects/*/locations/*/instances/*}:testIamPermissions',
+                'body' => '*',
+                'placeholders' => [
+                    'resource' => [
+                        'getters' => [
+                            'getResource',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        'google.longrunning.Operations' => [
+            'CancelOperation' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1beta2/{name=projects/*/locations/*/operations/*}:cancel',
+                'body' => '*',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'DeleteOperation' => [
+                'method' => 'delete',
+                'uriTemplate' => '/v1beta2/{name=projects/*/locations/*/operations/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'GetOperation' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1beta2/{name=projects/*/locations/*/operations/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+            'ListOperations' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1beta2/{name=projects/*/locations/*}/operations',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
