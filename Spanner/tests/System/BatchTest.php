dbf31de3792ac38681ed4767952502496a8f285d
larkee <larkee@users.noreply.github.com>
2020-05-08 14:25:45 +1000

    remove batch read test skip

--- Spanner/tests/System/BatchTest.php
+++ Spanner/tests/System/BatchTest.php
@@ -64,10 +64,6 @@ class BatchTest extends SpannerTestCase
 
     public function testBatch()
     {
-        if ((bool) getenv("SPANNER_EMULATOR_HOST")) {
-            $this->markTestSkipped();
-        }
-
         $query = 'SELECT
                 id,
                 decade
