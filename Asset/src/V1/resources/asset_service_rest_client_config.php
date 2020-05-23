556403fafd5850f4fff711c4622d9c111961ca35
yoshi-automation <yoshi-automation@google.com>
2020-05-23 10:18:15 -0700

    Integrate Python GAPIC Microgenerator in googleapis. This PR uses using documentai as an example. Depends on https://github.com/googleapis/gapic-generator-python/pull/402
    PiperOrigin-RevId: 309824146
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon May 4 15:06:44 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: e0f9d9e1f9de890db765be46f45ca8490723e3eb
    Source-Link: https://github.com/googleapis/googleapis/commit/e0f9d9e1f9de890db765be46f45ca8490723e3eb

--- Asset/src/V1/resources/asset_service_rest_client_config.php
+++ Asset/src/V1/resources/asset_service_rest_client_config.php
@@ -3,6 +3,17 @@
 return [
     'interfaces' => [
         'google.cloud.asset.v1.AssetService' => [
+            'DeleteFeed' => [
+                'method' => 'delete',
+                'uriTemplate' => '/v1/{name=*/*/feeds/*}',
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
             'ExportAssets' => [
                 'method' => 'post',
                 'uriTemplate' => '/v1/{parent=*/*}:exportAssets',
@@ -68,60 +79,6 @@ return [
                     'feed.name' => [
                         'getters' => [
                             'getFeed',
-                            'getName',
-                        ],
-                    ],
-                ],
-            ],
-            'DeleteFeed' => [
-                'method' => 'delete',
-                'uriTemplate' => '/v1/{name=*/*/feeds/*}',
-                'placeholders' => [
-                    'name' => [
-                        'getters' => [
-                            'getName',
-                        ],
-                    ],
-                ],
-            ],
-        ],
-        'google.longrunning.Operations' => [
-            'GetOperation' => [
-                'method' => 'get',
-                'uriTemplate' => '/v1alpha1/{name=projects/*/operations/*/*}',
-                'additionalBindings' => [
-                    [
-                        'method' => 'get',
-                        'uriTemplate' => '/v1alpha1/{name=organizations/*/operations/*/*}',
-                    ],
-                    [
-                        'method' => 'get',
-                        'uriTemplate' => '/v1alpha2/{name=projects/*/operations/*/*}',
-                    ],
-                    [
-                        'method' => 'get',
-                        'uriTemplate' => '/v1alpha2/{name=organizations/*/operations/*/*}',
-                    ],
-                    [
-                        'method' => 'get',
-                        'uriTemplate' => '/v1beta1/{name=projects/*/operations/*/*}',
-                    ],
-                    [
-                        'method' => 'get',
-                        'uriTemplate' => '/v1beta1/{name=folders/*/operations/*/*}',
-                    ],
-                    [
-                        'method' => 'get',
-                        'uriTemplate' => '/v1beta1/{name=organizations/*/operations/*/*}',
-                    ],
-                    [
-                        'method' => 'get',
-                        'uriTemplate' => '/v1/{name=*/*/operations/*/*}',
-                    ],
-                ],
-                'placeholders' => [
-                    'name' => [
-                        'getters' => [
                             'getName',
                         ],
                     ],
