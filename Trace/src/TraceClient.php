cc6251bb5af05929d25c509e63f9ead7a5c599d4
David Supplee <dwsupplee@gmail.com>
2020-05-26 13:49:24 -0700

    updated Trace/src/TraceClient.php [ci skip]

--- Trace/src/TraceClient.php
+++ Trace/src/TraceClient.php
@@ -40,7 +40,7 @@ class TraceClient
 {
     use ClientTrait;
 
-    const VERSION = '1.0.0';
+    const VERSION = '1.1.0';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/cloud-platform';
 
