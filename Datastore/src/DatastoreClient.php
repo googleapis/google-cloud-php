b6f0f14d8b1a0d8d145fa8be06b2eb0293564fcc
John Pedrie <john@pedrie.com>
2020-06-08 14:49:53 -0400

    chore: release 0.133.1 (#3121)

--- Datastore/src/DatastoreClient.php
+++ Datastore/src/DatastoreClient.php
@@ -80,7 +80,7 @@ class DatastoreClient
     use ClientTrait;
     use DatastoreTrait;
 
-    const VERSION = '1.11.3';
+    const VERSION = '1.11.4';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/datastore';
 
