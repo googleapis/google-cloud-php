0833223acaa4dbe62475a3dfc58cdabcf933c053
John Pedrie <john@pedrie.com>
2020-06-18 13:54:53 -0400

    feat: add support for subscription filtering (#3136)
    * feat: add support for subscription filtering
    
    * fix docs around create/update semantics

--- PubSub/src/Subscription.php
+++ PubSub/src/Subscription.php
@@ -244,6 +244,12 @@ class Subscription
      *           `ttl` is not set, the associated resource never expires. If a
      *           string is provided, it should be as a duration in seconds with
      *           up to nine fractional digits, terminated by 's', e.g "3.5s".
+     *     @type string $filter An expression written in the Pub/Sub
+     *           [filter language](https://cloud.google.com/pubsub/docs/filtering).
+     *           If non-empty, then only messages whose `attributes` field
+     *           matches the filter are delivered on this subscription. If
+     *           empty, then no messages are filtered out. Filtering can only be
+     *           configured at subscription creation.
      *     @type Duration|string $messageRetentionDuration How long to retain
      *           unacknowledged messages in the subscription's backlog, from the
      *           moment a message is published. If `$retainAckedMessages` is
