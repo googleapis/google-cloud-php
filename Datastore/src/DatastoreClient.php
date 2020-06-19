628fa08945a99c71f13758851db84a98a2f15de0
John Pedrie <john@pedrie.com>
2020-06-19 13:07:18 -0400

    updated Datastore/src/DatastoreClient.php [ci skip]

--- Datastore/src/DatastoreClient.php
+++ Datastore/src/DatastoreClient.php
@@ -80,7 +80,7 @@ class DatastoreClient
     use ClientTrait;
     use DatastoreTrait;
 
-    const VERSION = '1.11.4';
+    const VERSION = '1.11.5';
 
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/datastore';
 
