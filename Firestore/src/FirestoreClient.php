ac708f1d3945e3be367ebd47ba7491698724ca65
John Pedrie <john@pedrie.com>
2020-07-21 14:59:47 -0400

    chore: release 0.136.0 (#3239)

--- Firestore/src/FirestoreClient.php
+++ Firestore/src/FirestoreClient.php
@@ -75,7 +75,7 @@ class FirestoreClient
     use SnapshotTrait;
     use ValidateTrait;
 
-    const VERSION = '1.14.0';
+    const VERSION = '1.14.1';
 
     const DEFAULT_DATABASE = '(default)';
 
--- src/Version.php
+++ src/Version.php
@@ -22,5 +22,5 @@ namespace Google\Cloud;
  */
 class Version
 {
-    const VERSION = '0.135.0';
+    const VERSION = '0.136.0';
 }
