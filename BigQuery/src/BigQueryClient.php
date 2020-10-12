27f04ee010168025e6302aeb4d19d1af014b723c
John Pedrie <john@pedrie.com>
2020-10-12 18:10:13 -0400

    chore: release 0.142.0

--- BigQuery/src/BigQueryClient.php
+++ BigQuery/src/BigQueryClient.php
@@ -50,7 +50,7 @@ class BigQueryClient
         ClientTrait::jsonDecode insteadof RetryDeciderTrait;
     }
 
-    const VERSION = '1.18.0';
+    const VERSION = '1.19.0';
 
     const MAX_DELAY_MICROSECONDS = 32000000;
 
--- src/Version.php
+++ src/Version.php
@@ -22,5 +22,5 @@ namespace Google\Cloud;
  */
 class Version
 {
-    const VERSION = '0.141.0';
+    const VERSION = '0.142.0';
 }
