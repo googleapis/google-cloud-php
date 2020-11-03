ce237f0dd7554afe659b335a7515925511f925cc
yoshi-automation <yoshi-automation@google.com>
2020-11-03 03:12:05 -0800

    Remove redundant secretmanager v1 proto comment
    PiperOrigin-RevId: 329941383
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Sep 3 10:09:58 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: de9ba7dc763aab244e20d2ebb7bf897806998441
    Source-Link: https://github.com/googleapis/googleapis/commit/de9ba7dc763aab244e20d2ebb7bf897806998441

--- SecretManager/src/V1/SecretManagerServiceGrpcClient.php
+++ SecretManager/src/V1/SecretManagerServiceGrpcClient.php
@@ -19,9 +19,6 @@
 namespace Google\Cloud\SecretManager\V1;
 
 /**
- * `projects/&#42;/secrets/&#42;/versions/latest` is an alias to the `latest`
- * [SecretVersion][google.cloud.secretmanager.v1.SecretVersion].
- *
  * Secret Manager Service
  *
  * Manages secrets and operations using those secrets. Implements a REST
