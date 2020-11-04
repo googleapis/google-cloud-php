1dd2b5f0b7e0acf70591a23cf8141c1e58c5a6ce
Alexey Andreev <alexey.andreev@qlogic.io>
2020-11-05 02:56:39 +0300

    ci: add workflows for system tests with emulators (#3551)

--- .github/emulator/start-emulator.sh
+++ .github/emulator/start-emulator.sh
@@ -4,5 +4,5 @@ docker pull -q gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators
 docker run \
   -d \
   -p 8085:8085 \
-  gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators gcloud beta emulators pubsub start --host-port=0.0.0.0:8085
+  gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators gcloud beta emulators $1 start --host-port=0.0.0.0:8085
 sleep 10
