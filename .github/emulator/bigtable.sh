63e13512635ca907554a8fd30645682a1194d8d3
Alexey Andreev <alexey.andreev@qlogic.io>
2020-11-05 00:37:16 +0300

    ci: use single emulator starter for different libraries

--- .github/emulator/bigtable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -eux
-
-docker pull -q gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators
-docker run \
-  -d \
-  -p 8086:8086 \
-  gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators gcloud beta emulators bigtable start --host-port=0.0.0.0:8086
-sleep 10
