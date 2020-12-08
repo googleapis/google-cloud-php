e7e4bbc5f0c55e067020f283c2e9a83687ffa2e8
Alexey Andreev <alexey.andreev@qlogic.io>
2020-12-09 01:33:42 +0300

    chore: hardcode emulator project parameter

--- .github/emulator/start-emulator.sh
+++ .github/emulator/start-emulator.sh
@@ -1,11 +1,8 @@
 #!/bin/sh -eux
 
-EMULATOR=$1
-shift
 docker pull -q gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators
 CONTAINER=`docker run \
   -d \
   -p 8085:8085 \
-  gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators gcloud beta emulators $EMULATOR start --host-port=0.0.0.0:8085 "$@"`
+  gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators gcloud beta emulators $1 start --host-port=0.0.0.0:8085 --project=emulator-project`
 sleep 10
-docker logs $CONTAINER
