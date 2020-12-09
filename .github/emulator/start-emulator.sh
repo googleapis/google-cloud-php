e6b92171311e9c2cc51ee8f6e2e695b1fa3513f1
Alexey Andreev <alexey.andreev@qlogic.io>
2020-12-09 04:20:09 +0300

    chore: add emulator container logs

--- .github/emulator/start-emulator.sh
+++ .github/emulator/start-emulator.sh
@@ -6,3 +6,4 @@ CONTAINER=`docker run \
   -p 8085:8085 \
   gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators gcloud beta emulators $1 start --host-port=0.0.0.0:8085 --project=emulator-project`
 sleep 10
+docker logs $CONTAINER
