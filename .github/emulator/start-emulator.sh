93cf8bda3a07bed78a0232abf0a652fa07b3a449
Alexey Andreev <ava1280@yandex.ru>
2021-01-19 20:46:24 +0300

    ci: add project id for Datastore tests (#3635)

--- .github/emulator/start-emulator.sh
+++ .github/emulator/start-emulator.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -eux
 
 docker pull -q gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators
-docker run \
+CONTAINER=`docker run \
   -d \
   -p 8085:8085 \
-  gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators gcloud beta emulators $1 start --host-port=0.0.0.0:8085
+  gcr.io/google.com/cloudsdktool/cloud-sdk:316.0.0-emulators gcloud beta emulators $1 start --host-port=0.0.0.0:8085 --project=emulator-project`
 sleep 10
+docker logs $CONTAINER
