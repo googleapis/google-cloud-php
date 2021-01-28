06702861d81f6fef4fdee50a8c508c1d464a904e
yoshi-automation <yoshi-automation@google.com>
2021-01-28 10:22:38 -0800

    chore: update Go generator, rules_go, and protobuf
    PiperOrigin-RevId: 352816749
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jan 20 10:06:23 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: ceaaf31b3d13badab7cf9d3b570f5639db5593d9
    Source-Link: https://github.com/googleapis/googleapis/commit/ceaaf31b3d13badab7cf9d3b570f5639db5593d9

--- AutoMl/metadata/V1Beta1/Geometry.php
+++ AutoMl/metadata/V1Beta1/Geometry.php
@@ -15,9 +15,17 @@ class Geometry
           return;
         }
         \GPBMetadata\Google\Api\Annotations::initOnce();
-        $pool->internalAddGeneratedFile(hex2bin(
-            "0aff020a2a676f6f676c652f636c6f75642f6175746f6d6c2f763162657461312f67656f6d657472792e70726f746f121b676f6f676c652e636c6f75642e6175746f6d6c2e7631626574613122280a104e6f726d616c697a656456657274657812090a017818012001280212090a0179180220012802225a0a0c426f756e64696e67506f6c79124a0a136e6f726d616c697a65645f766572746963657318022003280b322d2e676f6f676c652e636c6f75642e6175746f6d6c2e763162657461312e4e6f726d616c697a656456657274657842a5010a1f636f6d2e676f6f676c652e636c6f75642e6175746f6d6c2e7631626574613150015a41676f6f676c652e676f6c616e672e6f72672f67656e70726f746f2f676f6f676c65617069732f636c6f75642f6175746f6d6c2f763162657461313b6175746f6d6cca021b476f6f676c655c436c6f75645c4175746f4d6c5c56316265746131ea021e476f6f676c653a3a436c6f75643a3a4175746f4d4c3a3a56316265746131620670726f746f33"
-        ), true);
+        $pool->internalAddGeneratedFile(
+            '
+ÿ
+*google/cloud/automl/v1beta1/geometry.protogoogle.cloud.automl.v1beta1"(
+NormalizedVertex	
+x (	
+y ("Z
+BoundingPolyJ
+normalized_vertices (2-.google.cloud.automl.v1beta1.NormalizedVertexB¥
+com.google.cloud.automl.v1beta1PZAgoogle.golang.org/genproto/googleapis/cloud/automl/v1beta1;automlÊGoogle\\Cloud\\AutoMl\\V1beta1êGoogle::Cloud::AutoML::V1beta1bproto3'
+        , true);
 
         static::$is_initialized = true;
     }
--- AutoMl/metadata/V1Beta1/Temporal.php
+++ AutoMl/metadata/V1Beta1/Temporal.php
@@ -16,9 +16,15 @@ class Temporal
         }
         \GPBMetadata\Google\Protobuf\Duration::initOnce();
         \GPBMetadata\Google\Api\Annotations::initOnce();
-        $pool->internalAddGeneratedFile(hex2bin(
-            "0a90030a2a676f6f676c652f636c6f75642f6175746f6d6c2f763162657461312f74656d706f72616c2e70726f746f121b676f6f676c652e636c6f75642e6175746f6d6c2e763162657461311a1c676f6f676c652f6170692f616e6e6f746174696f6e732e70726f746f22770a0b54696d655365676d656e7412340a1173746172745f74696d655f6f666673657418012001280b32192e676f6f676c652e70726f746f6275662e4475726174696f6e12320a0f656e645f74696d655f6f666673657418022001280b32192e676f6f676c652e70726f746f6275662e4475726174696f6e42a5010a1f636f6d2e676f6f676c652e636c6f75642e6175746f6d6c2e7631626574613150015a41676f6f676c652e676f6c616e672e6f72672f67656e70726f746f2f676f6f676c65617069732f636c6f75642f6175746f6d6c2f763162657461313b6175746f6d6cca021b476f6f676c655c436c6f75645c4175746f4d6c5c56316265746131ea021e476f6f676c653a3a436c6f75643a3a4175746f4d4c3a3a56316265746131620670726f746f33"
-        ), true);
+        $pool->internalAddGeneratedFile(
+            '
+ê
+*google/cloud/automl/v1beta1/temporal.protogoogle.cloud.automl.v1beta1google/api/annotations.proto"w
+TimeSegment4
+start_time_offset (2.google.protobuf.Duration2
+end_time_offset (2.google.protobuf.DurationB•
+com.google.cloud.automl.v1beta1PZAgoogle.golang.org/genproto/googleapis/cloud/automl/v1beta1;automl Google\\Cloud\\AutoMl\\V1beta1ÍGoogle::Cloud::AutoML::V1beta1bproto3'
+        , true);
 
         static::$is_initialized = true;
     }
