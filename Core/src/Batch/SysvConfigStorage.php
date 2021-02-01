a80c981d74a9938fc943a3169c1df88f39da9896
Brent Shaffer <betterbrent@google.com>
2021-02-01 12:53:18 -0800

    fix: do not attempt to serialize SysvSemaphore in PHP8

--- Core/src/Batch/SysvConfigStorage.php
+++ Core/src/Batch/SysvConfigStorage.php
@@ -156,4 +156,15 @@ class SysvConfigStorage implements ConfigStorageInterface
         $shmid = shm_attach($this->sysvKey, $this->shmSize, $this->perm);
         shm_remove_var($shmid, self::VAR_KEY);
     }
+
+    /**
+     * Serialize the object
+     */
+    public function __serialize()
+    {
+        $vars = get_object_vars($this);
+        // As of PHP 8.0, "semid" is the unserializable object "SysvSemaphore"
+        unset($vars['semid']);
+        return $vars;
+    }
 }
