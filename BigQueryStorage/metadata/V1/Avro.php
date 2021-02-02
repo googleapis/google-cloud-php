9ea18ada8477936963b5363a951181b07ee933e4
yoshi-automation <yoshi-automation@google.com>
2021-02-02 03:44:52 -0800

    chore: update Go generator, rules_go, and protobuf
    PiperOrigin-RevId: 352816749
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jan 20 10:06:23 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: ceaaf31b3d13badab7cf9d3b570f5639db5593d9
    Source-Link: https://github.com/googleapis/googleapis/commit/ceaaf31b3d13badab7cf9d3b570f5639db5593d9

--- BigQueryStorage/metadata/V1/Avro.php
+++ BigQueryStorage/metadata/V1/Avro.php
@@ -14,9 +14,18 @@ class Avro
         if (static::$is_initialized == true) {
           return;
         }
-        $pool->internalAddGeneratedFile(hex2bin(
-            "0af9020a2b676f6f676c652f636c6f75642f62696771756572792f73746f726167652f76312f6176726f2e70726f746f1220676f6f676c652e636c6f75642e62696771756572792e73746f726167652e7631221c0a0a4176726f536368656d61120e0a06736368656d61180120012809223d0a084176726f526f7773121e0a1673657269616c697a65645f62696e6172795f726f777318012001280c12110a09726f775f636f756e7418022001280342c2010a24636f6d2e676f6f676c652e636c6f75642e62696771756572792e73746f726167652e763142094176726f50726f746f50015a47676f6f676c652e676f6c616e672e6f72672f67656e70726f746f2f676f6f676c65617069732f636c6f75642f62696771756572792f73746f726167652f76313b73746f72616765aa0220476f6f676c652e436c6f75642e42696751756572792e53746f726167652e5631ca0220476f6f676c655c436c6f75645c42696751756572795c53746f726167655c5631620670726f746f33"
-        ), true);
+        $pool->internalAddGeneratedFile(
+            '
+ù
++google/cloud/bigquery/storage/v1/avro.proto google.cloud.bigquery.storage.v1"
+
+AvroSchema
+schema (	"=
+AvroRows
+serialized_binary_rows (
+	row_count (BÂ
+$com.google.cloud.bigquery.storage.v1B	AvroProtoPZGgoogle.golang.org/genproto/googleapis/cloud/bigquery/storage/v1;storageª Google.Cloud.BigQuery.Storage.V1Ê Google\\Cloud\\BigQuery\\Storage\\V1bproto3'
+        , true);
 
         static::$is_initialized = true;
     }
