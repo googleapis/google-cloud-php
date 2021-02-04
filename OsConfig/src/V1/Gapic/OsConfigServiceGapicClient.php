dca1d30c4c8eedf9555bc49c4e5e8664d0435eaf
yoshi-automation <yoshi-automation@google.com>
2021-02-04 05:18:40 -0800

    chore: update gapic-generator to v2.8.0
    PiperOrigin-RevId: 342906697
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Nov 17 11:27:56 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 5fdb685a684269e07c10c7518372eb5d7b6bc0a9
    Source-Link: https://github.com/googleapis/googleapis/commit/5fdb685a684269e07c10c7518372eb5d7b6bc0a9

--- OsConfig/src/V1/Gapic/OsConfigServiceGapicClient.php
+++ OsConfig/src/V1/Gapic/OsConfigServiceGapicClient.php
@@ -126,7 +126,7 @@ class OsConfigServiceGapicClient
             'descriptorsConfigPath' => __DIR__.'/../resources/os_config_service_descriptor_config.php',
             'gcpApiConfigPath' => __DIR__.'/../resources/os_config_service_grpc_config.json',
             'credentialsConfig' => [
-                'scopes' => self::$serviceScopes,
+                'defaultScopes' => self::$serviceScopes,
             ],
             'transportConfig' => [
                 'rest' => [
