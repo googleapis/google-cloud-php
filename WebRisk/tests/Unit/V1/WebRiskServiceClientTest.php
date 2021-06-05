d13a4a41153280a8c93c05bc0fb9f7ee1f028e67
yoshi-automation <yoshi-automation@google.com>
2021-06-05 15:01:12 +0000

    chore: migrate webrisk/{v1,v1beta} to the PHP microgenerator
    Committer: @miraleung
    PiperOrigin-RevId: 369293612
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Apr 19 13:44:35 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 0ad68c9f562e431ca1cbfa60a089af63d04b9714
    Source-Link: https://github.com/googleapis/googleapis/commit/0ad68c9f562e431ca1cbfa60a089af63d04b9714

--- WebRisk/tests/Unit/V1/WebRiskServiceClientTest.php
+++ WebRisk/tests/Unit/V1/WebRiskServiceClientTest.php
@@ -20,12 +20,14 @@
  * This file was automatically generated - do not edit!
  */
 
+declare(strict_types=1);
+
 namespace Google\Cloud\WebRisk\Tests\Unit\V1;
 
-use Google\Cloud\WebRisk\V1\WebRiskServiceClient;
 use Google\ApiCore\ApiException;
 use Google\ApiCore\CredentialsWrapper;
 use Google\ApiCore\Testing\GeneratedTest;
+
 use Google\ApiCore\Testing\MockTransport;
 use Google\Cloud\WebRisk\V1\ComputeThreatListDiffRequest\Constraints;
 use Google\Cloud\WebRisk\V1\ComputeThreatListDiffResponse;
@@ -33,12 +35,13 @@ use Google\Cloud\WebRisk\V1\SearchHashesResponse;
 use Google\Cloud\WebRisk\V1\SearchUrisResponse;
 use Google\Cloud\WebRisk\V1\Submission;
 use Google\Cloud\WebRisk\V1\ThreatType;
-use Google\Protobuf\Any;
+use Google\Cloud\WebRisk\V1\WebRiskServiceClient;
 use Google\Rpc\Code;
 use stdClass;
 
 /**
  * @group webrisk
+ *
  * @group gapic
  */
 class WebRiskServiceClientTest extends GeneratedTest
@@ -56,9 +59,7 @@ class WebRiskServiceClientTest extends GeneratedTest
      */
     private function createCredentials()
     {
-        return $this->getMockBuilder(CredentialsWrapper::class)
-            ->disableOriginalConstructor()
-            ->getMock();
+        return $this->getMockBuilder(CredentialsWrapper::class)->disableOriginalConstructor()->getMock();
     }
 
     /**
@@ -69,7 +70,6 @@ class WebRiskServiceClientTest extends GeneratedTest
         $options += [
             'credentials' => $this->createCredentials(),
         ];
-
         return new WebRiskServiceClient($options);
     }
 
@@ -79,20 +79,18 @@ class WebRiskServiceClientTest extends GeneratedTest
     public function computeThreatListDiffTest()
     {
         $transport = $this->createTransport();
-        $client = $this->createClient(['transport' => $transport]);
-
+        $client = $this->createClient([
+            'transport' => $transport,
+        ]);
         $this->assertTrue($transport->isExhausted());
-
         // Mock response
         $newVersionToken = '115';
         $expectedResponse = new ComputeThreatListDiffResponse();
         $expectedResponse->setNewVersionToken($newVersionToken);
         $transport->addResponse($expectedResponse);
-
         // Mock request
         $threatType = ThreatType::THREAT_TYPE_UNSPECIFIED;
         $constraints = new Constraints();
-
         $response = $client->computeThreatListDiff($threatType, $constraints);
         $this->assertEquals($expectedResponse, $response);
         $actualRequests = $transport->popReceivedCalls();
@@ -100,14 +98,10 @@ class WebRiskServiceClientTest extends GeneratedTest
         $actualFuncCall = $actualRequests[0]->getFuncCall();
         $actualRequestObject = $actualRequests[0]->getRequestObject();
         $this->assertSame('/google.cloud.webrisk.v1.WebRiskService/ComputeThreatListDiff', $actualFuncCall);
-
         $actualValue = $actualRequestObject->getThreatType();
-
         $this->assertProtobufEquals($threatType, $actualValue);
         $actualValue = $actualRequestObject->getConstraints();
-
         $this->assertProtobufEquals($constraints, $actualValue);
-
         $this->assertTrue($transport->isExhausted());
     }
 
@@ -117,26 +111,23 @@ class WebRiskServiceClientTest extends GeneratedTest
     public function computeThreatListDiffExceptionTest()
     {
         $transport = $this->createTransport();
-        $client = $this->createClient(['transport' => $transport]);
-
+        $client = $this->createClient([
+            'transport' => $transport,
+        ]);
         $this->assertTrue($transport->isExhausted());
-
         $status = new stdClass();
         $status->code = Code::DATA_LOSS;
         $status->details = 'internal error';
-
-        $expectedExceptionMessage = json_encode([
-           'message' => 'internal error',
-           'code' => Code::DATA_LOSS,
-           'status' => 'DATA_LOSS',
-           'details' => [],
+        $expectedExceptionMessage  = json_encode([
+            'message' => 'internal error',
+            'code' => Code::DATA_LOSS,
+            'status' => 'DATA_LOSS',
+            'details' => [],
         ], JSON_PRETTY_PRINT);
         $transport->addResponse(null, $status);
-
         // Mock request
         $threatType = ThreatType::THREAT_TYPE_UNSPECIFIED;
         $constraints = new Constraints();
-
         try {
             $client->computeThreatListDiff($threatType, $constraints);
             // If the $client method call did not throw, fail the test
@@ -145,154 +136,6 @@ class WebRiskServiceClientTest extends GeneratedTest
             $this->assertEquals($status->code, $ex->getCode());
             $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
         }
-
-        // Call popReceivedCalls to ensure the stub is exhausted
-        $transport->popReceivedCalls();
-        $this->assertTrue($transport->isExhausted());
-    }
-
-    /**
-     * @test
-     */
-    public function searchUrisTest()
-    {
-        $transport = $this->createTransport();
-        $client = $this->createClient(['transport' => $transport]);
-
-        $this->assertTrue($transport->isExhausted());
-
-        // Mock response
-        $expectedResponse = new SearchUrisResponse();
-        $transport->addResponse($expectedResponse);
-
-        // Mock request
-        $uri = 'uri116076';
-        $threatTypes = [];
-
-        $response = $client->searchUris($uri, $threatTypes);
-        $this->assertEquals($expectedResponse, $response);
-        $actualRequests = $transport->popReceivedCalls();
-        $this->assertSame(1, count($actualRequests));
-        $actualFuncCall = $actualRequests[0]->getFuncCall();
-        $actualRequestObject = $actualRequests[0]->getRequestObject();
-        $this->assertSame('/google.cloud.webrisk.v1.WebRiskService/SearchUris', $actualFuncCall);
-
-        $actualValue = $actualRequestObject->getUri();
-
-        $this->assertProtobufEquals($uri, $actualValue);
-        $actualValue = $actualRequestObject->getThreatTypes();
-
-        $this->assertProtobufEquals($threatTypes, $actualValue);
-
-        $this->assertTrue($transport->isExhausted());
-    }
-
-    /**
-     * @test
-     */
-    public function searchUrisExceptionTest()
-    {
-        $transport = $this->createTransport();
-        $client = $this->createClient(['transport' => $transport]);
-
-        $this->assertTrue($transport->isExhausted());
-
-        $status = new stdClass();
-        $status->code = Code::DATA_LOSS;
-        $status->details = 'internal error';
-
-        $expectedExceptionMessage = json_encode([
-           'message' => 'internal error',
-           'code' => Code::DATA_LOSS,
-           'status' => 'DATA_LOSS',
-           'details' => [],
-        ], JSON_PRETTY_PRINT);
-        $transport->addResponse(null, $status);
-
-        // Mock request
-        $uri = 'uri116076';
-        $threatTypes = [];
-
-        try {
-            $client->searchUris($uri, $threatTypes);
-            // If the $client method call did not throw, fail the test
-            $this->fail('Expected an ApiException, but no exception was thrown.');
-        } catch (ApiException $ex) {
-            $this->assertEquals($status->code, $ex->getCode());
-            $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
-        }
-
-        // Call popReceivedCalls to ensure the stub is exhausted
-        $transport->popReceivedCalls();
-        $this->assertTrue($transport->isExhausted());
-    }
-
-    /**
-     * @test
-     */
-    public function searchHashesTest()
-    {
-        $transport = $this->createTransport();
-        $client = $this->createClient(['transport' => $transport]);
-
-        $this->assertTrue($transport->isExhausted());
-
-        // Mock response
-        $expectedResponse = new SearchHashesResponse();
-        $transport->addResponse($expectedResponse);
-
-        // Mock request
-        $threatTypes = [];
-
-        $response = $client->searchHashes($threatTypes);
-        $this->assertEquals($expectedResponse, $response);
-        $actualRequests = $transport->popReceivedCalls();
-        $this->assertSame(1, count($actualRequests));
-        $actualFuncCall = $actualRequests[0]->getFuncCall();
-        $actualRequestObject = $actualRequests[0]->getRequestObject();
-        $this->assertSame('/google.cloud.webrisk.v1.WebRiskService/SearchHashes', $actualFuncCall);
-
-        $actualValue = $actualRequestObject->getThreatTypes();
-
-        $this->assertProtobufEquals($threatTypes, $actualValue);
-
-        $this->assertTrue($transport->isExhausted());
-    }
-
-    /**
-     * @test
-     */
-    public function searchHashesExceptionTest()
-    {
-        $transport = $this->createTransport();
-        $client = $this->createClient(['transport' => $transport]);
-
-        $this->assertTrue($transport->isExhausted());
-
-        $status = new stdClass();
-        $status->code = Code::DATA_LOSS;
-        $status->details = 'internal error';
-
-        $expectedExceptionMessage = json_encode([
-           'message' => 'internal error',
-           'code' => Code::DATA_LOSS,
-           'status' => 'DATA_LOSS',
-           'details' => [],
-        ], JSON_PRETTY_PRINT);
-        $transport->addResponse(null, $status);
-
-        // Mock request
-        $threatTypes = [];
-
-        try {
-            $client->searchHashes($threatTypes);
-            // If the $client method call did not throw, fail the test
-            $this->fail('Expected an ApiException, but no exception was thrown.');
-        } catch (ApiException $ex) {
-            $this->assertEquals($status->code, $ex->getCode());
-            $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
-        }
-
         // Call popReceivedCalls to ensure the stub is exhausted
         $transport->popReceivedCalls();
         $this->assertTrue($transport->isExhausted());
@@ -304,20 +147,18 @@ class WebRiskServiceClientTest extends GeneratedTest
     public function createSubmissionTest()
     {
         $transport = $this->createTransport();
-        $client = $this->createClient(['transport' => $transport]);
-
+        $client = $this->createClient([
+            'transport' => $transport,
+        ]);
         $this->assertTrue($transport->isExhausted());
-
         // Mock response
         $uri = 'uri116076';
         $expectedResponse = new Submission();
         $expectedResponse->setUri($uri);
         $transport->addResponse($expectedResponse);
-
         // Mock request
         $formattedParent = $client->projectName('[PROJECT]');
         $submission = new Submission();
-
         $response = $client->createSubmission($formattedParent, $submission);
         $this->assertEquals($expectedResponse, $response);
         $actualRequests = $transport->popReceivedCalls();
@@ -325,14 +166,10 @@ class WebRiskServiceClientTest extends GeneratedTest
         $actualFuncCall = $actualRequests[0]->getFuncCall();
         $actualRequestObject = $actualRequests[0]->getRequestObject();
         $this->assertSame('/google.cloud.webrisk.v1.WebRiskService/CreateSubmission', $actualFuncCall);
-
         $actualValue = $actualRequestObject->getParent();
-
         $this->assertProtobufEquals($formattedParent, $actualValue);
         $actualValue = $actualRequestObject->getSubmission();
-
         $this->assertProtobufEquals($submission, $actualValue);
-
         $this->assertTrue($transport->isExhausted());
     }
 
@@ -342,26 +179,23 @@ class WebRiskServiceClientTest extends GeneratedTest
     public function createSubmissionExceptionTest()
     {
         $transport = $this->createTransport();
-        $client = $this->createClient(['transport' => $transport]);
-
+        $client = $this->createClient([
+            'transport' => $transport,
+        ]);
         $this->assertTrue($transport->isExhausted());
-
         $status = new stdClass();
         $status->code = Code::DATA_LOSS;
         $status->details = 'internal error';
-
-        $expectedExceptionMessage = json_encode([
-           'message' => 'internal error',
-           'code' => Code::DATA_LOSS,
-           'status' => 'DATA_LOSS',
-           'details' => [],
+        $expectedExceptionMessage  = json_encode([
+            'message' => 'internal error',
+            'code' => Code::DATA_LOSS,
+            'status' => 'DATA_LOSS',
+            'details' => [],
         ], JSON_PRETTY_PRINT);
         $transport->addResponse(null, $status);
-
         // Mock request
         $formattedParent = $client->projectName('[PROJECT]');
         $submission = new Submission();
-
         try {
             $client->createSubmission($formattedParent, $submission);
             // If the $client method call did not throw, fail the test
@@ -370,7 +204,134 @@ class WebRiskServiceClientTest extends GeneratedTest
             $this->assertEquals($status->code, $ex->getCode());
             $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
         }
+        // Call popReceivedCalls to ensure the stub is exhausted
+        $transport->popReceivedCalls();
+        $this->assertTrue($transport->isExhausted());
+    }
 
+    /**
+     * @test
+     */
+    public function searchHashesTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient([
+            'transport' => $transport,
+        ]);
+        $this->assertTrue($transport->isExhausted());
+        // Mock response
+        $expectedResponse = new SearchHashesResponse();
+        $transport->addResponse($expectedResponse);
+        // Mock request
+        $threatTypes = [];
+        $response = $client->searchHashes($threatTypes);
+        $this->assertEquals($expectedResponse, $response);
+        $actualRequests = $transport->popReceivedCalls();
+        $this->assertSame(1, count($actualRequests));
+        $actualFuncCall = $actualRequests[0]->getFuncCall();
+        $actualRequestObject = $actualRequests[0]->getRequestObject();
+        $this->assertSame('/google.cloud.webrisk.v1.WebRiskService/SearchHashes', $actualFuncCall);
+        $actualValue = $actualRequestObject->getThreatTypes();
+        $this->assertProtobufEquals($threatTypes, $actualValue);
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function searchHashesExceptionTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient([
+            'transport' => $transport,
+        ]);
+        $this->assertTrue($transport->isExhausted());
+        $status = new stdClass();
+        $status->code = Code::DATA_LOSS;
+        $status->details = 'internal error';
+        $expectedExceptionMessage  = json_encode([
+            'message' => 'internal error',
+            'code' => Code::DATA_LOSS,
+            'status' => 'DATA_LOSS',
+            'details' => [],
+        ], JSON_PRETTY_PRINT);
+        $transport->addResponse(null, $status);
+        // Mock request
+        $threatTypes = [];
+        try {
+            $client->searchHashes($threatTypes);
+            // If the $client method call did not throw, fail the test
+            $this->fail('Expected an ApiException, but no exception was thrown.');
+        } catch (ApiException $ex) {
+            $this->assertEquals($status->code, $ex->getCode());
+            $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
+        }
+        // Call popReceivedCalls to ensure the stub is exhausted
+        $transport->popReceivedCalls();
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function searchUrisTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient([
+            'transport' => $transport,
+        ]);
+        $this->assertTrue($transport->isExhausted());
+        // Mock response
+        $expectedResponse = new SearchUrisResponse();
+        $transport->addResponse($expectedResponse);
+        // Mock request
+        $uri = 'uri116076';
+        $threatTypes = [];
+        $response = $client->searchUris($uri, $threatTypes);
+        $this->assertEquals($expectedResponse, $response);
+        $actualRequests = $transport->popReceivedCalls();
+        $this->assertSame(1, count($actualRequests));
+        $actualFuncCall = $actualRequests[0]->getFuncCall();
+        $actualRequestObject = $actualRequests[0]->getRequestObject();
+        $this->assertSame('/google.cloud.webrisk.v1.WebRiskService/SearchUris', $actualFuncCall);
+        $actualValue = $actualRequestObject->getUri();
+        $this->assertProtobufEquals($uri, $actualValue);
+        $actualValue = $actualRequestObject->getThreatTypes();
+        $this->assertProtobufEquals($threatTypes, $actualValue);
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function searchUrisExceptionTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient([
+            'transport' => $transport,
+        ]);
+        $this->assertTrue($transport->isExhausted());
+        $status = new stdClass();
+        $status->code = Code::DATA_LOSS;
+        $status->details = 'internal error';
+        $expectedExceptionMessage  = json_encode([
+            'message' => 'internal error',
+            'code' => Code::DATA_LOSS,
+            'status' => 'DATA_LOSS',
+            'details' => [],
+        ], JSON_PRETTY_PRINT);
+        $transport->addResponse(null, $status);
+        // Mock request
+        $uri = 'uri116076';
+        $threatTypes = [];
+        try {
+            $client->searchUris($uri, $threatTypes);
+            // If the $client method call did not throw, fail the test
+            $this->fail('Expected an ApiException, but no exception was thrown.');
+        } catch (ApiException $ex) {
+            $this->assertEquals($status->code, $ex->getCode());
+            $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
+        }
         // Call popReceivedCalls to ensure the stub is exhausted
         $transport->popReceivedCalls();
         $this->assertTrue($transport->isExhausted());
