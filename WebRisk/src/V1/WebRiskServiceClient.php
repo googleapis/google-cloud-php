bac75c703cf26f3068563de1c5b5170e1e65dda1
yoshi-automation <yoshi-automation@google.com>
2021-06-05 15:11:17 +0000

    fix!: Fixing PHP, Ruby and C# package names for the CloudDMS API. This is a breaking change for the above languages.
    Committer: @jmpfar
    PiperOrigin-RevId: 373087373
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon May 10 23:27:28 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 6c96d191b31797c48f7383940e7f8422098cf6f5
    Source-Link: https://github.com/googleapis/googleapis/commit/6c96d191b31797c48f7383940e7f8422098cf6f5

--- WebRisk/src/V1/WebRiskServiceClient.php
+++ WebRisk/src/V1/WebRiskServiceClient.php
@@ -22,8 +22,6 @@
  * Updates to the above are reflected here through a refresh process.
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\WebRisk\V1;
 
 use Google\Cloud\WebRisk\V1\Gapic\WebRiskServiceGapicClient;
--- WebRisk/src/V1beta1/WebRiskServiceV1Beta1Client.php
+++ WebRisk/src/V1beta1/WebRiskServiceV1Beta1Client.php
@@ -24,8 +24,6 @@
  * @experimental
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\WebRisk\V1beta1;
 
 use Google\Cloud\WebRisk\V1beta1\Gapic\WebRiskServiceV1Beta1GapicClient;
