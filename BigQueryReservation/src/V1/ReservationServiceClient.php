0925f59dee5a694ff6110b36240a19406dee3a6d
yoshi-automation <yoshi-automation@google.com>
2021-06-05 10:55:49 +0000

    fix!: Fixing PHP, Ruby and C# package names for the CloudDMS API. This is a breaking change for the above languages.
    Committer: @jmpfar
    PiperOrigin-RevId: 373087373
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon May 10 23:27:28 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 6c96d191b31797c48f7383940e7f8422098cf6f5
    Source-Link: https://github.com/googleapis/googleapis/commit/6c96d191b31797c48f7383940e7f8422098cf6f5

--- BigQueryReservation/src/V1/ReservationServiceClient.php
+++ BigQueryReservation/src/V1/ReservationServiceClient.php
@@ -22,8 +22,6 @@
  * Updates to the above are reflected here through a refresh process.
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\BigQuery\Reservation\V1;
 
 use Google\Cloud\BigQuery\Reservation\V1\Gapic\ReservationServiceGapicClient;
