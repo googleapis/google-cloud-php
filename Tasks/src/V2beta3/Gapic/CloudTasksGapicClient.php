20e01c652032b8e06d46d230cef7a184707313c9
yoshi-automation <yoshi-automation@google.com>
2021-06-05 14:27:24 +0000

    fix!: BREAKING_CHANGE: remove display_name from FeatureStore feat: add invalid_row_count to ImportFeatureValuesResponse and ImportFeatureValuesOperationMetadata
    PiperOrigin-RevId: 372952726
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon May 10 10:02:01 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: b96db5aeb026e2c6f4fdeeacd8e5a2fc5fa14930
    Source-Link: https://github.com/googleapis/googleapis/commit/b96db5aeb026e2c6f4fdeeacd8e5a2fc5fa14930

--- Tasks/src/V2beta3/Gapic/CloudTasksGapicClient.php
+++ Tasks/src/V2beta3/Gapic/CloudTasksGapicClient.php
@@ -24,8 +24,6 @@
  * @experimental
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\Tasks\V2beta3\Gapic;
 
 use Google\ApiCore\ApiException;
--- Tasks/tests/Unit/V2beta3/CloudTasksClientTest.php
+++ Tasks/tests/Unit/V2beta3/CloudTasksClientTest.php
@@ -20,8 +20,6 @@
  * This file was automatically generated - do not edit!
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\Tasks\Tests\Unit\V2beta3;
 
 use Google\ApiCore\ApiException;
