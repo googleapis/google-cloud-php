e81ca2e0777b9ed5589e17e8e6b0f248c9498308
yoshi-automation <yoshi-automation@google.com>
2021-06-05 11:42:24 +0000

    chore: Update privatecatalog_v1 BUILD.bazel for python
    PiperOrigin-RevId: 376205836
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu May 27 11:02:53 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 0e8432b084252dc86d0dc6fce7c7566a659404a9
    Source-Link: https://github.com/googleapis/googleapis/commit/0e8432b084252dc86d0dc6fce7c7566a659404a9

--- Dialogflow/src/V2/FulfillmentsClient.php
+++ Dialogflow/src/V2/FulfillmentsClient.php
@@ -22,8 +22,6 @@
  * Updates to the above are reflected here through a refresh process.
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\Dialogflow\V2;
 
 use Google\Cloud\Dialogflow\V2\Gapic\FulfillmentsGapicClient;
