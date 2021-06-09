c36e1719bc9ec6ec199269b9fce7db76f7c53727
yoshi-automation <yoshi-automation@google.com>
2021-06-09 00:55:13 +0000

    fix!: BREAKING_CHANGE: remove display_name from FeatureStore feat: add invalid_row_count to ImportFeatureValuesResponse and ImportFeatureValuesOperationMetadata
    PiperOrigin-RevId: 372952726
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon May 10 10:02:01 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: b96db5aeb026e2c6f4fdeeacd8e5a2fc5fa14930
    Source-Link: https://github.com/googleapis/googleapis/commit/b96db5aeb026e2c6f4fdeeacd8e5a2fc5fa14930

--- VideoIntelligence/src/V1/VideoIntelligenceServiceClient.php
+++ VideoIntelligence/src/V1/VideoIntelligenceServiceClient.php
@@ -22,8 +22,6 @@
  * Updates to the above are reflected here through a refresh process.
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\VideoIntelligence\V1;
 
 use Google\Cloud\VideoIntelligence\V1\Gapic\VideoIntelligenceServiceGapicClient;
