5e66f7b1530506705482cec54b460a5eb221d57a
yoshi-automation <yoshi-automation@google.com>
2021-06-09 00:20:01 +0000

    feat: add canonical_name and folder fields
    Assets and findings may be created in organizations, folders or projects, however in the current API version the name field for resources will always be prefixed by "organizations/...", for historical reasons. The "canonical_name" field shows the actual location of the resource, and in requests either the name or "canonical name" can be used for the "name" in the request.
    
    The "folder" field contains the folders in the resource's ancestry.
    
    PiperOrigin-RevId: 372964926
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon May 10 10:52:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 1d3573f5124e2f9ffdd174c6992269ea7f1e92a6
    Source-Link: https://github.com/googleapis/googleapis/commit/1d3573f5124e2f9ffdd174c6992269ea7f1e92a6

--- SecretManager/src/V1beta1/Gapic/SecretManagerServiceGapicClient.php
+++ SecretManager/src/V1beta1/Gapic/SecretManagerServiceGapicClient.php
@@ -24,8 +24,6 @@
  * @experimental
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\SecretManager\V1beta1\Gapic;
 
 use Google\ApiCore\ApiException;
--- SecretManager/src/V1beta1/SecretManagerServiceClient.php
+++ SecretManager/src/V1beta1/SecretManagerServiceClient.php
@@ -24,8 +24,6 @@
  * @experimental
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\SecretManager\V1beta1;
 
 use Google\Cloud\SecretManager\V1beta1\Gapic\SecretManagerServiceGapicClient;
