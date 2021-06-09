4fe5c40556a8d0e2e8d45ffd662be3e06fed09c7
yoshi-automation <yoshi-automation@google.com>
2021-06-09 10:29:39 +0000

    feat: Add Pub/Sub Lite Reservation APIs
    PiperOrigin-RevId: 377060979
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jun 2 08:09:14 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 51d99df0295d5cebed2b0ba41ce629c42ea8ca65
    Source-Link: https://github.com/googleapis/googleapis/commit/51d99df0295d5cebed2b0ba41ce629c42ea8ca65

--- OsConfig/src/V1/OsConfigServiceClient.php
+++ OsConfig/src/V1/OsConfigServiceClient.php
@@ -22,8 +22,6 @@
  * Updates to the above are reflected here through a refresh process.
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\OsConfig\V1;
 
 use Google\Cloud\OsConfig\V1\Gapic\OsConfigServiceGapicClient;
