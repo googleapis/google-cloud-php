813aa9fee01738dfc3e2befa59fdd42234291047
yoshi-automation <yoshi-automation@google.com>
2021-06-09 01:08:24 +0000

    fix!: BREAKING_CHANGE: remove display_name from FeatureStore feat: add invalid_row_count to ImportFeatureValuesResponse and ImportFeatureValuesOperationMetadata
    PiperOrigin-RevId: 372952726
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon May 10 10:02:01 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: b96db5aeb026e2c6f4fdeeacd8e5a2fc5fa14930
    Source-Link: https://github.com/googleapis/googleapis/commit/b96db5aeb026e2c6f4fdeeacd8e5a2fc5fa14930

--- WebSecurityScanner/src/V1beta/WebSecurityScannerClient.php
+++ WebSecurityScanner/src/V1beta/WebSecurityScannerClient.php
@@ -24,8 +24,6 @@
  * @experimental
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\WebSecurityScanner\V1beta;
 
 use Google\Cloud\WebSecurityScanner\V1beta\Gapic\WebSecurityScannerGapicClient;
