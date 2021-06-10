21f924bce6ac942c6db7e89dff1f189cf47cb062
yoshi-automation <yoshi-automation@google.com>
2021-06-10 10:11:47 +0000

    chore: regenerate API index
    Source-Author: Google APIs <runner@fv-az198-569.gjkxsqtjg41etoza45cw35jkyb.bx.internal.cloudapp.net>
    Source-Date: Wed Jun 2 17:53:51 2021 +0000
    Source-Repo: googleapis/googleapis
    Source-Sha: b6b589adde1c0341fae4c3c955b88bcb3a800934
    Source-Link: https://github.com/googleapis/googleapis/commit/b6b589adde1c0341fae4c3c955b88bcb3a800934

--- SecurityPrivateCa/src/V1beta1/CertificateAuthorityServiceClient.php
+++ SecurityPrivateCa/src/V1beta1/CertificateAuthorityServiceClient.php
@@ -24,8 +24,6 @@
  * @experimental
  */
 
-declare(strict_types=1);
-
 namespace Google\Cloud\Security\PrivateCA\V1beta1;
 
 use Google\Cloud\Security\PrivateCA\V1beta1\Gapic\CertificateAuthorityServiceGapicClient;
