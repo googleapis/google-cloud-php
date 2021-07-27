9f0bb034bcfb99a467a3b0263b48dc82d1f87fe9
yoshi-automation <yoshi-automation@google.com>
2021-07-27 10:08:24 +0000

    feat: add finding_class and indicator fields in Finding
    finding_class field is added in order to help our customer to quickly identify and categorize the different findings (threat, vulnerability, misconfiguration, ...). And indicator field is generated to indicate the domain and IP addresses that is causing the threat.
    
    PiperOrigin-RevId: 386975549
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Jul 26 15:39:44 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 5bf36f20f8e49c18620bf20a45645310f5bf6950
    Source-Link: https://github.com/googleapis/googleapis/commit/5bf36f20f8e49c18620bf20a45645310f5bf6950

--- SecurityCenter/src/V1/Gapic/SecurityCenterGapicClient.php
+++ SecurityCenter/src/V1/Gapic/SecurityCenterGapicClient.php
@@ -956,8 +956,8 @@ class SecurityCenterGapicClient
      * @param string  $findingId    Required. Unique identifier provided by the client within the parent scope.
      *                              It must be alphanumeric and less than or equal to 32 characters and
      *                              greater than 0 characters in length.
-     * @param Finding $finding      Required. The Finding being created. The name and security_marks will be
-     *                              ignored as they are both output only fields on this resource.
+     * @param Finding $finding      Required. The Finding being created. The name and security_marks will be ignored as
+     *                              they are both output only fields on this resource.
      * @param array   $optionalArgs {
      *     Optional.
      *
@@ -1001,15 +1001,14 @@ class SecurityCenterGapicClient
      * }
      * ```
      *
-     * @param string             $parent             Required. Resource name of the new notification config's parent. Its format
-     *                                               is "organizations/[organization_id]".
+     * @param string             $parent             Required. Resource name of the new notification config's parent. Its format is
+     *                                               "organizations/[organization_id]".
      * @param string             $configId           Required.
      *                                               Unique identifier provided by the client within the parent scope.
      *                                               It must be between 1 and 128 characters, and contains alphanumeric
      *                                               characters, underscores or hyphens only.
-     * @param NotificationConfig $notificationConfig Required. The notification config being created. The name and the service
-     *                                               account will be ignored as they are both output only fields on this
-     *                                               resource.
+     * @param NotificationConfig $notificationConfig Required. The notification config being created. The name and the service account
+     *                                               will be ignored as they are both output only fields on this resource.
      * @param array              $optionalArgs       {
      *     Optional.
      *
@@ -1054,8 +1053,8 @@ class SecurityCenterGapicClient
      *
      * @param string $parent       Required. Resource name of the new source's parent. Its format should be
      *                             "organizations/[organization_id]".
-     * @param Source $source       Required. The Source being created, only the display_name and description
-     *                             will be used. All other fields will be ignored.
+     * @param Source $source       Required. The Source being created, only the display_name and description will be
+     *                             used. All other fields will be ignored.
      * @param array  $optionalArgs {
      *     Optional.
      *
@@ -1224,8 +1223,8 @@ class SecurityCenterGapicClient
      * }
      * ```
      *
-     * @param string $name         Required. Name of the organization to get organization settings for. Its
-     *                             format is "organizations/[organization_id]/organizationSettings".
+     * @param string $name         Required. Name of the organization to get organization settings for. Its format is
+     *                             "organizations/[organization_id]/organizationSettings".
      * @param array  $optionalArgs {
      *     Optional.
      *
@@ -1323,9 +1322,9 @@ class SecurityCenterGapicClient
      * @param string $parent       Required. Name of the organization to groupBy. Its format is
      *                             "organizations/[organization_id], folders/[folder_id], or
      *                             projects/[project_id]".
-     * @param string $groupBy      Required. Expression that defines what assets fields to use for grouping.
-     *                             The string value should follow SQL syntax: comma separated list of fields.
-     *                             For example:
+     * @param string $groupBy      Required. Expression that defines what assets fields to use for grouping. The string
+     *                             value should follow SQL syntax: comma separated list of fields. For
+     *                             example:
      *                             "security_center_properties.resource_project,security_center_properties.project".
      *
      *                             The following fields are supported when compare_duration is not set:
@@ -1532,9 +1531,9 @@ class SecurityCenterGapicClient
      *                             provide a source_id of `-`. For example:
      *                             organizations/{organization_id}/sources/-, folders/{folder_id}/sources/-,
      *                             or projects/{project_id}/sources/-
-     * @param string $groupBy      Required. Expression that defines what assets fields to use for grouping
-     *                             (including `state_change`). The string value should follow SQL syntax:
-     *                             comma separated list of fields. For example: "parent,resource_name".
+     * @param string $groupBy      Required. Expression that defines what assets fields to use for grouping (including
+     *                             `state_change`). The string value should follow SQL syntax: comma separated
+     *                             list of fields. For example: "parent,resource_name".
      *
      *                             The following fields are supported:
      *
@@ -2223,8 +2222,8 @@ class SecurityCenterGapicClient
      * }
      * ```
      *
-     * @param string $parent       Required. Resource name of the parent of sources to list. Its format should
-     *                             be "organizations/[organization_id], folders/[folder_id], or
+     * @param string $parent       Required. Resource name of the parent of sources to list. Its format should be
+     *                             "organizations/[organization_id], folders/[folder_id], or
      *                             projects/[project_id]".
      * @param array  $optionalArgs {
      *     Optional.
@@ -2312,8 +2311,8 @@ class SecurityCenterGapicClient
      * }
      * ```
      *
-     * @param string $parent       Required. Name of the organization to run asset discovery for. Its format
-     *                             is "organizations/[organization_id]".
+     * @param string $parent       Required. Name of the organization to run asset discovery for. Its format is
+     *                             "organizations/[organization_id]".
      * @param array  $optionalArgs {
      *     Optional.
      *
@@ -2498,8 +2497,8 @@ class SecurityCenterGapicClient
      * }
      * ```
      *
-     * @param Finding $finding      Required. The finding resource to update or create if it does not already
-     *                              exist. parent, security_marks, and update_time will be ignored.
+     * @param Finding $finding      Required. The finding resource to update or create if it does not already exist.
+     *                              parent, security_marks, and update_time will be ignored.
      *
      *                              In the case of creation, the finding id portion of the name must be
      *                              alphanumeric and less than or equal to 32 characters and greater than 0
