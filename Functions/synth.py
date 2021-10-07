e911a415ff9cdb103ebc361e895298c07cb0831a
David Supplee <dwsupplee@gmail.com>
2021-10-07 21:22:22 +0000

    remove unused synth commands

--- Functions/synth.py
+++ Functions/synth.py
@@ -72,12 +72,6 @@ s.replace(
     r'Copyright \d{4}',
     'Copyright 2021')
 
-# Change the wording for the deprecation warning.
-s.replace(
-    'src/*/*_*.php',
-    r'will be removed in the next major release',
-    'will be removed in a future release')
-
 ### [START] protoc backwards compatibility fixes
 
 # roll back to private properties.
@@ -93,13 +87,6 @@ s.replace(
     "src/**/V*/**/*.php",
     r"final class",
     r"class")
-
-# Replace "Unwrapped" with "Value" for method names.
-s.replace(
-    "src/**/V*/**/*.php",
-    r"public function ([s|g]\w{3,})Unwrapped",
-    r"public function \1Value"
-)
 
 ### [END] protoc backwards compatibility fixes
 
