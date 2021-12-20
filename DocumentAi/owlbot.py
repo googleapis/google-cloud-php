1ffe06c301303fb620309fb63cf7b000c9b5c25f
saranshdhingra <saranshdhingra@google.com>
2021-12-20 18:00:24 +0530

    chore: Migrate DocumentAi to OwlBot

--- DocumentAi/owlbot.py
+++ DocumentAi/owlbot.py
@@ -14,35 +14,40 @@
 
 """This script is used to synthesize generated parts of this library."""
 
-import subprocess
-import synthtool as s
-import synthtool.gcp as gcp
 import logging
+from pathlib import Path
+import subprocess
+
+import synthtool as s
+from synthtool.languages import php
+from synthtool import _tracked_paths
 
 logging.basicConfig(level=logging.DEBUG)
 
-gapic = gcp.GAPICBazel()
-common = gcp.CommonTemplates()
+src = Path(f"../{php.STAGING_DIR}/DocumentAi").resolve()
+dest = Path().resolve()
 
-library = gapic.php_library(
-    service='documentai',
-    version='V1',
-    bazel_target='//google/cloud/documentai/v1:google-cloud-documentai-v1-php',
+# Added so that we can pass copy_excludes in the owlbot_main() call
+_tracked_paths.add(src)
+
+php.owlbot_main(
+    src=src,
+    dest=dest,
+    copy_excludes=[
+        src / "**/*_*.php"
+    ]
 )
 
-# copy all src including partial veneer classes
-s.move(library / 'src')
+# remove class_alias code
+s.replace(
+    "src/V*/*/*.php",
+    r"^// Adding a class alias for backwards compatibility with the previous class name.$"
+    + "\n"
+    + r"^class_alias\(.*\);$"
+    + "\n",
+    '')
 
-# copy proto files to src also
-s.move(
-    sources=library / 'proto/src/Google/Cloud/DocumentAI',
-    destination='src/',
-    excludes=['**/*_*.php']
-)
-s.move(library / 'tests/')
 
-# copy GPBMetadata file to metadata
-s.move(library / 'proto/src/GPBMetadata/Google/Cloud/Documentai', 'metadata/')
 
 # document and utilize apiEndpoint instead of serviceAddress
 s.replace(
@@ -61,16 +66,6 @@ s.replace(
     "**/Gapic/*GapicClient.php",
     r"\$transportConfig, and any \$serviceAddress",
     r"$transportConfig, and any `$apiEndpoint`")
-
-# fix year
-s.replace(
-    '**/*Client.php',
-    r'Copyright \d{4}',
-    'Copyright 2021')
-s.replace(
-    'tests/**/*Test.php',
-    r'Copyright \d{4}',
-    'Copyright 2021')
 
 ### [START] protoc backwards compatibility fixes
 
