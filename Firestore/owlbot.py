d0cef1a95439a3d04f124e7872c8d88b0b94f004
Brent Shaffer <betterbrent@google.com>
2022-02-02 11:20:28 -0800

    chore: fix BC issues in owlbot and FirestoreAdminClient (#5085)

--- Firestore/owlbot.py
+++ Firestore/owlbot.py
@@ -46,7 +46,14 @@ php.owlbot_main(
 admin_library = Path(f"../{php.STAGING_DIR}/Firestore/v1/Admin").resolve()
 
 # copy all src
-s.move(admin_library / f'src', 'src/Admin', merge=preserve_copyright_year)
+s.move(
+    admin_library / f'src',
+    'src/Admin',
+    merge=preserve_copyright_year,
+    excludes=[
+        admin_library / 'src/V1/FirestoreAdminClient.php',
+    ]
+)
 
 # copy proto files to src also
 s.move(admin_library / f'proto/src/Google/Cloud/Firestore', f'src/', merge=preserve_copyright_year)
@@ -140,5 +147,16 @@ s.replace(
     r"\1](https://cloud.google.com\2)"
 )
 
-# Address breaking changes
-subprocess.run('git show 00edd91a0bcd94deccbeb312a5ddae4e81a178e0 | git apply', shell=True)
+# fix backwards-compatibility issues with LRO methods
+lro_methods = ['createIndex', 'exportDocuments', 'importDocuments', 'updateField']
+for lro_method in lro_methods:
+    s.replace(
+        "src/Admin/V1/Gapic/FirestoreAdminGapicClient.php",
+        rf'{lro_method}\(',
+        rf'{lro_method}LRO(',
+    )
+    s.replace(
+        "tests/Unit/Admin/V1/FirestoreAdminClientTest.php",
+        rf'{lro_method}\(',
+        rf'{lro_method}LRO(',
+    )
---
+++ Firestore/tests/Unit/Admin/V1/FirestoreAdminClientBCTest.php
@@ -0,0 +1,344 @@
+<?php
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+namespace Google\Cloud\Firestore\Admin\Tests\Unit\V1;
+
+use Google\ApiCore\ApiException;
+
+use Google\ApiCore\CredentialsWrapper;
+use Google\ApiCore\LongRunning\OperationsClient;
+use Google\ApiCore\Testing\GeneratedTest;
+
+use Google\ApiCore\Testing\MockTransport;
+use Google\Cloud\Firestore\Admin\V1\ExportDocumentsResponse;
+use Google\Cloud\Firestore\Admin\V1\Field;
+
+use Google\Cloud\Firestore\Admin\V1\FirestoreAdminClient;
+use Google\Cloud\Firestore\Admin\V1\Index;
+use Google\Cloud\Firestore\Admin\V1\ListFieldsResponse;
+use Google\Cloud\Firestore\Admin\V1\ListIndexesResponse;
+use Google\LongRunning\GetOperationRequest;
+use Google\LongRunning\Operation;
+use Google\Protobuf\Any;
+use Google\Protobuf\GPBEmpty;
+use Google\Rpc\Code;
+use stdClass;
+
+/**
+ * @group firestore-admin
+ *
+ * @group gapic
+ */
+class FirestoreAdminClientBCTest extends GeneratedTest
+{
+    /**
+     * @return TransportInterface
+     */
+    private function createTransport($deserialize = null)
+    {
+        return new MockTransport($deserialize);
+    }
+
+    /**
+     * @return CredentialsWrapper
+     */
+    private function createCredentials()
+    {
+        return $this->getMockBuilder(CredentialsWrapper::class)->disableOriginalConstructor()->getMock();
+    }
+
+    /**
+     * @return FirestoreAdminClient
+     */
+    private function createClient(array $options = [])
+    {
+        $options += [
+            'credentials' => $this->createCredentials(),
+        ];
+        return new FirestoreAdminClient($options);
+    }
+
+    /**
+     * @test
+     */
+    public function createIndexTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient(['transport' => $transport]);
+
+        $this->assertTrue($transport->isExhausted());
+
+        // Mock response
+        $name = 'name3373707';
+        $done = true;
+        $expectedResponse = new Operation();
+        $expectedResponse->setName($name);
+        $expectedResponse->setDone($done);
+        $transport->addResponse($expectedResponse);
+
+        // Mock request
+        $formattedParent = $client->parentName('[PROJECT]', '[DATABASE]', '[COLLECTION_ID]');
+        $index = new Index();
+
+        $response = $client->createIndex($formattedParent, $index);
+        $this->assertEquals($expectedResponse, $response);
+        $actualRequests = $transport->popReceivedCalls();
+        $this->assertSame(1, count($actualRequests));
+        $actualFuncCall = $actualRequests[0]->getFuncCall();
+        $actualRequestObject = $actualRequests[0]->getRequestObject();
+        $this->assertSame('/google.firestore.admin.v1.FirestoreAdmin/CreateIndex', $actualFuncCall);
+
+        $actualValue = $actualRequestObject->getParent();
+
+        $this->assertProtobufEquals($formattedParent, $actualValue);
+        $actualValue = $actualRequestObject->getIndex();
+
+        $this->assertProtobufEquals($index, $actualValue);
+
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function exportDocumentsTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient(['transport' => $transport]);
+
+        $this->assertTrue($transport->isExhausted());
+
+        // Mock response
+        $name2 = 'name2-1052831874';
+        $done = true;
+        $expectedResponse = new Operation();
+        $expectedResponse->setName($name2);
+        $expectedResponse->setDone($done);
+        $transport->addResponse($expectedResponse);
+
+        // Mock request
+        $formattedName = $client->databaseName('[PROJECT]', '[DATABASE]');
+
+        $response = $client->exportDocuments($formattedName);
+        $this->assertEquals($expectedResponse, $response);
+        $actualRequests = $transport->popReceivedCalls();
+        $this->assertSame(1, count($actualRequests));
+        $actualFuncCall = $actualRequests[0]->getFuncCall();
+        $actualRequestObject = $actualRequests[0]->getRequestObject();
+        $this->assertSame('/google.firestore.admin.v1.FirestoreAdmin/ExportDocuments', $actualFuncCall);
+
+        $actualValue = $actualRequestObject->getName();
+
+        $this->assertProtobufEquals($formattedName, $actualValue);
+
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function exportDocumentsExceptionTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient(['transport' => $transport]);
+
+        $this->assertTrue($transport->isExhausted());
+
+        $status = new stdClass();
+        $status->code = Code::DATA_LOSS;
+        $status->details = 'internal error';
+
+        $expectedExceptionMessage = json_encode([
+           'message' => 'internal error',
+           'code' => Code::DATA_LOSS,
+           'status' => 'DATA_LOSS',
+           'details' => [],
+        ], JSON_PRETTY_PRINT);
+        $transport->addResponse(null, $status);
+
+        // Mock request
+        $formattedName = $client->databaseName('[PROJECT]', '[DATABASE]');
+
+        try {
+            $client->exportDocuments($formattedName);
+            // If the $client method call did not throw, fail the test
+            $this->fail('Expected an ApiException, but no exception was thrown.');
+        } catch (ApiException $ex) {
+            $this->assertEquals($status->code, $ex->getCode());
+            $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
+        }
+
+        // Call popReceivedCalls to ensure the stub is exhausted
+        $transport->popReceivedCalls();
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function importDocumentsTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient(['transport' => $transport]);
+
+        $this->assertTrue($transport->isExhausted());
+
+        // Mock response
+        $name2 = 'name2-1052831874';
+        $done = true;
+        $expectedResponse = new Operation();
+        $expectedResponse->setName($name2);
+        $expectedResponse->setDone($done);
+        $transport->addResponse($expectedResponse);
+
+        // Mock request
+        $formattedName = $client->databaseName('[PROJECT]', '[DATABASE]');
+
+        $response = $client->importDocuments($formattedName);
+        $this->assertEquals($expectedResponse, $response);
+        $actualRequests = $transport->popReceivedCalls();
+        $this->assertSame(1, count($actualRequests));
+        $actualFuncCall = $actualRequests[0]->getFuncCall();
+        $actualRequestObject = $actualRequests[0]->getRequestObject();
+        $this->assertSame('/google.firestore.admin.v1.FirestoreAdmin/ImportDocuments', $actualFuncCall);
+
+        $actualValue = $actualRequestObject->getName();
+
+        $this->assertProtobufEquals($formattedName, $actualValue);
+
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function importDocumentsExceptionTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient(['transport' => $transport]);
+
+        $this->assertTrue($transport->isExhausted());
+
+        $status = new stdClass();
+        $status->code = Code::DATA_LOSS;
+        $status->details = 'internal error';
+
+        $expectedExceptionMessage = json_encode([
+           'message' => 'internal error',
+           'code' => Code::DATA_LOSS,
+           'status' => 'DATA_LOSS',
+           'details' => [],
+        ], JSON_PRETTY_PRINT);
+        $transport->addResponse(null, $status);
+
+        // Mock request
+        $formattedName = $client->databaseName('[PROJECT]', '[DATABASE]');
+
+        try {
+            $client->importDocuments($formattedName);
+            // If the $client method call did not throw, fail the test
+            $this->fail('Expected an ApiException, but no exception was thrown.');
+        } catch (ApiException $ex) {
+            $this->assertEquals($status->code, $ex->getCode());
+            $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
+        }
+
+        // Call popReceivedCalls to ensure the stub is exhausted
+        $transport->popReceivedCalls();
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function updateFieldTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient(['transport' => $transport]);
+
+        $this->assertTrue($transport->isExhausted());
+
+        // Mock response
+        $name = 'name3373707';
+        $done = true;
+        $expectedResponse = new Operation();
+        $expectedResponse->setName($name);
+        $expectedResponse->setDone($done);
+        $transport->addResponse($expectedResponse);
+
+        // Mock request
+        $field = new Field();
+
+        $response = $client->updateField($field);
+        $this->assertEquals($expectedResponse, $response);
+        $actualRequests = $transport->popReceivedCalls();
+        $this->assertSame(1, count($actualRequests));
+        $actualFuncCall = $actualRequests[0]->getFuncCall();
+        $actualRequestObject = $actualRequests[0]->getRequestObject();
+        $this->assertSame('/google.firestore.admin.v1.FirestoreAdmin/UpdateField', $actualFuncCall);
+
+        $actualValue = $actualRequestObject->getField();
+
+        $this->assertProtobufEquals($field, $actualValue);
+
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /**
+     * @test
+     */
+    public function updateFieldExceptionTest()
+    {
+        $transport = $this->createTransport();
+        $client = $this->createClient(['transport' => $transport]);
+
+        $this->assertTrue($transport->isExhausted());
+
+        $status = new stdClass();
+        $status->code = Code::DATA_LOSS;
+        $status->details = 'internal error';
+
+        $expectedExceptionMessage = json_encode([
+           'message' => 'internal error',
+           'code' => Code::DATA_LOSS,
+           'status' => 'DATA_LOSS',
+           'details' => [],
+        ], JSON_PRETTY_PRINT);
+        $transport->addResponse(null, $status);
+
+        // Mock request
+        $field = new Field();
+
+        try {
+            $client->updateField($field);
+            // If the $client method call did not throw, fail the test
+            $this->fail('Expected an ApiException, but no exception was thrown.');
+        } catch (ApiException $ex) {
+            $this->assertEquals($status->code, $ex->getCode());
+            $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
+        }
+
+        // Call popReceivedCalls to ensure the stub is exhausted
+        $transport->popReceivedCalls();
+        $this->assertTrue($transport->isExhausted());
+    }
+}
