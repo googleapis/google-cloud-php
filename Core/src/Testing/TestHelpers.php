c35b4c7da8b1250a959c02a174e489bae742f9a7
David Supplee <dwsupplee@gmail.com>
2022-05-04 15:00:29 -0700

    broaden regex

--- Core/src/Testing/TestHelpers.php
+++ Core/src/Testing/TestHelpers.php
@@ -113,7 +113,7 @@ class TestHelpers
             '/vendor/',
             '/dev/',
             new RegexFileFilter('/\w{0,}\/vendor\//'),
-            new RegexFileFilter('/\w{0,}\/V\d{1,}\//')
+            new RegexFileFilter('/\w{0,}\/V\d{1,}\w{0,}\//')
         ]);
         $coverage = new Coverage($scanner);
         $coverage->buildListToCover();
