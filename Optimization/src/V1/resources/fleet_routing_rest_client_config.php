15ab70d39af996a34338f641992ab3fdd014f41d
David Supplee <dwsupplee@gmail.com>
2022-05-13 14:53:30 -0700

    feat: introduce cloud optimization (#5272)
    * feat: introduce cloud optimization
    
    * add imports to owlbot
    
    * remove umbrella package from README
    
    * add formatting

---
+++ Optimization/src/V1/resources/fleet_routing_rest_client_config.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.optimization.v1.FleetRouting' => [
+            'BatchOptimizeTours' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{parent=projects/*/locations/*}:batchOptimizeTours',
+                'body' => '*',
+                'additionalBindings' => [
+                    [
+                        'method' => 'post',
+                        'uriTemplate' => '/v1/{parent=projects/*}:batchOptimizeTours',
+                        'body' => '*',
+                    ],
+                ],
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'OptimizeTours' => [
+                'method' => 'post',
+                'uriTemplate' => '/v1/{parent=projects/*/locations/*}:optimizeTours',
+                'body' => '*',
+                'additionalBindings' => [
+                    [
+                        'method' => 'post',
+                        'uriTemplate' => '/v1/{parent=projects/*}:optimizeTours',
+                        'body' => '*',
+                    ],
+                ],
+                'placeholders' => [
+                    'parent' => [
+                        'getters' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        'google.longrunning.Operations' => [
+            'GetOperation' => [
+                'method' => 'get',
+                'uriTemplate' => '/v1/{name=projects/*/operations/*}',
+                'additionalBindings' => [
+                    [
+                        'method' => 'get',
+                        'uriTemplate' => '/v1/{name=projects/*/locations/*/operations/*}',
+                    ],
+                ],
+                'placeholders' => [
+                    'name' => [
+                        'getters' => [
+                            'getName',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
