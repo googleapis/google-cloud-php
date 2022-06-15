02d85dd1905a538c9f1ab88088b79e18ce0b94c5
David Supplee <dwsupplee@gmail.com>
2022-06-15 12:31:18 -0700

    feat: introduce gke multi cloud (anthos) (#5336)

---
+++ GkeMultiCloud/src/V1/resources/azure_clusters_descriptor_config.php
@@ -0,0 +1,118 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.gkemulticloud.v1.AzureClusters' => [
+            'CreateAzureClient' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Cloud\GkeMultiCloud\V1\AzureClient',
+                    'metadataReturnType' => '\Google\Cloud\GkeMultiCloud\V1\OperationMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+            ],
+            'CreateAzureCluster' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Cloud\GkeMultiCloud\V1\AzureCluster',
+                    'metadataReturnType' => '\Google\Cloud\GkeMultiCloud\V1\OperationMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+            ],
+            'CreateAzureNodePool' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Cloud\GkeMultiCloud\V1\AzureNodePool',
+                    'metadataReturnType' => '\Google\Cloud\GkeMultiCloud\V1\OperationMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+            ],
+            'DeleteAzureClient' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Protobuf\GPBEmpty',
+                    'metadataReturnType' => '\Google\Cloud\GkeMultiCloud\V1\OperationMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+            ],
+            'DeleteAzureCluster' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Protobuf\GPBEmpty',
+                    'metadataReturnType' => '\Google\Cloud\GkeMultiCloud\V1\OperationMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+            ],
+            'DeleteAzureNodePool' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Protobuf\GPBEmpty',
+                    'metadataReturnType' => '\Google\Cloud\GkeMultiCloud\V1\OperationMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+            ],
+            'UpdateAzureCluster' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Cloud\GkeMultiCloud\V1\AzureCluster',
+                    'metadataReturnType' => '\Google\Cloud\GkeMultiCloud\V1\OperationMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+            ],
+            'UpdateAzureNodePool' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Cloud\GkeMultiCloud\V1\AzureNodePool',
+                    'metadataReturnType' => '\Google\Cloud\GkeMultiCloud\V1\OperationMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+            ],
+            'ListAzureClients' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getAzureClients',
+                ],
+            ],
+            'ListAzureClusters' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getAzureClusters',
+                ],
+            ],
+            'ListAzureNodePools' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getAzureNodePools',
+                ],
+            ],
+        ],
+    ],
+];
