f68f1f66c017ea40e006daf9be1aeda79c7dc91b
David Supplee <dwsupplee@gmail.com>
2022-06-23 13:48:41 -0700

    chore: introduce functions samples

---
+++ Functions/samples/V1/CloudFunctionsServiceClient/test_iam_permissions.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START cloudfunctions_v1_generated_CloudFunctionsService_TestIamPermissions_sync]
+use Google\Cloud\Functions\V1\CloudFunctionsServiceClient;
+
+$cloudFunctionsServiceClient = new CloudFunctionsServiceClient();
+try {
+    $resource = 'resource';
+    $permissions = [];
+    $response = $cloudFunctionsServiceClient->testIamPermissions($resource, $permissions);
+} finally {
+    $cloudFunctionsServiceClient->close();
+}
+
+// [END cloudfunctions_v1_generated_CloudFunctionsService_TestIamPermissions_sync]
---
+++ Functions/samples/V1/CloudFunctionsServiceClient/update_function.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START cloudfunctions_v1_generated_CloudFunctionsService_UpdateFunction_sync]
+use Google\Cloud\Functions\V1\CloudFunction;
+use Google\Cloud\Functions\V1\CloudFunctionsServiceClient;
+
+$cloudFunctionsServiceClient = new CloudFunctionsServiceClient();
+try {
+    $function = new CloudFunction();
+    $operationResponse = $cloudFunctionsServiceClient->updateFunction($function);
+    $operationResponse->pollUntilComplete();
+    if ($operationResponse->operationSucceeded()) {
+        $result = $operationResponse->getResult();
+    // doSomethingWith($result)
+    } else {
+        $error = $operationResponse->getError();
+        // handleError($error)
+    }
+    
+    // Alternatively:
+    // start the operation, keep the operation name, and resume later
+    $operationResponse = $cloudFunctionsServiceClient->updateFunction($function);
+    $operationName = $operationResponse->getName();
+    // ... do other work
+    $newOperationResponse = $cloudFunctionsServiceClient->resumeOperation($operationName, 'updateFunction');
+    while (!$newOperationResponse->isDone()) {
+        // ... do other work
+        $newOperationResponse->reload();
+    }
+    
+    if ($newOperationResponse->operationSucceeded()) {
+        $result = $newOperationResponse->getResult();
+    // doSomethingWith($result)
+    } else {
+        $error = $newOperationResponse->getError();
+        // handleError($error)
+    }
+    
+} finally {
+    $cloudFunctionsServiceClient->close();
+}
+
+// [END cloudfunctions_v1_generated_CloudFunctionsService_UpdateFunction_sync]
