e191484ab6550258459eecebd26fa20efc20482a
David Supplee <dwsupplee@gmail.com>
2022-12-13 09:49:11 -0800

    feat: introduce vmware engine (#5692)
    Co-authored-by: Brent Shaffer <betterbrent@google.com>

---
+++ VmwareEngine/README.md
@@ -0,0 +1,37 @@
+# Google Cloud Vmware Engine for PHP
+
+> Idiomatic PHP client for [Google Cloud Vmware Engine](https://cloud.google.com/vmware-engine).
+
+[![Latest Stable Version](https://poser.pugx.org/google/cloud-vmware-engine/v/stable)](https://packagist.org/packages/google/cloud-vmware-engine) [![Packagist](https://img.shields.io/packagist/dm/google/cloud-vmware-engine.svg)](https://packagist.org/packages/google/cloud-vmware-engine)
+
+* [API documentation](https://cloud.google.com/php/docs/reference/cloud-vmware-engine/latest)
+
+**NOTE:** This repository is part of [Google Cloud PHP](https://github.com/googleapis/google-cloud-php). Any
+support requests, bug reports, or development contributions should be directed to
+that project.
+
+### Installation
+
+To begin, install the preferred dependency manager for PHP, [Composer](https://getcomposer.org/).
+
+Now to install just this component:
+
+```sh
+$ composer require google/cloud-vmware-engine
+```
+
+This component supports both REST over HTTP/1.1 and gRPC. In order to take advantage of the benefits offered by gRPC (such as streaming methods)
+please see our [gRPC installation guide](https://cloud.google.com/php/grpc).
+
+### Authentication
+
+Please see our [Authentication guide](https://github.com/googleapis/google-cloud-php/blob/main/AUTHENTICATION.md) for more information
+on authenticating your client. Once authenticated, you'll be ready to start making requests.
+
+### Version
+
+This component is considered beta. As such, it should be expected to be mostly stable and we're working towards a release candidate. We will address issues and requests with a higher priority.
+
+### Next Steps
+
+1. Understand the [official documentation](https://cloud.google.com/vmware-engine/docs).
---
+++ VmwareEngine/samples/V1/VmwareEngineClient/get_hcx_activation_key.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START vmwareengine_v1_generated_VmwareEngine_GetHcxActivationKey_sync]
+use Google\ApiCore\ApiException;
+use Google\Cloud\VmwareEngine\V1\HcxActivationKey;
+use Google\Cloud\VmwareEngine\V1\VmwareEngineClient;
+
+/**
+ * Retrieves a `HcxActivationKey` resource by its resource name.
+ *
+ * @param string $formattedName The resource name of the HCX activation key to retrieve.
+ *                              Resource names are schemeless URIs that follow the conventions in
+ *                              https://cloud.google.com/apis/design/resource_names.
+ *                              For example:
+ *                              `projects/my-project/locations/us-west1/privateClouds/my-cloud/hcxActivationKeys/my-key`
+ *                              Please see {@see VmwareEngineClient::hcxActivationKeyName()} for help formatting this field.
+ */
+function get_hcx_activation_key_sample(string $formattedName): void
+{
+    // Create a client.
+    $vmwareEngineClient = new VmwareEngineClient();
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var HcxActivationKey $response */
+        $response = $vmwareEngineClient->getHcxActivationKey($formattedName);
+        printf('Response data: %s' . PHP_EOL, $response->serializeToJsonString());
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $formattedName = VmwareEngineClient::hcxActivationKeyName(
+        '[PROJECT]',
+        '[LOCATION]',
+        '[PRIVATE_CLOUD]',
+        '[HCX_ACTIVATION_KEY]'
+    );
+
+    get_hcx_activation_key_sample($formattedName);
+}
+// [END vmwareengine_v1_generated_VmwareEngine_GetHcxActivationKey_sync]
