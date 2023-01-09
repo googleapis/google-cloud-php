2ee189994a09affdb3dcff1851a0b8b5c898e4f0
Yash Sahu <54198301+yash30201@users.noreply.github.com>
2023-01-09 07:13:06 +0530

    Update Storage/src/Connection/RetryTrait.php
    Co-authored-by: Saransh Dhingra <saranshdhingra@google.com>

--- Storage/src/Connection/RetryTrait.php
+++ Storage/src/Connection/RetryTrait.php
@@ -99,9 +99,7 @@ trait RetryTrait
      */
 
     /**
-     * The strategy value which enforces Non-Idempotent & Conditionally
-     * Non-Idempotent operations to get always retried if error code is a
-     * retriable one.
+     * Retry an API operation when an exception occurs if the exception has a retryable error code.
      * @var string
      */
     public static $RETRY_STRATEGY_ALWAYS = 'always';
