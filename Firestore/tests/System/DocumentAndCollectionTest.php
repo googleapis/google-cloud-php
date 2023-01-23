48e27a1a82a19acf321918b87fd7370ea0aa00f4
yash30201 <54198301+yash30201@users.noreply.github.com>
2023-01-23 09:43:38 +0000

    fix(Firestore): SnapshotTrait getSnapshot method
    * Added system test for DocumentReference::snapshot() method with readTime optional parameter in the $args options array

--- Firestore/tests/System/DocumentAndCollectionTest.php
+++ Firestore/tests/System/DocumentAndCollectionTest.php
@@ -17,6 +17,7 @@
 
 namespace Google\Cloud\Firestore\Tests\System;
 
+use Google\Cloud\Core\Timestamp;
 use Google\Cloud\Firestore\CollectionReference;
 use Google\Cloud\Firestore\DocumentReference;
 
@@ -42,6 +43,17 @@ class DocumentAndCollectionTest extends FirestoreTestCase
         $document->create(['firstName' => 'Kate']);
         $this->assertTrue($document->snapshot()->exists());
         $this->assertEquals(['firstName' => 'Kate'], $document->snapshot()->data());
+    }
+
+    public function testSnapshotWithReadTime()
+    {
+        $readTime = new Timestamp(new \DateTimeImmutable());
+        $snapshotData = $this->document->snapshot([
+            'readTime' => $readTime
+        ])->data();
+
+        $this->assertEquals('John', $snapshotData['firstName']);
+        $this->assertEquals('USA', $snapshotData['country']);
     }
 
     public function testUpdate()
