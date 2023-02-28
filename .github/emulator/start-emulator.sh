14afb22afadf2aba5b937dcb297e74b44c00fd10
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2023-02-28 17:40:57 +0000

    fix: decoupling emulator versions

--- .github/emulator/start-emulator.sh
+++ .github/emulator/start-emulator.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -eux
 
-docker pull -q gcr.io/google.com/cloudsdktool/cloud-sdk:419.0.0-emulators
+docker pull -q gcr.io/google.com/cloudsdktool/cloud-sdk:$2
 CONTAINER=`docker run \
   -d \
   -p 8085:8085 \
-  gcr.io/google.com/cloudsdktool/cloud-sdk:419.0.0-emulators gcloud beta emulators $1 start --host-port=0.0.0.0:8085 --project=emulator-project`
+  gcr.io/google.com/cloudsdktool/cloud-sdk:$2 gcloud beta emulators $1 start --host-port=0.0.0.0:8085 --project=emulator-project`
 sleep 10
 docker logs $CONTAINER
