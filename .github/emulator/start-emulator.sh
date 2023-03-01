286c69714d53c416d121976ad0bb84c5681beda1
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2023-03-01 07:37:00 +0000

    fix: added comment for emulator version

--- .github/emulator/start-emulator.sh
+++ .github/emulator/start-emulator.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -eux
 
-docker pull -q gcr.io/google.com/cloudsdktool/cloud-sdk:$2
+# Emulator version maybe different for individual products
+IMAGE="gcr.io/google.com/cloudsdktool/cloud-sdk:$2"
+docker pull -q $IMAGE
 CONTAINER=`docker run \
   -d \
   -p 8085:8085 \
-  gcr.io/google.com/cloudsdktool/cloud-sdk:$2 gcloud beta emulators $1 start --host-port=0.0.0.0:8085 --project=emulator-project`
+  $IMAGE gcloud beta emulators $1 start --host-port=0.0.0.0:8085 --project=emulator-project`
 sleep 10
 docker logs $CONTAINER
