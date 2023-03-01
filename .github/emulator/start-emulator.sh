77b4bd0b9f5afec18f7230b17234662d1c17030b
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2023-03-01 23:32:05 +0530

    chore: pr comments

--- .github/emulator/start-emulator.sh
+++ .github/emulator/start-emulator.sh
@@ -1,7 +1,16 @@
 #!/bin/sh -eux
 
+# default to "emulators", which is the latest available version
+EMULATOR_VERSION=emulators
+if [ "$#" -eq 2 ]; then
+    # use the supplied emulator version (e.g. "384.0.1-emulators")
+    EMULATOR_VERSION=$2
+elif [ "$#" -ne 1 ]; then
+    echo "usage: start-emulator.sh PRODUCT_GROUP [EMULATOR_VERSION]"
+    exit 1;
+fi
 # Emulator version maybe different for individual products
-IMAGE="gcr.io/google.com/cloudsdktool/cloud-sdk:$2"
+IMAGE="gcr.io/google.com/cloudsdktool/cloud-sdk:$EMULATOR_VERSION"
 docker pull -q $IMAGE
 CONTAINER=`docker run \
   -d \
