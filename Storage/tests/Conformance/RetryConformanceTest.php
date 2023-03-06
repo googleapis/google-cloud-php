0996b864e8a7465dff50d2c021cc24facb37eb2c
Saransh Dhingra <saranshdhingra@google.com>
2023-03-06 17:34:14 +0000

    Added a default for emulator URL

--- Storage/tests/Conformance/RetryConformanceTest.php
+++ Storage/tests/Conformance/RetryConformanceTest.php
@@ -48,7 +48,7 @@ class RetryConformanceTest extends TestCase
     private static $cases = [];
 
     // Storage test bench URL. To be populated by an env variable.
-    private static $emulatorUrl = '';
+    private static $emulatorUrl = 'http://localhost:9000';
 
     public static function set_up_before_class()
     {
@@ -58,7 +58,9 @@ class RetryConformanceTest extends TestCase
         }
 
         $setup = true;
-        self::$emulatorUrl = getenv('STORAGE_EMULATOR_HOST');
+        if (getenv('STORAGE_EMULATOR_HOST')) {
+            self::$emulatorUrl = getenv('STORAGE_EMULATOR_HOST');
+        }
 
         self::$httpClient = new Client([
             'base_uri' => self::$emulatorUrl
@@ -114,7 +116,7 @@ class RetryConformanceTest extends TestCase
     {
         self::set_up_before_class();
         // These scenario IDs will be run
-        $scenarios = [1, 2, 3, 4, 5, 6, 7, 8];
+        $scenarios = [1];
 
         $cases = [];
 
