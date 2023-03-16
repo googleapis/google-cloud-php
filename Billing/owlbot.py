3a4bcb37ff25782ba3205f95638243478e93c525
Noah Dietz <noahdietz@users.noreply.github.com>
2023-03-16 13:58:16 -0700

    chore: remove unnecessary experimental tag removal (#5976)

--- Billing/owlbot.py
+++ Billing/owlbot.py
@@ -34,11 +34,6 @@ php.owlbot_main(src=src, dest=dest)
 
 
 
-# V1 is GA, so remove @experimental tags
-s.replace(
-    'src/V1/**/*Client.php',
-    r'^(\s+\*\n)?\s+\*\s@experimental\n',
-    '')
 
 # Change the wording for the deprecation warning.
 s.replace(
