704727b14b78c4049ad7c2a30807ada4791bbe98
Noah Dietz <ndietz@google.com>
2023-03-21 21:15:42 +0000

    add __call comment

--- SecretManager/src/V1/Client/BaseClient/SecretManagerServiceBaseClient.php
+++ SecretManager/src/V1/Client/BaseClient/SecretManagerServiceBaseClient.php
@@ -272,6 +272,7 @@ abstract class SecretManagerServiceBaseClient
         $this->setClientOptions($clientOptions);
     }
 
+    /** Handles execution of the async variants for each documented method. */
     public function __call($method, $args)
     {
         if (substr($method, -5) !== 'Async') {
