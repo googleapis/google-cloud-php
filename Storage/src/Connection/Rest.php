8e7263e58be5045696c85a4d8fe297335703c558
Saransh Dhingra <saranshdhingra@google.com>
2023-04-06 08:47:12 +0000

    Removed an extra constant from RequestWrapper and used an existing one from ApiCore

--- Storage/src/Connection/Rest.php
+++ Storage/src/Connection/Rest.php
@@ -17,6 +17,7 @@
 
 namespace Google\Cloud\Storage\Connection;
 
+use Google\ApiCore\AgentHeader;
 use Google\Cloud\Core\RequestBuilder;
 use Google\Cloud\Core\RequestWrapper;
 use Google\Cloud\Core\RestTrait;
@@ -720,14 +721,14 @@ class Rest implements ConnectionInterface
     ) {
         $valueToAdd = sprintf("gccl-invocation-id/%s", $requestHash);
         $this->updateHeader(
-            RequestWrapper::HEADER_API_CLIENT_IDENTIFICATION,
+            AgentHeader::AGENT_HEADER_KEY,
             $arguments,
             $valueToAdd
         );
 
         $valueToAdd = sprintf("gccl-attempt-count/%s", $currentAttempt);
         $this->updateHeader(
-            RequestWrapper::HEADER_API_CLIENT_IDENTIFICATION,
+            AgentHeader::AGENT_HEADER_KEY,
             $arguments,
             $valueToAdd,
             false
