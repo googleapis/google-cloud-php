34e63f305aac5afc3534c8479d4a71c22b46f375
Brent Shaffer <betterbrent@google.com>
2023-04-19 09:16:03 -0600

    feat: introduce ConfidentialComputing (#6084)

---
+++ ConfidentialComputing/src/V1/resources/confidential_computing_descriptor_config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.confidentialcomputing.v1.ConfidentialComputing' => [
+            'ListLocations' => [
+                'pageStreaming' => [
+                    'requestPageTokenGetMethod' => 'getPageToken',
+                    'requestPageTokenSetMethod' => 'setPageToken',
+                    'requestPageSizeGetMethod' => 'getPageSize',
+                    'requestPageSizeSetMethod' => 'setPageSize',
+                    'responsePageTokenGetMethod' => 'getNextPageToken',
+                    'resourcesGetMethod' => 'getLocations',
+                ],
+            ],
+        ],
+    ],
+];
