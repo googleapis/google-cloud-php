14451a437b7833cfd09ee682fc70daf5484ae0d6
Brent Shaffer <betterbrent@google.com>
2023-04-19 17:19:05 -0700

    misc fixes for cs

--- Spanner/src/Connection/Grpc.php
+++ Spanner/src/Connection/Grpc.php
@@ -1019,7 +1019,7 @@ class Grpc implements ConnectionInterface
                 ? PBReadOnly::class
                 : 'Google\Cloud\Spanner\V1\TransactionOptions\ReadOnly';
             $readOnly = $this->serializer->decodeMessage(
-                new $readOnlyClass(), // @phpstan-ignore-line
+                new $readOnlyClass(),
                 $transactionOptions['readOnly']
             );
             $options->setReadOnly($readOnly);
