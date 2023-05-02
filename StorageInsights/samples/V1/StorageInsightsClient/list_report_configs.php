13430eaf46750474e074c87b469abe284133b240
yash30201 <54198301+yash30201@users.noreply.github.com>
2023-05-02 01:57:10 +0000

    Adding storage insights samples

---
+++ StorageInsights/samples/V1/StorageInsightsClient/list_report_configs.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START storageinsights_v1_generated_StorageInsights_ListReportConfigs_sync]
+use Google\ApiCore\ApiException;
+use Google\ApiCore\PagedListResponse;
+use Google\Cloud\StorageInsights\V1\ReportConfig;
+use Google\Cloud\StorageInsights\V1\StorageInsightsClient;
+
+/**
+ * Lists ReportConfigs in a given project and location.
+ *
+ * @param string $formattedParent Parent value for ListReportConfigsRequest
+ *                                Please see {@see StorageInsightsClient::locationName()} for help formatting this field.
+ */
+function list_report_configs_sample(string $formattedParent): void
+{
+    // Create a client.
+    $storageInsightsClient = new StorageInsightsClient();
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var PagedListResponse $response */
+        $response = $storageInsightsClient->listReportConfigs($formattedParent);
+
+        /** @var ReportConfig $element */
+        foreach ($response as $element) {
+            printf('Element data: %s' . PHP_EOL, $element->serializeToJsonString());
+        }
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $formattedParent = StorageInsightsClient::locationName('[PROJECT]', '[LOCATION]');
+
+    list_report_configs_sample($formattedParent);
+}
+// [END storageinsights_v1_generated_StorageInsights_ListReportConfigs_sync]
