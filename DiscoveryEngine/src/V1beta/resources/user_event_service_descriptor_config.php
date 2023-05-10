50bea44acf0b978e496e4b2959167f07748207a3
Brent Shaffer <betterbrent@google.com>
2023-05-10 16:06:29 +0000

    feat: introduce DiscoveryEngine

---
+++ DiscoveryEngine/src/V1beta/resources/user_event_service_descriptor_config.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    'interfaces' => [
+        'google.cloud.discoveryengine.v1beta.UserEventService' => [
+            'ImportUserEvents' => [
+                'longRunning' => [
+                    'operationReturnType' => '\Google\Cloud\DiscoveryEngine\V1beta\ImportUserEventsResponse',
+                    'metadataReturnType' => '\Google\Cloud\DiscoveryEngine\V1beta\ImportUserEventsMetadata',
+                    'initialPollDelayMillis' => '500',
+                    'pollDelayMultiplier' => '1.5',
+                    'maxPollDelayMillis' => '5000',
+                    'totalPollTimeoutMillis' => '300000',
+                ],
+                'callType' => \Google\ApiCore\Call::LONGRUNNING_CALL,
+                'headerParams' => [
+                    [
+                        'keyName' => 'parent',
+                        'fieldAccessors' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'CollectUserEvent' => [
+                'callType' => \Google\ApiCore\Call::UNARY_CALL,
+                'responseType' => 'Google\Api\HttpBody',
+                'headerParams' => [
+                    [
+                        'keyName' => 'parent',
+                        'fieldAccessors' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'WriteUserEvent' => [
+                'callType' => \Google\ApiCore\Call::UNARY_CALL,
+                'responseType' => 'Google\Cloud\DiscoveryEngine\V1beta\UserEvent',
+                'headerParams' => [
+                    [
+                        'keyName' => 'parent',
+                        'fieldAccessors' => [
+                            'getParent',
+                        ],
+                    ],
+                ],
+            ],
+            'templateMap' => [
+                'dataStore' => 'projects/{project}/locations/{location}/dataStores/{data_store}',
+                'document' => 'projects/{project}/locations/{location}/dataStores/{data_store}/branches/{branch}/documents/{document}',
+                'projectLocationCollectionDataStore' => 'projects/{project}/locations/{location}/collections/{collection}/dataStores/{data_store}',
+                'projectLocationCollectionDataStoreBranchDocument' => 'projects/{project}/locations/{location}/collections/{collection}/dataStores/{data_store}/branches/{branch}/documents/{document}',
+                'projectLocationDataStore' => 'projects/{project}/locations/{location}/dataStores/{data_store}',
+                'projectLocationDataStoreBranchDocument' => 'projects/{project}/locations/{location}/dataStores/{data_store}/branches/{branch}/documents/{document}',
+            ],
+        ],
+    ],
+];
