64f4ebcae2eb9c14f0c90eab5ab905655bcad3f2
Yash Sahu <54198301+yash30201@users.noreply.github.com>
2023-06-09 14:58:51 +0530

    feat(Spanner): Federation M2 (#6300)
    * Updated documentation
    * Implemented testing:
        * System tests
        * Unit tests

--- Spanner/tests/System/BatchTest.php
+++ Spanner/tests/System/BatchTest.php
@@ -200,6 +200,60 @@ class BatchTest extends SpannerTestCase
         $snapshot->close();
     }
 
+    public function testBatchWithDataBoostEnabled()
+    {
+        // Emulator does not support dataBoostEnabled
+        $this->skipEmulatorTests();
+
+        $query = 'SELECT
+                id,
+                decade
+            FROM ' . self::$tableName . '
+            WHERE
+                decade > @earlyBound
+            AND
+                decade < @lateBound';
+
+        $parameters = [
+            'earlyBound' => 1960,
+            'lateBound' => 1980
+        ];
+
+        $resultSet = iterator_to_array(self::$database->execute($query, ['parameters' => $parameters]));
+
+        $batch = self::$client->batch(self::INSTANCE_NAME, self::$dbName);
+        $string = $batch->snapshot()->serialize();
+
+        $snapshot = $batch->snapshotFromString($string);
+
+        $partitions = $snapshot->partitionQuery($query, [
+            'parameters' => $parameters,
+            'dataBoostEnabled' => true
+        ]);
+        $this->assertEquals(count($resultSet), $this->executePartitions($batch, $snapshot, $partitions));
+
+        $keySet = new KeySet([
+            'ranges' => [
+                new KeyRange([
+                    'start' => $parameters['earlyBound'],
+                    'startType' => KeyRange::TYPE_OPEN,
+                    'end' => $parameters['lateBound'],
+                    'endType' => KeyRange::TYPE_OPEN
+                ])
+            ]
+        ]);
+
+        $partitions = $snapshot->partitionRead(
+            self::$tableName,
+            $keySet,
+            ['id', 'decade'],
+            ['dataBoostEnabled' => true]
+        );
+        $this->assertEquals(count($resultSet), $this->executePartitions($batch, $snapshot, $partitions));
+
+        $snapshot->close();
+    }
+
     private function executePartitions(BatchClient $client, BatchSnapshot $snapshot, array $partitions)
     {
         $partitionResultSet = [];
