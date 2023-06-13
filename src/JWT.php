64fa8bdda84abb1e2eb2396954079c3289e7cfa5
Pinchon Karim <kpn13@users.noreply.github.com>
2023-06-13 18:35:01 +0200

    feat: allow get headers when decoding token (firebase/php-jwt#442)
    Co-authored-by: Vishwaraj Anand <vishwaraj.anand00@gmail.com>
    Co-authored-by: Brent Shaffer <betterbrent@google.com>

--- src/JWT.php
+++ src/JWT.php
@@ -78,6 +78,7 @@ class JWT
      *                                                                      Supported algorithms are 'ES384','ES256',
      *                                                                      'HS256', 'HS384', 'HS512', 'RS256', 'RS384'
      *                                                                      and 'RS512'.
+     * @param stdClass               $headers                               Optional. Populates stdClass with headers.
      *
      * @return stdClass The JWT's payload as a PHP object
      *
@@ -94,7 +95,8 @@ class JWT
      */
     public static function decode(
         string $jwt,
-        $keyOrKeyArray
+        $keyOrKeyArray,
+        stdClass &$headers = null
     ): stdClass {
         // Validate JWT
         $timestamp = \is_null(static::$timestamp) ? \time() : static::$timestamp;
@@ -110,6 +112,9 @@ class JWT
         $headerRaw = static::urlsafeB64Decode($headb64);
         if (null === ($header = static::jsonDecode($headerRaw))) {
             throw new UnexpectedValueException('Invalid header encoding');
+        }
+        if ($headers !== null) {
+            $headers = $header;
         }
         $payloadRaw = static::urlsafeB64Decode($bodyb64);
         if (null === ($payload = static::jsonDecode($payloadRaw))) {
