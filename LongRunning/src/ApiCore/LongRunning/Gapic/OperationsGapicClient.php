b45e2057f5cdbc9b02570f516642da6d96227b2a
Brent Shaffer <betterbrent@google.com>
2023-10-06 17:00:31 -0700

    try to trick breaking change detector

---
+++ LongRunning/src/ApiCore/LongRunning/Gapic/OperationsGapicClient.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Google\ApiCore\LongRunning\Gapic;
+
+if (false) {
+    /**
+     * This class is deprecated. Use Google\LongRunning\OperationsClient instead.
+     * @deprecated
+     */
+    class OperationsGapicClient extends \Google\LongRunning\Gapic\OperationsGapicClient {}
+}
+// Autoload the class and its alias
+class_exists('\Google\LongRunning\Gapic\OperationsGapicClient');
+@trigger_error('Google\LongRunning\ApiCore\OperationsGapicClient is deprecated and will be removed in the next major release. Use Google\LongRunning\OperationsClient instead', E_USER_DEPRECATED);
+
--- LongRunning/src/ApiCore/LongRunning/OperationsClient.php
+++ LongRunning/src/ApiCore/LongRunning/OperationsClient.php
@@ -7,9 +7,9 @@ if (false) {
      * This class is deprecated. Use Google\LongRunning\OperationsClient instead.
      * @deprecated
      */
-    class OperationsClient {}
+    class OperationsClient extends \Google\LongRunning\OperationsClient {}
 }
 // Autoload the class and its alias
 class_exists('\Google\LongRunning\OperationsClient');
-@trigger_error('Google\LongRunning\ApiCore\OperationsClientis deprecated and will be removed in the next major release. Use Google\LongRunning\OperationsClient instead', E_USER_DEPRECATED);
+@trigger_error('Google\LongRunning\ApiCore\OperationsClient is deprecated and will be removed in the next major release. Use Google\LongRunning\OperationsClient instead', E_USER_DEPRECATED);
 
