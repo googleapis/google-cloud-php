f2c1a6cd9f1346cbbcdc8b1aec70336ad7b9ddbf
Brent Shaffer <betterbrent@google.com>
2023-10-10 18:30:08 +0000

    chore: update minimum gax and core versions

--- dev/sh/bump-dep.sh
+++ dev/sh/bump-dep.sh
@@ -7,8 +7,10 @@ set -ev
 # version to the specified command line argument.
 #
 # Usage (to update gax version to e.g. 0.1.2):
-# $ ./dev/sh/bump-gax.sh 0.1.2
+# $ ./dev/sh/bump-dep.sh 'google/gax' 0.1.2
+
+dep=$(echo $1 | sed 's/\//\\\//g')
 
 find . -maxdepth 2 -name composer.json \
      -not -path "./vendor/*" \
-     -exec sed -i "s/google\/gax[\"']: [\"']\^[0-9]\+\.[0-9]\+\(\.[0-9]\+\)\?[\"']/google\/gax\": \"\^$1\"/" "{}" \;
+     -exec sed -i "s/$dep[\"']: [\"']\^[0-9]\+\.[0-9]\+\(\.[0-9]\+\)\?[\"']/$dep\": \"\^$2\"/" "{}" \;
