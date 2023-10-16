9b4ae736af04a611ab9d7d3c2323744e42da567f
Brent Shaffer <betterbrent@google.com>
2023-10-16 10:08:59 -0700

    fix docfx for consolidated clients, update fixtures

--- dev/tests/Unit/Command/DocFxCommandTest.php
+++ dev/tests/Unit/Command/DocFxCommandTest.php
@@ -92,7 +92,7 @@ class DocFxCommandTest extends TestCase
     {
         $this->assertTrue(
             file_exists(self::$fixturesDir . '/docfx/Vision/' . $file),
-            sprintf('%s does not exist in fixtures (%s)', $file, self::$tmpDir . '/' . $file)
+            sprintf('tests/fixtures/docfx/%s does not exist (%s)', $file, self::$tmpDir . '/' . $file)
         );
 
         $left  = self::$fixturesDir . '/docfx/Vision/' . $file;
