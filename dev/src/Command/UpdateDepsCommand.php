34b09201a906e01861267e821f329417d93e9642
Brent Shaffer <betterbrent@google.com>
2023-11-03 13:52:07 -0700

    add command to update composer deps

---
+++ dev/src/Command/UpdateDepsCommand.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Dev\Command;
+
+use Google\Cloud\Dev\Composer;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * List repo details
+ * @internal
+ */
+class UpdateDepsCommand extends Command
+{
+    private GitHub $github;
+
+    protected function configure()
+    {
+        $this->setName('update-deps')
+            ->setDescription('update a dependency across all components')
+            ->addArgument('package', InputArgument::REQUIRED, 'Package name to update, e.g. "google/gax"')
+            ->addArgument('version', InputArgument::OPTIONAL, 'Package version to update to, e.g. "1.4.0"', '')
+            ->addOption('bump', '', InputOption::VALUE_NONE, 'Bump to latest version of the package')
+            ->addOption('add', '', InputOption::VALUE_NONE, 'Adds the dep if it doesn\'t exist')
+            ->addOption('dev', '', InputOption::VALUE_NONE, 'Updated the dep in "require-dev"')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $package = $input->getArgument('package');
+        if (!$version = $input->getArgument('version')) {
+            if (!$input->getOption('bump')) {
+                throw new \InvalidArgumentException('You must either supply a package version or the --bump flag');
+            }
+            if (!$version = Composer::getLatestVersion($package)) {
+                throw new \InvalidArgumentException('Could not find a version for ' . $package);
+            }
+        } elseif ($input->getOption('bump')) {
+            throw new \InvalidArgumentException('You cannot supply both a package version and the --bump flag');
+        }
+
+        $projectRoot = realpath(__DIR__ . '/../../../');
+        $result = (new Finder())->files()->in($projectRoot)->depth('<= 1')->name('composer.json');
+        $paths = array_map(fn ($file) => $file->getRelativePathname(), iterator_to_array($result));
+        sort($paths);
+        $require = $input->getOption('dev') ? 'require-dev' : 'require';
+        foreach ($paths as $path) {
+            $composerJson = json_decode(file_get_contents($projectRoot . '/' . $path), true);
+            if (!isset($composerJson[$require][$package])) {
+                if (!$input->getOption('add')) {
+                    continue;
+                }
+                $composerJson[$require] = [];
+            }
+            $composerJson[$require][$package] = $version;
+            file_put_contents($projectRoot . '/' . $path, json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . "\n");
+        }
+        $output->writeln(sprintf("Updated <fg=white>%s files</> to use <comment>$package</>: <info>$version</>", count($paths)));
+
+        return 0;
+    }
+}
