4e0b460add3c599b79ca3e1dd863eec197e711ee
Yash Sahu <54198301+yash30201@users.noreply.github.com>
2023-11-17 13:42:18 +0000

    feat: Introduce Telecoautomation (#6794)

---
+++ TelcoAutomation/samples/V1/TelcoAutomationClient/list_edge_slms.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START telcoautomation_v1_generated_TelcoAutomation_ListEdgeSlms_sync]
+use Google\ApiCore\ApiException;
+use Google\ApiCore\PagedListResponse;
+use Google\Cloud\TelcoAutomation\V1\Client\TelcoAutomationClient;
+use Google\Cloud\TelcoAutomation\V1\EdgeSlm;
+use Google\Cloud\TelcoAutomation\V1\ListEdgeSlmsRequest;
+
+/**
+ * Lists EdgeSlms in a given project and location.
+ *
+ * @param string $formattedParent Parent value for ListEdgeSlmsRequest
+ *                                Please see {@see TelcoAutomationClient::locationName()} for help formatting this field.
+ */
+function list_edge_slms_sample(string $formattedParent): void
+{
+    // Create a client.
+    $telcoAutomationClient = new TelcoAutomationClient();
+
+    // Prepare the request message.
+    $request = (new ListEdgeSlmsRequest())
+        ->setParent($formattedParent);
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var PagedListResponse $response */
+        $response = $telcoAutomationClient->listEdgeSlms($request);
+
+        /** @var EdgeSlm $element */
+        foreach ($response as $element) {
+            printf('Element data: %s' . PHP_EOL, $element->serializeToJsonString());
+        }
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $formattedParent = TelcoAutomationClient::locationName('[PROJECT]', '[LOCATION]');
+
+    list_edge_slms_sample($formattedParent);
+}
+// [END telcoautomation_v1_generated_TelcoAutomation_ListEdgeSlms_sync]
