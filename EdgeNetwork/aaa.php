fc048d08de671f0b0114555616a8b000379decdb
yash30201 <54198301+yash30201@users.noreply.github.com>
2023-11-25 04:46:17 +0000

    feat: Introduce EdgeNetwork

---
+++ EdgeNetwork/aaa.php
@@ -0,0 +1,5 @@
+<?php
+
+$str = "https:\/\/console.developers.google.com\/apis\/api\/edgenetwork.googleapis.com\/overview?project=yashsahu-datastore";
+$res = str_replace("\/", "/", $str);
+echo $res . PHP_EOL;
