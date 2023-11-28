f622952598279acc910de4d1f810770bd04294e1
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2023-11-28 23:05:31 +0530

    fix: delete EdgeNetwork/aaa.php

--- EdgeNetwork/aaa.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$str = "https:\/\/console.developers.google.com\/apis\/api\/edgenetwork.googleapis.com\/overview?project=yashsahu-datastore";
-$res = str_replace("\/", "/", $str);
-echo $res . PHP_EOL;
