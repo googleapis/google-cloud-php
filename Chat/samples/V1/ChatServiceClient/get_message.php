cec87467e4db76871b22f5af728225e6401419ad
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2024-02-25 19:44:42 +0000

    feat: [Chat] add new client library surface

---
+++ Chat/samples/V1/ChatServiceClient/get_message.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START chat_v1_generated_ChatService_GetMessage_sync]
+use Google\ApiCore\ApiException;
+use Google\Chat\V1\ChatServiceClient;
+use Google\Chat\V1\Message;
+
+/**
+ * Returns details about a message.
+ * For an example, see [Read a
+ * message](https://developers.google.com/chat/api/guides/v1/messages/get).
+ *
+ * Requires
+ * [authentication](https://developers.google.com/chat/api/guides/auth).
+ * Supports
+ * [app
+ * authentication](https://developers.google.com/chat/api/guides/auth/service-accounts)
+ * and [user
+ * authentication](https://developers.google.com/chat/api/guides/auth/users).
+ *
+ * Note: Might return a message from a blocked member or space.
+ *
+ * @param string $formattedName Resource name of the message.
+ *
+ *                              Format: `spaces/{space}/messages/{message}`
+ *
+ *                              If you've set a custom ID for your message, you can use the value from the
+ *                              `clientAssignedMessageId` field for `{message}`. For details, see [Name a
+ *                              message]
+ *                              (https://developers.google.com/chat/api/guides/v1/messages/create#name_a_created_message). Please see
+ *                              {@see ChatServiceClient::messageName()} for help formatting this field.
+ */
+function get_message_sample(string $formattedName): void
+{
+    // Create a client.
+    $chatServiceClient = new ChatServiceClient();
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var Message $response */
+        $response = $chatServiceClient->getMessage($formattedName);
+        printf('Response data: %s' . PHP_EOL, $response->serializeToJsonString());
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $formattedName = ChatServiceClient::messageName('[SPACE]', '[MESSAGE]');
+
+    get_message_sample($formattedName);
+}
+// [END chat_v1_generated_ChatService_GetMessage_sync]
---
+++ Chat/samples/V1/ChatServiceClient/list_memberships.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START chat_v1_generated_ChatService_ListMemberships_sync]
+use Google\ApiCore\ApiException;
+use Google\ApiCore\PagedListResponse;
+use Google\Chat\V1\ChatServiceClient;
+use Google\Chat\V1\Membership;
+
+/**
+ * Lists memberships in a space. For an example, see [List
+ * memberships](https://developers.google.com/chat/api/guides/v1/members/list).
+ * Listing memberships with
+ * [app
+ * authentication](https://developers.google.com/chat/api/guides/auth/service-accounts)
+ * lists memberships in spaces that the Chat app has
+ * access to, but excludes Chat app memberships,
+ * including its own. Listing memberships with
+ * [User
+ * authentication](https://developers.google.com/chat/api/guides/auth/users)
+ * lists memberships in spaces that the authenticated user has access to.
+ *
+ * Requires
+ * [authentication](https://developers.google.com/chat/api/guides/auth).
+ * Supports
+ * [app
+ * authentication](https://developers.google.com/chat/api/guides/auth/service-accounts)
+ * and [user
+ * authentication](https://developers.google.com/chat/api/guides/auth/users).
+ *
+ * @param string $formattedParent The resource name of the space for which to fetch a membership
+ *                                list.
+ *
+ *                                Format: spaces/{space}
+ *                                Please see {@see ChatServiceClient::spaceName()} for help formatting this field.
+ */
+function list_memberships_sample(string $formattedParent): void
+{
+    // Create a client.
+    $chatServiceClient = new ChatServiceClient();
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var PagedListResponse $response */
+        $response = $chatServiceClient->listMemberships($formattedParent);
+
+        /** @var Membership $element */
+        foreach ($response as $element) {
+            printf('Element data: %s' . PHP_EOL, $element->serializeToJsonString());
+        }
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $formattedParent = ChatServiceClient::spaceName('[SPACE]');
+
+    list_memberships_sample($formattedParent);
+}
+// [END chat_v1_generated_ChatService_ListMemberships_sync]
