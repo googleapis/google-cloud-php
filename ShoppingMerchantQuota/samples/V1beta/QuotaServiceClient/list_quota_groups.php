da24c5360e02d39ee7a6ae80902ef84ca337c5f4
yash30201 <54198301+yash30201@users.noreply.github.com>
2024-03-20 02:31:02 +0000

    feat: Introduce ShoppingMerchantQuota

---
+++ ShoppingMerchantQuota/samples/V1beta/QuotaServiceClient/list_quota_groups.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START merchantapi_v1beta_generated_QuotaService_ListQuotaGroups_sync]
+use Google\ApiCore\ApiException;
+use Google\ApiCore\PagedListResponse;
+use Google\Shopping\Merchant\Quota\V1beta\Client\QuotaServiceClient;
+use Google\Shopping\Merchant\Quota\V1beta\ListQuotaGroupsRequest;
+use Google\Shopping\Merchant\Quota\V1beta\QuotaGroup;
+
+/**
+ * Lists the daily call quota and usage per group for your Merchant
+ * Center account.
+ *
+ * @param string $formattedParent The merchant account who owns the collection of method quotas
+ *                                Format: accounts/{account}
+ *                                Please see {@see QuotaServiceClient::accountName()} for help formatting this field.
+ */
+function list_quota_groups_sample(string $formattedParent): void
+{
+    // Create a client.
+    $quotaServiceClient = new QuotaServiceClient();
+
+    // Prepare the request message.
+    $request = (new ListQuotaGroupsRequest())
+        ->setParent($formattedParent);
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var PagedListResponse $response */
+        $response = $quotaServiceClient->listQuotaGroups($request);
+
+        /** @var QuotaGroup $element */
+        foreach ($response as $element) {
+            printf('Element data: %s' . PHP_EOL, $element->serializeToJsonString());
+        }
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $formattedParent = QuotaServiceClient::accountName('[ACCOUNT]');
+
+    list_quota_groups_sample($formattedParent);
+}
+// [END merchantapi_v1beta_generated_QuotaService_ListQuotaGroups_sync]
---
+++ ShoppingMerchantQuota/tests/Unit/V1beta/Client/QuotaServiceClientTest.php
@@ -0,0 +1,170 @@
+<?php
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+namespace Google\Shopping\Merchant\Quota\Tests\Unit\V1beta\Client;
+
+use Google\ApiCore\ApiException;
+use Google\ApiCore\CredentialsWrapper;
+use Google\ApiCore\Testing\GeneratedTest;
+use Google\ApiCore\Testing\MockTransport;
+use Google\Rpc\Code;
+use Google\Shopping\Merchant\Quota\V1beta\Client\QuotaServiceClient;
+use Google\Shopping\Merchant\Quota\V1beta\ListQuotaGroupsRequest;
+use Google\Shopping\Merchant\Quota\V1beta\ListQuotaGroupsResponse;
+use Google\Shopping\Merchant\Quota\V1beta\QuotaGroup;
+use stdClass;
+
+/**
+ * @group quota
+ *
+ * @group gapic
+ */
+class QuotaServiceClientTest extends GeneratedTest
+{
+    /** @return TransportInterface */
+    private function createTransport($deserialize = null)
+    {
+        return new MockTransport($deserialize);
+    }
+
+    /** @return CredentialsWrapper */
+    private function createCredentials()
+    {
+        return $this->getMockBuilder(CredentialsWrapper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /** @return QuotaServiceClient */
+    private function createClient(array $options = [])
+    {
+        $options += [
+            'credentials' => $this->createCredentials(),
+        ];
+        return new QuotaServiceClient($options);
+    }
+
+    /** @test */
+    public function listQuotaGroupsTest()
+    {
+        $transport = $this->createTransport();
+        $gapicClient = $this->createClient([
+            'transport' => $transport,
+        ]);
+        $this->assertTrue($transport->isExhausted());
+        // Mock response
+        $nextPageToken = '';
+        $quotaGroupsElement = new QuotaGroup();
+        $quotaGroups = [$quotaGroupsElement];
+        $expectedResponse = new ListQuotaGroupsResponse();
+        $expectedResponse->setNextPageToken($nextPageToken);
+        $expectedResponse->setQuotaGroups($quotaGroups);
+        $transport->addResponse($expectedResponse);
+        // Mock request
+        $formattedParent = $gapicClient->accountName('[ACCOUNT]');
+        $request = (new ListQuotaGroupsRequest())->setParent($formattedParent);
+        $response = $gapicClient->listQuotaGroups($request);
+        $this->assertEquals($expectedResponse, $response->getPage()->getResponseObject());
+        $resources = iterator_to_array($response->iterateAllElements());
+        $this->assertSame(1, count($resources));
+        $this->assertEquals($expectedResponse->getQuotaGroups()[0], $resources[0]);
+        $actualRequests = $transport->popReceivedCalls();
+        $this->assertSame(1, count($actualRequests));
+        $actualFuncCall = $actualRequests[0]->getFuncCall();
+        $actualRequestObject = $actualRequests[0]->getRequestObject();
+        $this->assertSame('/google.shopping.merchant.quota.v1beta.QuotaService/ListQuotaGroups', $actualFuncCall);
+        $actualValue = $actualRequestObject->getParent();
+        $this->assertProtobufEquals($formattedParent, $actualValue);
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /** @test */
+    public function listQuotaGroupsExceptionTest()
+    {
+        $transport = $this->createTransport();
+        $gapicClient = $this->createClient([
+            'transport' => $transport,
+        ]);
+        $this->assertTrue($transport->isExhausted());
+        $status = new stdClass();
+        $status->code = Code::DATA_LOSS;
+        $status->details = 'internal error';
+        $expectedExceptionMessage = json_encode(
+            [
+                'message' => 'internal error',
+                'code' => Code::DATA_LOSS,
+                'status' => 'DATA_LOSS',
+                'details' => [],
+            ],
+            JSON_PRETTY_PRINT
+        );
+        $transport->addResponse(null, $status);
+        // Mock request
+        $formattedParent = $gapicClient->accountName('[ACCOUNT]');
+        $request = (new ListQuotaGroupsRequest())->setParent($formattedParent);
+        try {
+            $gapicClient->listQuotaGroups($request);
+            // If the $gapicClient method call did not throw, fail the test
+            $this->fail('Expected an ApiException, but no exception was thrown.');
+        } catch (ApiException $ex) {
+            $this->assertEquals($status->code, $ex->getCode());
+            $this->assertEquals($expectedExceptionMessage, $ex->getMessage());
+        }
+        // Call popReceivedCalls to ensure the stub is exhausted
+        $transport->popReceivedCalls();
+        $this->assertTrue($transport->isExhausted());
+    }
+
+    /** @test */
+    public function listQuotaGroupsAsyncTest()
+    {
+        $transport = $this->createTransport();
+        $gapicClient = $this->createClient([
+            'transport' => $transport,
+        ]);
+        $this->assertTrue($transport->isExhausted());
+        // Mock response
+        $nextPageToken = '';
+        $quotaGroupsElement = new QuotaGroup();
+        $quotaGroups = [$quotaGroupsElement];
+        $expectedResponse = new ListQuotaGroupsResponse();
+        $expectedResponse->setNextPageToken($nextPageToken);
+        $expectedResponse->setQuotaGroups($quotaGroups);
+        $transport->addResponse($expectedResponse);
+        // Mock request
+        $formattedParent = $gapicClient->accountName('[ACCOUNT]');
+        $request = (new ListQuotaGroupsRequest())->setParent($formattedParent);
+        $response = $gapicClient->listQuotaGroupsAsync($request)->wait();
+        $this->assertEquals($expectedResponse, $response->getPage()->getResponseObject());
+        $resources = iterator_to_array($response->iterateAllElements());
+        $this->assertSame(1, count($resources));
+        $this->assertEquals($expectedResponse->getQuotaGroups()[0], $resources[0]);
+        $actualRequests = $transport->popReceivedCalls();
+        $this->assertSame(1, count($actualRequests));
+        $actualFuncCall = $actualRequests[0]->getFuncCall();
+        $actualRequestObject = $actualRequests[0]->getRequestObject();
+        $this->assertSame('/google.shopping.merchant.quota.v1beta.QuotaService/ListQuotaGroups', $actualFuncCall);
+        $actualValue = $actualRequestObject->getParent();
+        $this->assertProtobufEquals($formattedParent, $actualValue);
+        $this->assertTrue($transport->isExhausted());
+    }
+}
