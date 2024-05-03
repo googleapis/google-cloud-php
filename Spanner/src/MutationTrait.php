2a4af128b745b9136c017f5e9909b025c9a69d12
yash30201 <54198301+yash30201@users.noreply.github.com>
2024-05-03 09:26:50 +0000

    Improve Logic reuse.

--- Spanner/src/MutationTrait.php
+++ Spanner/src/MutationTrait.php
@@ -23,10 +23,244 @@ use Google\ApiCore\ValidationException;
 /**
  * Common helper methods used for creating array representation of
  * {@see \Google\Cloud\Spanner\V1\Mutation}
+ *
+ * @internal
  */
 trait MutationTrait
 {
     use ArrayTrait;
+
+    /**
+     * @var array
+     */
+    private array $mutationData = [];
+
+    /**
+     * Add an insert mutation.
+     *
+     * Example:
+     * ```
+     * $mutationGroup->insert('Posts', [
+     *     'ID' => 10,
+     *     'title' => 'My New Post',
+     *     'content' => 'Hello World'
+     * ]);
+     * ```
+     *
+     * @param string $table The table to insert into.
+     * @param array $data The data to insert.
+     * @return MutationGroup Current mutation group data object.
+     */
+    public function insert($table, array $data)
+    {
+        return $this->insertBatch($table, [$data]);
+    }
+
+    /**
+     * Add one or more insert mutations.
+     *
+     * Example:
+     * ```
+     * $mutationGroup->insertBatch('Posts', [
+     *     [
+     *         'ID' => 10,
+     *         'title' => 'My New Post',
+     *         'content' => 'Hello World'
+     *     ]
+     * ]);
+     * ```
+     *
+     * @param string $table The table to insert into.
+     * @param array $dataSet The data to insert.
+     * @return $this Current object instance, to enable object chaining.
+     */
+    public function insertBatch($table, array $dataSet)
+    {
+        $this->enqueue(Operation::OP_INSERT, $table, $dataSet);
+
+        return $this;
+    }
+
+    /**
+     * Add an update mutation.
+     *
+     * Example:
+     * ```
+     * $mutationGroup->update('Posts', [
+     *     'ID' => 10,
+     *     'title' => 'My New Post [Updated!]',
+     *     'content' => 'Modified Content'
+     * ]);
+     * ```
+     *
+     * @param string $table The table to update.
+     * @param array $data The data to update.
+     * @return $this Current object instance, to enable object chaining.
+     */
+    public function update($table, array $data)
+    {
+        return $this->updateBatch($table, [$data]);
+    }
+
+    /**
+     * Add one or more update mutations.
+     *
+     * Example:
+     * ```
+     * $mutationGroup->updateBatch('Posts', [
+     *     [
+     *         'ID' => 10,
+     *         'title' => 'My New Post [Updated!]',
+     *         'content' => 'Modified Content'
+     *     ]
+     * ]);
+     * ```
+     *
+     * @param string $table The table to update.
+     * @param array $dataSet The data to update.
+     * @return $this Current object instance, to enable object chaining.
+     */
+    public function updateBatch($table, array $dataSet)
+    {
+        $this->enqueue(Operation::OP_UPDATE, $table, $dataSet);
+
+        return $this;
+    }
+
+    /**
+     * Add an insert or update mutation.
+     *
+     * Example:
+     * ```
+     * $mutationGroup->insertOrUpdate('Posts', [
+     *     'ID' => 10,
+     *     'title' => 'My New Post',
+     *     'content' => 'Hello World'
+     * ]);
+     * ```
+     *
+     * @param string $table The table to insert into or update.
+     * @param array $data The data to insert or update.
+     * @return $this Current mutation group, to enable object chaining.
+     */
+    public function insertOrUpdate($table, array $data)
+    {
+        return $this->insertOrUpdateBatch($table, [$data]);
+    }
+
+    /**
+     * Add one or more insert or update mutations.
+     *
+     * Example:
+     * ```
+     * $mutationGroup->insertOrUpdateBatch('Posts', [
+     *     [
+     *         'ID' => 10,
+     *         'title' => 'My New Post',
+     *         'content' => 'Hello World'
+     *     ]
+     * ]);
+     * ```
+     *
+     * @param string $table The table to insert into or update.
+     * @param array $dataSet The data to insert or update.
+     * @return $this Current object instance, to enable object chaining.
+     */
+    public function insertOrUpdateBatch($table, array $dataSet)
+    {
+        $this->enqueue(Operation::OP_INSERT_OR_UPDATE, $table, $dataSet);
+
+        return $this;
+    }
+
+    /**
+     * Add an replace mutation.
+     *
+     * Example:
+     * ```
+     * $mutationGroup->replace('Posts', [
+     *     'ID' => 10,
+     *     'title' => 'My New Post [Replaced]',
+     *     'content' => 'Hello Moon'
+     * ]);
+     * ```
+     *
+     * @param string $table The table to replace into.
+     * @param array $data The data to replace.
+     * @return $this Current object instance, to enable object chaining.
+     */
+    public function replace($table, array $data)
+    {
+        return $this->replaceBatch($table, [$data]);
+    }
+
+    /**
+     * Add one or more replace mutations.
+     *
+     * Example:
+     * ```
+     * $mutationGroup->replaceBatch('Posts', [
+     *     [
+     *         'ID' => 10,
+     *         'title' => 'My New Post [Replaced]',
+     *         'content' => 'Hello Moon'
+     *     ]
+     * ]);
+     * ```
+     *
+     * @param string $table The table to replace into.
+     * @param array $dataSet The data to replace.
+     * @return $this Current object instance, to enable object chaining.
+     */
+    public function replaceBatch($table, array $dataSet)
+    {
+        $this->enqueue(Operation::OP_REPLACE, $table, $dataSet);
+
+        return $this;
+    }
+
+    /**
+     * Add an delete mutation.
+     *
+     * Example:
+     * ```
+     * $keySet = new KeySet([
+     *     'keys' => [10]
+     * ]);
+     *
+     * $mutationGroup->delete('Posts', $keySet);
+     * ```
+     *
+     * @param string $table The table to mutate.
+     * @param KeySet $keySet The KeySet to identify rows to delete.
+     * @return $this Current object instance, to enable object chaining.
+     */
+    public function delete($table, KeySet $keySet)
+    {
+        $this->enqueue(Operation::OP_DELETE, $table, [$keySet]);
+
+        return $this;
+    }
+
+    /**
+     * Format, validate and enqueue mutations in the transaction.
+     *
+     * @param string $op The operation type.
+     * @param string $table The table name
+     * @param array $dataSet the mutations to enqueue
+     * @return void
+     */
+    private function enqueue($op, $table, array $dataSet)
+    {
+        foreach ($dataSet as $data) {
+            if ($op === Operation::OP_DELETE) {
+                $this->mutationData[] = $this->deleteMutation($table, $data);
+            } else {
+                $this->mutationData[] = $this->mutation($op, $table, $data);
+            }
+        }
+    }
+
     /**
      * Create a formatted mutation.
      *
@@ -62,6 +296,15 @@ trait MutationTrait
                 'keySet' => $this->flattenKeySet($keySet),
             ]
         ];
+    }
+
+    /**
+     * Returns the mutation data as associative array.
+     * @return array
+     */
+    private function getMutations()
+    {
+        return $this->mutationData;
     }
 
     private function getValueMapper()
