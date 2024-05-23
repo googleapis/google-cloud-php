4d09967e8c6f82d8f6592046911b4698f74101e3
Saransh Dhingra <saranshdhingra@google.com>
2024-05-23 07:09:27 +0000

    Added 'final' class bypassing in Core's bootstrap file

--- Core/unit-bootstrap.php
+++ Core/unit-bootstrap.php
@@ -3,8 +3,12 @@
 use Google\ApiCore\Testing\MessageAwareArrayComparator;
 use Google\ApiCore\Testing\ProtobufMessageComparator;
 use Google\ApiCore\Testing\ProtobufGPBEmptyComparator;
+use DG\BypassFinals;
 
 date_default_timezone_set('UTC');
 \SebastianBergmann\Comparator\Factory::getInstance()->register(new MessageAwareArrayComparator());
 \SebastianBergmann\Comparator\Factory::getInstance()->register(new ProtobufMessageComparator());
 \SebastianBergmann\Comparator\Factory::getInstance()->register(new ProtobufGPBEmptyComparator());
+
+// Make sure that while testing we bypass the `final` keyword for the GAPIC client.
+BypassFinals::enable();
