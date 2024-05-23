6198f82628e2f6f1d8976cabb42358b4fe6d8e00
Saransh Dhingra <saranshdhingra@google.com>
2024-05-23 07:37:11 +0000

    Added 'final' class bypassing in Core's snippet-bootstrap file

--- Core/snippet-bootstrap.php
+++ Core/snippet-bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
 use Google\Cloud\Core\Testing\TestHelpers;
+use DG\BypassFinals;
 
 TestHelpers::snippetBootstrap();
 
 date_default_timezone_set('UTC');
+
+// Make sure that while testing we bypass the `final` keyword for the GAPIC client.
+BypassFinals::enable();
