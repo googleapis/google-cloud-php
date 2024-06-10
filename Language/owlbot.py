1d94d4da0d47ee8069c83ebc27599f557ba7adec
Brent Shaffer <betterbrent@google.com>
2024-06-10 22:43:32 +0000

    updates for promoting to stable/v1

--- Language/owlbot.py
+++ Language/owlbot.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,7 +30,13 @@ dest = Path().resolve()
 # Added so that we can pass copy_excludes in the owlbot_main() call
 _tracked_paths.add(src)
 
-php.owlbot_main(src=src, dest=dest)
+php.owlbot_main(
+    src=src,
+    dest=dest,
+    copy_excludes=[
+        src / "**/[A-Z]*_*.php",
+    ]
+)
 
 # remove class_alias code
 s.replace(
