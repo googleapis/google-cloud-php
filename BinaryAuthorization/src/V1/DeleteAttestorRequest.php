3a7a27d2dc27f0ff91ceb1de288c3ff7bc51921d
Brent Shaffer <betterbrent@google.com>
2024-06-13 15:07:14 -0700

    chore!: promote BinaryAuthorization to V1 (#7392)

--- BinaryAuthorization/src/V1/DeleteAttestorRequest.php
+++ BinaryAuthorization/src/V1/DeleteAttestorRequest.php
@@ -21,7 +21,7 @@ class DeleteAttestorRequest extends \Google\Protobuf\Internal\Message
      *
      * Generated from protobuf field <code>string name = 1 [(.google.api.field_behavior) = REQUIRED, (.google.api.resource_reference) = {</code>
      */
-    private $name = '';
+    protected $name = '';
 
     /**
      * @param string $name Required. The name of the [attestors][google.cloud.binaryauthorization.v1.Attestor] to delete, in the format
