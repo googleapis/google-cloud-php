bf65b45292f998b9d0b31c3a90f8c47783a77072
Brent Shaffer <betterbrent@google.com>
2025-01-06 21:03:26 +0000

    remove unneded owlbot logic

--- CommonProtos/owlbot.py
+++ CommonProtos/owlbot.py
@@ -62,14 +62,6 @@ for proto in protos:
 s.move("metadata/Google/Iam/V1", "metadata/Iam/V1")
 s.move("metadata/Google/Logging/Type", "metadata/Logging/Type")
 
-# remove owl-bot-staging dir
-if os.path.exists(php.STAGING_DIR):
-    shutil.rmtree(Path(php.STAGING_DIR))
-
-# remove the metadata/Google files that we copied
-if os.path.exists("metadata/Google"):
-    shutil.rmtree(Path("metadata/Google"))
-
 s.replace(
     "src/**/*.php",
     r"^// Adding a class alias for backwards compatibility with the previous class name.$"
