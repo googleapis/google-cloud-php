80bda9497b4cdc17975dad9197f1d044a4e525b9
Brent Shaffer <betterbrent@google.com>
2025-01-08 20:01:46 +0000

    fix generated clients

--- Monitoring/src/V3/Client/QueryServiceClient.php
+++ Monitoring/src/V3/Client/QueryServiceClient.php
@@ -34,6 +34,7 @@ use Google\ApiCore\ValidationException;
 use Google\Auth\FetchAuthTokenInterface;
 use Google\Cloud\Monitoring\V3\QueryTimeSeriesRequest;
 use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * Service Description: The QueryService API is used to manage time series data in Cloud
@@ -144,6 +145,9 @@ final class QueryServiceClient
      *     @type callable $clientCertSource
      *           A callable which returns the client cert as a string. This can be used to
      *           provide a certificate and private key to the transport layer for mTLS.
+     *     @type false|LoggerInterface $logger
+     *           A PSR-3 compliant logger. If set to false, logging is disabled, ignoring the
+     *           'GOOGLE_SDK_PHP_LOGGING' environment flag
      * }
      *
      * @throws ValidationException
