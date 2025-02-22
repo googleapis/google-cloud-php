ba58f9184e749b71dedc5ac0af27cd564b371dfd
Brent Shaffer <betterbrent@google.com>
2025-02-21 16:00:23 -0800

    Revert "remove shell classes"
    This reverts commit 72e3b936e8a2611be5680be7364c16e968934c3d.

---
+++ Spanner/src/Admin/Instance/V1/InstanceAdminClient.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Copyright 2017 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was generated from the file
+ * https://github.com/google/googleapis/blob/master/google/spanner/admin/instance/v1/spanner_instance_admin.proto
+ * and updates to that file get reflected here through a refresh process.
+ *
+ * EXPERIMENTAL: This client library class has not yet been declared GA (1.0). This means that
+ * even though we intend the surface to be stable, we may make backwards incompatible changes
+ * if necessary.
+ *
+ * @experimental
+ */
+
+namespace Google\Cloud\Spanner\Admin\Instance\V1;
+
+use Google\Cloud\Spanner\Admin\Instance\V1\Gapic\InstanceAdminGapicClient;
+
+/**
+ * {@inheritdoc}
+ */
+class InstanceAdminClient extends InstanceAdminGapicClient
+{
+    // This class is intentionally empty, and is intended to hold manual
+    // additions to the generated {@see InstanceAdminGapicClient} class.
+}
