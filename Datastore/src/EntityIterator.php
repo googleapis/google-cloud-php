c17d2c30119fe866f4e0ee4e1e5eafead1d0ab49
Héctor Mendoza <hecmenjac@google.com>
2025-06-10 16:12:11 -0400

    feat(Datastore): Add support for the ExplainOptions (#8361)

--- Datastore/src/EntityIterator.php
+++ Datastore/src/EntityIterator.php
@@ -18,6 +18,7 @@
 namespace Google\Cloud\Datastore;
 
 use Google\Cloud\Core\Iterator\ItemIteratorTrait;
+use Google\Cloud\Datastore\V1\ExplainMetrics;
 
 /**
  * Iterates over a set of {@see \Google\Cloud\Datastore\Entity} items.
@@ -25,6 +26,11 @@ use Google\Cloud\Core\Iterator\ItemIteratorTrait;
 class EntityIterator implements \Iterator
 {
     use ItemIteratorTrait;
+
+    /**
+     * @var null|ExplainMetrics
+     */
+    private null|ExplainMetrics $explainMetrics = null;
 
     /**
      * The state of the query after the current batch.
@@ -40,5 +46,100 @@ class EntityIterator implements \Iterator
         return method_exists($this->pageIterator, 'moreResultsType')
             ? $this->pageIterator->moreResultsType()
             : null;
+    }
+
+    /**
+     * Returns a ExplainMetrics object from the query.
+     *
+     * By default, the query does not get executed and the explain metrics object
+     * contains only the planning statistics {@see \Google\Cloud\Datastore\V1\ExplainMetrics}.
+     *
+     * If the request was configured with the ExplainOptions object `Analyze` attribute to true
+     * the request then also gets executed, including the ExecutionStats on the ExplainMetrics
+     * object
+     *
+     * Contrary to looping through the result of run query, this method caches the first metrics
+     * to avoid variations when analyzing said metrics.
+     *
+     * Example:
+     * ```
+     * use Google\Cloud\Datastore\DatastoreClient;
+     * use Google\Cloud\Datastore\V1\ExplainOptions;
+     *
+     * $datastore = new DatastoreClient();
+     *
+     * explainOptions = (new ExplainOptions())->setAnalyze(false);
+     * $queryOptions = [
+     *     'explainOptions' => $explainOptions
+     * ];
+     *
+     * // The query does not get executed
+     * $res = $datastore->runQuery($query, $queryOptions);
+     *
+     * $explainMetrics = $res->getExplainMetrics();
+     *
+     * // This is populated
+     * $explainMetrics->planningSummary
+     *
+     * // This is not populated
+     * $explainMetrics->executionStats
+     * ```
+     *
+     * Example:
+     * ```
+     * explainOptions = (new ExplainOptions())->setAnalyze(true);
+     * $queryOptions = [
+     *     'explainOptions' => $explainOptions
+     * ];
+     *
+     * // The query does not get executed
+     * $res = $datastore->runQuery($query, $queryOptions);
+     *
+     * $explainMetrics = $res->getExplainMetrics();
+     *
+     * // This is populated
+     * $explainMetrics->getPlanSummary()
+     *
+     * // This is also populated
+     * $explainMetrics->getExecutionStats()
+     * ```
+     *
+     * @return null|ExplainMetrics
+     */
+    public function getExplainMetrics(): null|ExplainMetrics
+    {
+        if (is_null($this->explainMetrics)) {
+            $this->explainMetrics = $this->gatherExplainMetrics();
+        }
+
+        return $this->explainMetrics;
+    }
+
+    private function gatherExplainMetrics(): null|explainMetrics
+    {
+        $metrics = null;
+        $this->pageIterator->current();
+
+        while (is_null($metrics)) {
+            $metrics = $this->pageIterator->getExplainMetrics();
+
+            if (!$this->nextResultToken()) {
+                break;
+            }
+
+            $this->pageIterator->next();
+        }
+
+        $this->rewind();
+
+        if (is_null($metrics)) {
+            return null;
+        }
+
+        $explainMetrics = new ExplainMetrics();
+        $jsonString = json_encode($metrics);
+        $explainMetrics->mergeFromJsonString($jsonString);
+
+        return $explainMetrics;
     }
 }
