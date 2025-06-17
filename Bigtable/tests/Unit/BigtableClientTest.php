f3c0c6c968b9b1724ffb60e9dbaebe05c52821a5
Brent Shaffer <betterbrent@google.com>
2025-06-17 15:33:12 -0700

    feat(Bigtable): add pingAndWarm Client option (#8229)

--- Bigtable/tests/Unit/BigtableClientTest.php
+++ Bigtable/tests/Unit/BigtableClientTest.php
@@ -19,9 +19,14 @@ namespace Google\Cloud\Bigtable\Tests\Unit;
 
 use Google\Cloud\Bigtable\BigtableClient;
 use Google\Cloud\Bigtable\Table;
+use Google\Cloud\Bigtable\V2\Client\BigtableClient as BigtableGapicClient;
+use Google\Cloud\Bigtable\V2\PingAndWarmRequest;
+use Google\Cloud\Bigtable\V2\PingAndWarmResponse;
 use Google\Cloud\Core\InsecureCredentialsWrapper;
 use Google\Cloud\Core\Testing\TestHelpers;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
 
 /**
  * @group bigtable
@@ -29,6 +34,8 @@ use PHPUnit\Framework\TestCase;
  */
 class BigtableClientTest extends TestCase
 {
+    use ProphecyTrait;
+
     private $client;
 
     public function setUp(): void
@@ -43,5 +50,62 @@ class BigtableClientTest extends TestCase
         $table = $this->client->table('my-instance', 'my-table');
 
         $this->assertInstanceOf(Table::class, $table);
+    }
+
+    public function testPingAndWarm()
+    {
+        $gapicClient = $this->prophesize(BigtableGapicClient::class);
+        $gapicClient->pingAndWarm(Argument::any())
+            ->shouldNotBeCalled();
+
+        $bigtable = new BigtableClient([
+            'projectId' => 'my-project',
+            'credentials' => new InsecureCredentialsWrapper(),
+        ]);
+        (fn() => $this->gapicClient = $gapicClient->reveal())->call($bigtable);
+
+        // calling "::table" without the option does not pingandwarm
+        $bigtable->table('my-instance', 'my-table');
+
+        // creating the client with the option does not pingandwarm
+        $bigtable = new BigtableClient([
+            'projectId' => 'my-project',
+            'credentials' => new InsecureCredentialsWrapper(),
+            'pingAndWarm' => true, // this would throw an auth error if it was called
+        ]);
+
+        $pingAndWarmResponse = new PingAndWarmResponse();
+        $gapicClient = $this->prophesize(BigtableGapicClient::class);
+        $gapicClient->pingAndWarm(
+            Argument::that(function (PingAndWarmRequest $request) {
+                return $request->getName() === 'projects/my-project/instances/my-instance';
+            }),
+            []
+        )
+            ->shouldBeCalledOnce()
+            ->willReturn($pingAndWarmResponse);
+        $gapicClient->pingAndWarm(
+            Argument::that(function (PingAndWarmRequest $request) {
+                return $request->getName() === 'projects/my-project/instances/my-instance-2';
+            }),
+            []
+        )
+            ->shouldBeCalledOnce()
+            ->willReturn($pingAndWarmResponse);
+
+        // calling "::table" with the option should call pingandwarm
+        $bigtable = new BigtableClient([
+            'projectId' => 'my-project',
+            'credentials' => new InsecureCredentialsWrapper(),
+            'gapicClient' => $gapicClient->reveal(),
+            'pingAndWarm' => true,
+        ]);
+        (fn() => $this->gapicClient = $gapicClient->reveal())->call($bigtable);
+        $bigtable->table('my-instance', 'my-table');
+
+        // "PingAndWarm" will only be called once per instance
+        $bigtable->table('my-instance', 'my-table'); // not called
+        $bigtable->table('my-instance', 'my-table-2'); // still not called
+        $bigtable->table('my-instance-2', 'my-table'); // called
     }
 }
