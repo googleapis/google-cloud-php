a5c7c74116b670d21cb88d7336b0d731b4d393a9
Brent Shaffer <betterbrent@google.com>
2025-07-23 15:35:42 -0700

    add validateOptions helper method

--- Core/src/ApiHelperTrait.php
+++ Core/src/ApiHelperTrait.php
@@ -20,6 +20,7 @@ namespace Google\Cloud\Core;
 use Google\ApiCore\ArrayTrait;
 use Google\ApiCore\Options\CallOptions;
 use Google\Protobuf\NullValue;
+use Google\Protobuf\Internal\Message;
 
 /**
  * @internal
@@ -263,5 +264,43 @@ trait ApiHelperTrait
         $callOptions = $this->pluckArray($keys, $input);
 
         return [$input, $callOptions];
+    }
+
+    /**
+     * Helper method used to validate optons based on the supplied $optionTypes
+     * $optionTypes can be an array of string keys, a protobuf Message classname, or a
+     * the CallOptions classname. Parameters are split and returned in the order
+     * that the options types are provided.
+     */
+    private function validateOptions(array $options, array|string ...$optionTypes): array
+    {
+        $splitOptions = [];
+        foreach ($optionTypes as $optionType) {
+            if (is_array($optionType)) {
+                $splitOptions[] = $this->pluckArray($optionType, $options);
+            } elseif (is_string($optionType)) {
+                if (is_subclass_of($optionType, Message::class)) {
+                    $messageKeys = array_map(
+                        fn ($method) => lcfirst(substr($method, 3)),
+                        array_filter(
+                            get_class_methods($optionType),
+                            fn ($m) => 0 === strpos($m, 'get')
+                        )
+                    );
+                    $splitOptions[] = $this->pluckArray($messageKeys, $options);
+                } elseif ($optionType === CallOptions::class) {
+                    $callOptionKeys = array_keys((new CallOptions([]))->toArray());
+                    $splitOptions[] = $this->pluckArray($callOptionKeys, $options);
+                }
+            }
+        }
+
+        if (!empty($options)) {
+            throw new \Exception(
+                'Unexpected option(s) provided: ' . implode(', ', array_keys($options))
+            );
+        }
+
+        return $splitOptions;
     }
 }
