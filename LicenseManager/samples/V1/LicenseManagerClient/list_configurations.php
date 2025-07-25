78ff024d57c677cfc6a8688d96b21e2c84aadd70
Héctor Mendoza <hecmenjac@google.com>
2025-07-25 17:01:59 -0400

    feat: add LicenseManager component (#8471)

---
+++ LicenseManager/samples/V1/LicenseManagerClient/list_configurations.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START licensemanager_v1_generated_LicenseManager_ListConfigurations_sync]
+use Google\ApiCore\ApiException;
+use Google\ApiCore\PagedListResponse;
+use Google\Cloud\LicenseManager\V1\Client\LicenseManagerClient;
+use Google\Cloud\LicenseManager\V1\Configuration;
+use Google\Cloud\LicenseManager\V1\ListConfigurationsRequest;
+
+/**
+ * Lists Configurations in a given project and location.
+ *
+ * @param string $formattedParent Parent value for ListConfigurationsRequest
+ *                                Please see {@see LicenseManagerClient::locationName()} for help formatting this field.
+ */
+function list_configurations_sample(string $formattedParent): void
+{
+    // Create a client.
+    $licenseManagerClient = new LicenseManagerClient();
+
+    // Prepare the request message.
+    $request = (new ListConfigurationsRequest())
+        ->setParent($formattedParent);
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var PagedListResponse $response */
+        $response = $licenseManagerClient->listConfigurations($request);
+
+        /** @var Configuration $element */
+        foreach ($response as $element) {
+            printf('Element data: %s' . PHP_EOL, $element->serializeToJsonString());
+        }
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $formattedParent = LicenseManagerClient::locationName('[PROJECT]', '[LOCATION]');
+
+    list_configurations_sample($formattedParent);
+}
+// [END licensemanager_v1_generated_LicenseManager_ListConfigurations_sync]
