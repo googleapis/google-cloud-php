05be50379b3c09f1a51c0737d130e2047b6c61c2
Brent Shaffer <betterbrent@google.com>
2025-08-22 14:07:52 -0700

    test cleanup

--- dev/tests/Unit/Command/UpdateReadmeSampleCommandTest.php
+++ dev/tests/Unit/Command/UpdateReadmeSampleCommandTest.php
@@ -29,31 +29,30 @@ use Symfony\Component\Filesystem\Filesystem;
 class UpdateReadmeSampleCommandTest extends TestCase
 {
     private static CommandTester $commandTester;
-    private $root;
+    private $componentDir;
 
     public function setUp(): void
     {
-        $this->root = sys_get_temp_dir() . '/google-cloud-php-tests-' . rand();
-        $componentDir = $this->root . '/ClientSnippets';
+        $this->componentDir = sys_get_temp_dir() . '/google-cloud-php-tests-' . rand() . '/ClientSnippets';
         $filesystem = new Filesystem();
-        $filesystem->mirror(__DIR__ . '/../../fixtures/component/ClientSnippets', $componentDir);
+        $filesystem->mirror(__DIR__ . '/../../fixtures/component/ClientSnippets', $this->componentDir);
 
         // Write minimum composer.json
-        file_put_contents($componentDir . '/composer.json', json_encode([
+        file_put_contents($this->componentDir . '/composer.json', json_encode([
             'name' => 'ClientSnippets',
             'description' => 'Component for testing',
             'autoload' => ['psr-4' => ['TestComponent' => 'src']],
             'homepage' => 'https://github.com/googleapis/google-cloud-php-client-snippet',
         ]));
         // Write minimum repo-metadata.json
-        file_put_contents($componentDir . '/.repo-metadata.json', json_encode([
+        file_put_contents($this->componentDir . '/.repo-metadata.json', json_encode([
             'release_level' => 'preview',
             'client_documentation' => 'https://github.com/googleapis/google-cloud-php',
             'library_type' => 'GAPIC_AUTO',
         ]));
 
         $application = new Application();
-        $application->add(new UpdateReadmeSampleCommand($this->root));
+        $application->add(new UpdateReadmeSampleCommand($this->componentDir . '/..'));
         self::$commandTester = new CommandTester($application->get('update-readme-sample'));
     }
 
@@ -65,14 +64,14 @@ class UpdateReadmeSampleCommandTest extends TestCase
 
         $this->assertStringContainsString(
             "use Google\Cloud\ClientSnippets\V1\Client\ClientSnippetsClient;",
-            file_get_contents($this->root . '/ClientSnippets/README.md')
+            file_get_contents($this->componentDir . '/README.md')
         );
     }
 
     public function testUpdateSampleWithNewVersion()
     {
-        $v1SamplePath = $this->root . '/ClientSnippets/samples/v1/ClientSnippetsClient/an_rpc_method.php';
-        $v2SampleDir = $this->root . '/ClientSnippets/samples/v2/ClientSnippetsClient';
+        $v1SamplePath = $this->componentDir . '/samples/v1/ClientSnippetsClient/an_rpc_method.php';
+        $v2SampleDir = $this->componentDir . '/samples/v2/ClientSnippetsClient';
         $v2Sample = str_replace('V1', 'V2', file_get_contents($v1SamplePath));
         if (!is_dir($v2SampleDir)) {
             mkdir($v2SampleDir, 0777, true);
@@ -80,7 +79,7 @@ class UpdateReadmeSampleCommandTest extends TestCase
         file_put_contents($v2SampleDir . '/a_v2_rpc_method.php', $v2Sample);
 
         // Create mock component so the version shows up
-        $v2SrcDir = $this->root . '/ClientSnippets/src/V2';
+        $v2SrcDir = $this->componentDir . '/src/V2';
         if (!is_dir($v2SrcDir)) {
             mkdir($v2SrcDir, 0777, true);
         }
@@ -90,7 +89,7 @@ class UpdateReadmeSampleCommandTest extends TestCase
             '--component' => ['ClientSnippets'],
         ]);
 
-        $readme = file_get_contents($this->root . '/ClientSnippets/README.md');
+        $readme = file_get_contents($this->componentDir . '/README.md');
         $this->assertStringContainsString(
             "use Google\Cloud\ClientSnippets\V2\Client\ClientSnippetsClient;",
             $readme
