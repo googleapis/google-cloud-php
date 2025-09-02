6a8a6b0a99940ab6d91aa7a41b297b7adce0475b
hectorhammett <hecmenjac@google.com>
2025-09-02 10:35:16 +0000

    Update the OperationTest.php

---
+++ Datastore/tests/Unit/ProtoEncodeTrait.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Datastore\Tests\Unit;
+
+use Google\Protobuf\Internal\Message;
+use InvalidArgumentException;
+
+trait ProtoEncodeTrait
+{
+    public static function generateProto(string $message, array $data): Message
+    {
+        /** @var Message */
+        $message = new $message();
+        $json = json_encode($data);
+
+        if ($json === false) {
+            throw new InvalidArgumentException('The data given cannot be serialized into Json');
+        }
+
+        $message->mergeFromJsonString($json);
+
+        return $message;
+    }
+}
