c0d1e227e59b26e0aef45ef0d7deccf6327f5c0d
Brent Shaffer <betterbrent@google.com>
2025-10-01 14:34:47 +0000

    feat: introduce SecurityCompliance

---
+++ SecurityCompliance/samples/V1/ConfigClient/get_cloud_control.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START cloudsecuritycompliance_v1_generated_Config_GetCloudControl_sync]
+use Google\ApiCore\ApiException;
+use Google\Cloud\CloudSecurityCompliance\V1\Client\ConfigClient;
+use Google\Cloud\CloudSecurityCompliance\V1\CloudControl;
+use Google\Cloud\CloudSecurityCompliance\V1\GetCloudControlRequest;
+
+/**
+ * Gets details of a single CloudControl.
+ * This method retrieves a CloudControl resource, which can be either Built-in
+ * or Custom, identified by its name.
+ *
+ * By default, the latest major version of the CloudControl is returned.
+ * A specific major version can be retrieved by specifying the
+ * `major_revision_id` in the request.
+ *
+ * @param string $formattedName The name of the cloudcontrol to retrieve in the format:
+ *                              organizations/{organization}/locations/{location}/cloudControls/{cloud_control}
+ *                              Please see {@see ConfigClient::cloudControlName()} for help formatting this field.
+ */
+function get_cloud_control_sample(string $formattedName): void
+{
+    // Create a client.
+    $configClient = new ConfigClient();
+
+    // Prepare the request message.
+    $request = (new GetCloudControlRequest())
+        ->setName($formattedName);
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var CloudControl $response */
+        $response = $configClient->getCloudControl($request);
+        printf('Response data: %s' . PHP_EOL, $response->serializeToJsonString());
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $formattedName = ConfigClient::cloudControlName('[ORGANIZATION]', '[LOCATION]', '[CLOUD_CONTROL]');
+
+    get_cloud_control_sample($formattedName);
+}
+// [END cloudsecuritycompliance_v1_generated_Config_GetCloudControl_sync]
