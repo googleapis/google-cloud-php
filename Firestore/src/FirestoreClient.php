8874e027023762b373a4741fe4f6213c54fece2c
Jérôme Gamez <jerome@gamez.name>
2025-10-26 22:52:16 +0100

    chore: Remove `@deprecated` annotation from config array fields

--- Firestore/src/FirestoreClient.php
+++ Firestore/src/FirestoreClient.php
@@ -122,7 +122,7 @@ class FirestoreClient
      *     @type FetchAuthTokenInterface $credentialsFetcher A credentials
      *           fetcher instance.
      *     @type array $keyFile [DEPRECATED]
-     *           @deprecated This option is being deprecated because of a potential security risk.
+     *           This option is being deprecated because of a potential security risk.
      *           This option does not validate the credential configuration. The security
      *           risk occurs when a credential configuration is accepted from a source
      *           that is not under your control and used without validation on your side.
@@ -144,7 +144,7 @@ class FirestoreClient
      *           configurations received from external sources.
      *           @see https://cloud.google.com/docs/authentication/external/externally-sourced-credentials
     *     @type string $keyFilePath [DEPRECATED]
-     *           @deprecated This option is being deprecated because of a potential security risk.
+     *           This option is being deprecated because of a potential security risk.
      *           This option does not validate the credential configuration. The security
      *           risk occurs when a credential configuration is accepted from a source
      *           that is not under your control and used without validation on your side.
--- Trace/src/TraceClient.php
+++ Trace/src/TraceClient.php
@@ -73,7 +73,7 @@ class TraceClient
      *     @type FetchAuthTokenInterface $credentialsFetcher A credentials
      *           fetcher instance.
      *     @type array $keyFile [DEPRECATED]
-     *           @deprecated This option is being deprecated because of a potential security risk.
+     *           This option is being deprecated because of a potential security risk.
      *           This option does not validate the credential configuration. The security
      *           risk occurs when a credential configuration is accepted from a source
      *           that is not under your control and used without validation on your side.
@@ -95,7 +95,7 @@ class TraceClient
      *           configurations received from external sources.
      *           @see https://cloud.google.com/docs/authentication/external/externally-sourced-credentials
     *     @type string $keyFilePath [DEPRECATED]
-     *           @deprecated This option is being deprecated because of a potential security risk.
+     *           This option is being deprecated because of a potential security risk.
      *           This option does not validate the credential configuration. The security
      *           risk occurs when a credential configuration is accepted from a source
      *           that is not under your control and used without validation on your side.
