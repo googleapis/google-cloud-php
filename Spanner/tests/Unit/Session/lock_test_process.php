d3950939e2b56664d276794ffbacfb4ab1de4b45
Brent Shaffer <betterbrent@google.com>
2025-11-10 17:23:46 -0700

    chore: update to latest google/auth and use sysv cache in tests (#8707)

--- Spanner/tests/Unit/Session/lock_test_process.php
+++ Spanner/tests/Unit/Session/lock_test_process.php
@@ -3,11 +3,12 @@
 namespace Google\Cloud\Spanner\Tests\Unit\Session;
 
 use DG\BypassFinals;
+use Exception;
+use Google\Auth\Cache\FileSystemCacheItemPool;
 use Google\Cloud\Spanner\Session\SessionCache;
 use Google\Cloud\Spanner\V1\Client\SpannerClient;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 /**
  * Runs a process which is designed to wait while a session is acquired.
@@ -16,21 +17,33 @@ if (count($argv) !== 2) {
     die('Usage: lock_test_process.php DATABASE_NAME' . PHP_EOL);
 }
 
-if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+$spannerAutoload = __DIR__ . '/../../../vendor/autoload.php';
+$googleCloudAutoload = __DIR__ . '/../../../../vendor/autoload.php';
+
+if (file_exists($spannerAutoload) && file_exists($googleCloudAutoload)) {
+    throw new Exception('Both autoloaders exist, please remove one');
+}
+
+if (file_exists($spannerAutoload)) {
     // google/cloud-spanner autoload
-    require __DIR__ . '/../../../vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    require $spannerAutoload;
+} elseif (file_exists($googleCloudAutoload)) {
     // google/cloud autoload
-    require __DIR__ . '/../../../../vendor/autoload.php';
+    require $googleCloudAutoload;
+} else {
+    throw new Exception('no autoloader found');
 }
 
 BypassFinals::enable();
 
-$acquireSession = new class($argv[1]) {
+[$_cmd, $databaseName] = $argv;
+
+$acquireSession = new class($databaseName) {
     use ProphecyTrait;
 
-    public function __construct(private string $databaseName)
-    {
+    public function __construct(
+        private string $databaseName,
+    ) {
     }
 
     public function run(): string
@@ -41,11 +54,9 @@ $acquireSession = new class($argv[1]) {
                 throw new \Exception('createSession called in child process - this shouldn\'t happen');
             });
 
-        $parts = explode('/', $this->databaseName);
         $sessionCache = new SessionCache(
             $spannerClient->reveal(),
-            $this->databaseName,
-            ['cacheItemPool' => new FilesystemAdapter(array_pop($parts))]
+            $this->databaseName
         );
 
         return $sessionCache->name();
