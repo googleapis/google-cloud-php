60dcaf2a1d9e0b7d7102827fd6f9102db73ee9c6
Brent Shaffer <betterbrent@google.com>
2025-12-08 20:00:54 +0000

    feat: add AdsDataManager component

---
+++ AdsDataManager/README.md
@@ -0,0 +1,73 @@
+# Google Ads Data Manager for PHP
+
+> Idiomatic PHP client for [Google Ads Data Manager](https://developers.google.com/data-manager).
+
+[![Latest Stable Version](https://poser.pugx.org/googleads/datamanager/v/stable)](https://packagist.org/packages/googleads/datamanager) [![Packagist](https://img.shields.io/packagist/dm/googleads/datamanager.svg)](https://packagist.org/packages/googleads/datamanager)
+
+* [API documentation](https://cloud.google.com/php/docs/reference/googleads/datamanager/latest)
+
+**NOTE:** This repository is part of [Google Cloud PHP](https://github.com/googleapis/google-cloud-php). Any
+support requests, bug reports, or development contributions should be directed to
+that project.
+
+### Installation
+
+To begin, install the preferred dependency manager for PHP, [Composer](https://getcomposer.org/).
+
+Now, install this component:
+
+```sh
+$ composer require googleads/datamanager
+```
+
+> Browse the complete list of [Google Cloud APIs](https://cloud.google.com/php/docs/reference)
+> for PHP
+
+This component supports both REST over HTTP/1.1 and gRPC. In order to take advantage of the benefits
+offered by gRPC (such as streaming methods) please see our
+[gRPC installation guide](https://cloud.google.com/php/grpc).
+
+### Authentication
+
+Please see our [Authentication guide](https://github.com/googleapis/google-cloud-php/blob/main/AUTHENTICATION.md) for more information
+on authenticating your client. Once authenticated, you'll be ready to start making requests.
+
+### Sample
+
+```php
+use Google\Ads\DataManager\V1\Client\IngestionServiceClient;
+use Google\Ads\DataManager\V1\RetrieveRequestStatusRequest;
+use Google\Ads\DataManager\V1\RetrieveRequestStatusResponse;
+use Google\ApiCore\ApiException;
+
+// Create a client.
+$ingestionServiceClient = new IngestionServiceClient();
+
+// Prepare the request message.
+$request = (new RetrieveRequestStatusRequest())
+    ->setRequestId($requestId);
+
+// Call the API and handle any network failures.
+try {
+    /** @var RetrieveRequestStatusResponse $response */
+    $response = $ingestionServiceClient->retrieveRequestStatus($request);
+    printf('Response data: %s' . PHP_EOL, $response->serializeToJsonString());
+} catch (ApiException $ex) {
+    printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+}
+```
+
+See the [samples directory](https://github.com/googleapis/php-ads-datamanager/tree/main/samples) for a canonical list of samples.
+
+### Debugging
+
+Please see our [Debugging guide](https://github.com/googleapis/google-cloud-php/blob/main/DEBUG.md)
+for more information about the debugging tools.
+
+### Version
+
+This component is considered alpha. As such, it is still a work-in-progress and is more likely to get backwards-incompatible updates.
+
+### Next Steps
+
+1. Understand the [official documentation](https://developers.google.com/data-manager).
---
+++ AdsDataManager/samples/V1/IngestionServiceClient/remove_audience_members.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * GENERATED CODE WARNING
+ * This file was automatically generated - do not edit!
+ */
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// [START datamanager_v1_generated_IngestionService_RemoveAudienceMembers_sync]
+use Google\Ads\DataManager\V1\AudienceMember;
+use Google\Ads\DataManager\V1\Client\IngestionServiceClient;
+use Google\Ads\DataManager\V1\Destination;
+use Google\Ads\DataManager\V1\ProductAccount;
+use Google\Ads\DataManager\V1\RemoveAudienceMembersRequest;
+use Google\Ads\DataManager\V1\RemoveAudienceMembersResponse;
+use Google\ApiCore\ApiException;
+
+/**
+ * Removes a list of
+ * [AudienceMember][google.ads.datamanager.v1.AudienceMember] resources from
+ * the provided [Destination][google.ads.datamanager.v1.Destination].
+ *
+ * @param string $destinationsOperatingAccountAccountId The ID of the account. For example, your Google Ads account ID.
+ * @param string $destinationsProductDestinationId      The object within the product account to ingest into. For
+ *                                                      example, a Google Ads audience ID, a Display & Video 360 audience ID or a
+ *                                                      Google Ads conversion action ID.
+ */
+function remove_audience_members_sample(
+    string $destinationsOperatingAccountAccountId,
+    string $destinationsProductDestinationId
+): void {
+    // Create a client.
+    $ingestionServiceClient = new IngestionServiceClient();
+
+    // Prepare the request message.
+    $destinationsOperatingAccount = (new ProductAccount())
+        ->setAccountId($destinationsOperatingAccountAccountId);
+    $destination = (new Destination())
+        ->setOperatingAccount($destinationsOperatingAccount)
+        ->setProductDestinationId($destinationsProductDestinationId);
+    $destinations = [$destination,];
+    $audienceMembers = [new AudienceMember()];
+    $request = (new RemoveAudienceMembersRequest())
+        ->setDestinations($destinations)
+        ->setAudienceMembers($audienceMembers);
+
+    // Call the API and handle any network failures.
+    try {
+        /** @var RemoveAudienceMembersResponse $response */
+        $response = $ingestionServiceClient->removeAudienceMembers($request);
+        printf('Response data: %s' . PHP_EOL, $response->serializeToJsonString());
+    } catch (ApiException $ex) {
+        printf('Call failed with message: %s' . PHP_EOL, $ex->getMessage());
+    }
+}
+
+/**
+ * Helper to execute the sample.
+ *
+ * This sample has been automatically generated and should be regarded as a code
+ * template only. It will require modifications to work:
+ *  - It may require correct/in-range values for request initialization.
+ *  - It may require specifying regional endpoints when creating the service client,
+ *    please see the apiEndpoint client configuration option for more details.
+ */
+function callSample(): void
+{
+    $destinationsOperatingAccountAccountId = '[ACCOUNT_ID]';
+    $destinationsProductDestinationId = '[PRODUCT_DESTINATION_ID]';
+
+    remove_audience_members_sample(
+        $destinationsOperatingAccountAccountId,
+        $destinationsProductDestinationId
+    );
+}
+// [END datamanager_v1_generated_IngestionService_RemoveAudienceMembers_sync]
