92ac1803531c21f3a764a6e58ab47aef729e8f80
Brent Shaffer <betterbrent@google.com>
2026-04-07 15:51:43 -0700

    add legacy interceptors

--- Gax/tests/Unit/testdata/mocks/TestInterceptor.php
+++ Gax/tests/Unit/testdata/mocks/TestInterceptor.php
@@ -2,31 +2,54 @@
 
 namespace Google\ApiCore\Tests\Unit\Transport;
 
+use Google\ApiCore\Transport\Grpc\UnaryInterceptorInterface;
 use Grpc\Interceptor;
 
-class TestInterceptor extends Interceptor
-{
-    public function interceptUnaryUnary(
-        $method,
-        $argument,
-        $deserialize,
-        $continuation,
-        array $metadata = [],
-        array $options = []
-    ) {
-        $options['test-interceptor-insert'] = 'inserted-value';
-        return $continuation($method, $argument, $deserialize, $metadata, $options);
-    }
+$useDeprecatedInterceptors = (new \ReflectionClass(Interceptor::class))
+    ->getMethod('interceptUnaryUnary')
+    ->getParameters()[3]
+    ->getName() === 'metadata';
 
-    public function interceptUnaryStream(
-        $method,
-        $argument,
-        $deserialize,
-        $continuation,
-        array $metadata = [],
-        array $options = []
-    ) {
-        $options['test-interceptor-insert'] = 'inserted-value';
-        return $continuation($method, $argument, $deserialize, $metadata, $options);
+if ($useDeprecatedInterceptors) {
+    class TestUnaryInterceptor implements UnaryInterceptorInterface
+    {
+        public function interceptUnaryUnary(
+            $method,
+            $argument,
+            $deserialize,
+            array $metadata,
+            array $options,
+            callable $continuation
+        ) {
+            $options['test-interceptor-insert'] = 'inserted-value';
+            return $continuation($method, $argument, $deserialize, $metadata, $options);
+        }
+    }
+} else {
+    class TestInterceptor extends Interceptor
+    {
+        public function interceptUnaryUnary(
+            $method,
+            $argument,
+            $deserialize,
+            $continuation,
+            array $metadata = [],
+            array $options = []
+        ) {
+            $options['test-interceptor-insert'] = 'inserted-value';
+            return $continuation($method, $argument, $deserialize, $metadata, $options);
+        }
+
+        public function interceptUnaryStream(
+            $method,
+            $argument,
+            $deserialize,
+            $continuation,
+            array $metadata = [],
+            array $options = []
+        ) {
+            $options['test-interceptor-insert'] = 'inserted-value';
+            return $continuation($method, $argument, $deserialize, $metadata, $options);
+        }
     }
 }
--- Gax/tests/Unit/testdata/mocks/TestUnaryInterceptor.php
+++ Gax/tests/Unit/testdata/mocks/TestUnaryInterceptor.php
@@ -2,19 +2,54 @@
 
 namespace Google\ApiCore\Tests\Unit\Transport;
 
+use Google\ApiCore\Transport\Grpc\UnaryInterceptorInterface;
 use Grpc\Interceptor;
 
-class TestUnaryInterceptor extends Interceptor
-{
-    public function interceptUnaryUnary(
-        $method,
-        $argument,
-        $deserialize,
-        $continuation = null,
-        array $metadata = [],
-        array $options = []
-    ) {
-        $options['test-interceptor-insert'] = 'inserted-value';
-        return $continuation($method, $argument, $deserialize, $metadata, $options);
+$useDeprecatedInterceptors = (new \ReflectionClass(Interceptor::class))
+    ->getMethod('interceptUnaryUnary')
+    ->getParameters()[3]
+    ->getName() === 'metadata';
+
+if ($useDeprecatedInterceptors) {
+    class TestInterceptor extends Interceptor
+    {
+        public function interceptUnaryUnary(
+            $method,
+            $argument,
+            $deserialize,
+            array $metadata = [],
+            array $options = [],
+            $continuation
+        ) {
+            $options['test-interceptor-insert'] = 'inserted-value';
+            return $continuation($method, $argument, $deserialize, $metadata, $options);
+        }
+
+        public function interceptUnaryStream(
+            $method,
+            $argument,
+            $deserialize,
+            array $metadata = [],
+            array $options = [],
+            $continuation
+        ) {
+            $options['test-interceptor-insert'] = 'inserted-value';
+            return $continuation($method, $argument, $deserialize, $metadata, $options);
+        }
+    }
+} else {
+    class TestUnaryInterceptor extends Interceptor
+    {
+        public function interceptUnaryUnary(
+            $method,
+            $argument,
+            $deserialize,
+            $continuation = null,
+            array $metadata = [],
+            array $options = []
+        ) {
+            $options['test-interceptor-insert'] = 'inserted-value';
+            return $continuation($method, $argument, $deserialize, $metadata, $options);
+        }
     }
 }
