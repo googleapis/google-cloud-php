c3100b2ed1029a67841098658d3de8cf14e66982
hectorhammett <hecmenjac@google.com>
2026-04-08 20:34:54 +0000

    Update the Firestore client version to 2.0.0

--- Firestore/src/FirestoreClient.php
+++ Firestore/src/FirestoreClient.php
@@ -88,7 +88,7 @@ class FirestoreClient
     use ClientOptionsTrait;
     use RequestProcessorTrait;
 
-    const VERSION = '2.0.0-RC1';
+    const VERSION = '2.0.0';
 
     const DEFAULT_DATABASE = '(default)';
     const FULL_CONTROL_SCOPE = 'https://www.googleapis.com/auth/cloud-platform';
