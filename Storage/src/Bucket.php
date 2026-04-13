b65b013104792a43f27529f83c10bfa1696a2f21
salilg-eng <salilg@google.com>
2026-04-13 10:10:47 +0000

    Updated code

--- Storage/src/Bucket.php
+++ Storage/src/Bucket.php
@@ -762,9 +762,6 @@ class Bucket
      *           distinct results. **Defaults to** `false`.
      *     @type string $fields Selector which will cause the response to only
      *           return the specified fields.
-     *     @type string $filter Filter results to include only objects to which the
-     *           specified context is attached. You can filter by the presence,
-     *           absence, or specific value of context keys.
      *     @type string $matchGlob A glob pattern to filter results. The string
      *           value must be UTF-8 encoded. See:
      *           https://cloud.google.com/storage/docs/json_api/v1/objects/list#list-object-glob
