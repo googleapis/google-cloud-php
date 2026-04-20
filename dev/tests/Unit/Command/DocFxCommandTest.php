354f726ffbd28c069ded6ba13fef53f573e4ff0f
Brent Shaffer <betterbrent@google.com>
2026-04-20 12:29:24 -0700

    fix docfx tests

--- dev/tests/Unit/Command/DocFxCommandTest.php
+++ dev/tests/Unit/Command/DocFxCommandTest.php
@@ -41,7 +41,7 @@ class DocFxCommandTest extends TestCase
         $componentDir = __DIR__ . '/../../../../Vision';
         $process = DocFxCommand::getPhpDocCommand($componentDir, self::$tmpDir);
         $process->mustRun();
-        $left = self::$fixturesDir . '/phpdoc/structure.xml';
+        $left = self::$fixturesDir . '/phpdoc/vision.xml';
         $right = self::$tmpDir . '/structure.xml';
 
         $this->assertFileEqualsWithDiff($left, $right, '1' === getenv('UPDATE_FIXTURES'));
@@ -138,7 +138,7 @@ class DocFxCommandTest extends TestCase
     {
         $output = self::getCommandTester()->execute([
             '--component' => 'Vision',
-            '--xml' => self::$fixturesDir . '/phpdoc/structure.xml',
+            '--xml' => self::$fixturesDir . '/phpdoc/vision.xml',
             '--out' => self::$tmpDir = sys_get_temp_dir() . '/' . rand(),
             '--metadata-version' => '1.0.0',
             '--path' => self::$fixturesDir . '/component/Vision',
