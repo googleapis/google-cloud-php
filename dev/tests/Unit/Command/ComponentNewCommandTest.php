7e327bdcd7ef3400915b417dd0338ef1b15395a3
Min Zhu <zhumin@google.com>
2026-08-06 04:15:48 +0000

    fix order-dependent overwrite prompt expectations in test

--- dev/tests/Unit/Command/ComponentNewCommandTest.php
+++ dev/tests/Unit/Command/ComponentNewCommandTest.php
@@ -108,6 +108,10 @@ class ComponentNewCommandTest extends TestCase
 
     public function testNewComponentWithCustomOptions()
     {
+        if (!is_dir(self::$tmpDir . '/SecretManager')) {
+            mkdir(self::$tmpDir . '/SecretManager', 0777, true);
+        }
+
         self::$commandTester->setInputs([
             'Y',                                                            // Component already exists. Overwrite it? ? [Y/n]
             'n',                                                            // Does this information look correct? [Y/n]
@@ -164,6 +168,10 @@ class ComponentNewCommandTest extends TestCase
 
     public function testNewComponentWithUpdateComponent()
     {
+        if (!is_dir(self::$tmpDir . '/SecretManager')) {
+            mkdir(self::$tmpDir . '/SecretManager', 0777, true);
+        }
+
         $dummyCommand = $this->prophesize(Command::class);
 
         $dummyCommand->isEnabled()->willReturn(true);
@@ -229,6 +237,10 @@ class ComponentNewCommandTest extends TestCase
 
     public function testNewComponentWithExistingComponent()
     {
+        if (!is_dir(self::$tmpDir . '/SecretManager')) {
+            mkdir(self::$tmpDir . '/SecretManager', 0777, true);
+        }
+
         $application = new Application();
         $application->add(new ComponentNewCommand(self::$tmpDir));
 
