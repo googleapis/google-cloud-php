8ab829e733a29639e030bde11c306d68b4b3674c
hectorhammett <hecmenjac@google.com>
2026-08-21 19:09:00 +0000

    update interfaceNode to the correct patch for Auth

--- dev/src/DocFx/Node/InterfaceNode.php
+++ dev/src/DocFx/Node/InterfaceNode.php
@@ -38,6 +38,7 @@ class InterfaceNode extends ClassNode
     {
         // Project root components
         $componentDirs = array_map('realpath', glob(__DIR__ . '/../../../../*/src', GLOB_ONLYDIR));
+        $componentDirs[] = __DIR__ . '/../../../vendor/google/cloud/Auth/src';
 
         $finder = new ComposerFinder();
         $finder
