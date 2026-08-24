8451f26dcca9534134391bfbdd4a0db591b11e2d
salilg-eng <salilg@google.com>
2026-08-24 05:32:04 +0000

    fix(Storage): ensure StorageClient retryStrategy is applied correctly to all operations

--- Storage/src/Connection/Rest.php
+++ Storage/src/Connection/Rest.php
@@ -317,6 +317,8 @@ class Rest implements ConnectionInterface
             'prettyPrint' => false,
         ];
 
+        $args['retryStrategy'] ??= $this->retryStrategy;
+
         $args['restRetryFunction'] = $this->restRetryFunction ?? $this->getRestRetryFunction(
             'objects',
             'get',
@@ -324,7 +326,6 @@ class Rest implements ConnectionInterface
         );
 
         $args += array_filter([
-            'retryStrategy' => $this->retryStrategy,
             'restDelayFunction' => $this->restDelayFunction,
             'restCalcDelayFunction' => $this->restCalcDelayFunction,
             'restRetryListener' => $this->restRetryListener,
@@ -1023,6 +1024,8 @@ class Rest implements ConnectionInterface
         ];
         $retryResource = isset($retryMap[$resource]) ? $retryMap[$resource] : $resource;
 
+        $options['retryStrategy'] ??= $this->retryStrategy;
+
         $options['restRetryFunction'] = $this->restRetryFunction ?? $this->getRestRetryFunction(
             $retryResource,
             $method,
@@ -1030,7 +1033,6 @@ class Rest implements ConnectionInterface
         );
 
         $options += array_filter([
-            'retryStrategy' => $this->retryStrategy,
             'restDelayFunction' => $this->restDelayFunction,
             'restCalcDelayFunction' => $this->restCalcDelayFunction,
             'restRetryListener' => $this->restRetryListener,
