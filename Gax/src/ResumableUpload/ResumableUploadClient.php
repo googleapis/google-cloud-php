8d72117de7ec1ed9808bab0934e5d2b0cab621a9
Brent Shaffer <betterbrent@google.com>
2026-09-03 11:10:40 +0000

    fix(Gax): restore ?int typehint for timeoutMillis in sendRequest

--- Gax/src/ResumableUpload/ResumableUploadClient.php
+++ Gax/src/ResumableUpload/ResumableUploadClient.php
@@ -476,7 +476,7 @@ class ResumableUploadClient
 
     private function sendRequest(
         RequestInterface $request,
-        int|float|null $timeoutMillis = null,
+        ?int $timeoutMillis = null,
         ?RetrySettings $retrySettings = null
     ): ResponseInterface {
         $reqHeaders = $request->getHeaders();
